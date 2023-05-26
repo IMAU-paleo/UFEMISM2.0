1f6d7022f350185a23ec2370ba4b8c933788ea27
Victor Azizi <victor@lipsum.eu>
2023-05-26 09:16:57 +0200

    update nix flake, support nix run (#2)

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # UFEMISM2.0
 Version 2.0 of the Utrecht FinitE voluMe Ice-Sheet Model
+
+## Running with nix
+
+If you have access to a linux machine with the nix package manager. UFEMISM can be run with:
+
+```
+nix run github:IMAU-paleo/UFEMISM2.0?dir=tools/flake 2 <config_file>
+```
+
+where `2` specifies the number of processors. This is a bit different then the normal syntax. Because
+normally UFEMISM is run with `mpirun -n 2` prefixed instead of `2` being part of the UFEMISM command line. 
--- tools/flake/flake.nix
+++ tools/flake/flake.nix
@@ -26,17 +26,39 @@
           ncview
       ];
     };
-    defaultPackage.x86_64-linux = 
+    packages.x86_64-linux.UFEMISM = 
     stdenv.mkDerivation {
-       name = "minimism";
+       name = "UFEMISM";
        src = self;
        sourceRoot = "source/src";
-       buildInputs = self.devShell.x86_64-linux.buildInputs;
+       buildInputs = [ gfortran
+                       openmpi
+                       netcdf4
+                       netcdffortran
+                       petsc
+                       lapack
+                       pkg-config ];
        installPhase = "
-         cp UFEMISM_program minimism;
          mkdir -p $out/bin; 
-         install -t $out/bin minimism";
+         install -t $out/bin UFEMISM_program
+         install -t $out/bin ../tools/flake/UFEMISM_wrapper.sh";
     };
+
+
+    # nix run <loc>#UFEMISM_program
+    apps.x86_64-linux.UFEMISM_program = {
+    type = "app";
+    program = "${self.packages.x86_64-linux.UFEMISM}/bin/UFEMISM_program";
+    };
+
+    # nix run <loc>#UFEMISM_wrapper
+    apps.x86_64-linux.UFEMISM_wrapper = {
+    type = "app";
+    program = "${self.packages.x86_64-linux.UFEMISM}/bin/UFEMISM_wrapper.sh";
+    };
+
+    # Default nix run
+    apps.x86_64-linux.default = self.apps.x86_64-linux.UFEMISM_wrapper;
   };
 
 }
