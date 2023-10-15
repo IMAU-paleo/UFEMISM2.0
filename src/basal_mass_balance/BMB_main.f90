50845662d7bfebd70ead890470ff7d71c5a19a3f
Jorjo Bernales <j.a.bernalesconcha@uu.nl>
2023-10-15 19:47:22 +0200

    Remove grounded area fraction factor from extrapolated BMB
    Too conservative, not justified.

--- src/basal_mass_balance/BMB_main.f90
+++ src/basal_mass_balance/BMB_main.f90
@@ -596,13 +596,7 @@ CONTAINS
 
       ! Grounded-side grounding lines, or previously iced inverted ones
       ELSEIF (ice%mask_gl_gr( vi) .OR. (ice%mask_icefree_ocean( vi) .AND. BMB%mask_gl_gr( vi))) THEN
-        IF (BMB%mask_gl_gr( vi)) THEN
-          ! Original grounded-side grounding line: apply full value
-          BMB%BMB( vi) = BMB_gl_gr( vi)
-        ELSE
-          ! New grounded-side grounding line: scale value based on floating fraction
-          BMB%BMB( vi) = BMB_gl_gr( vi) * (1._dp - ice%fraction_gr( vi))
-        END IF
+        BMB%BMB( vi) = BMB_gl_gr( vi)
 
       ! Original ice-free ocean
       ELSEIF (ice%mask_icefree_ocean( vi)) THEN
