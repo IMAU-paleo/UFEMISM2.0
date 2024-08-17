dea96899ea7e9d4fe3fef671dfd1f9621f63eab9
TijnBerends <c.j.berends@uu.nl>
2024-08-17 15:36:43 +0200

    Split assertions_unit_tests into smaller modules
    Because I plan to add a bunch of new test_tol_XXX routines for derived types (grids, mesh)

---
+++ src/validation/assertions_unit_tests_grid_lonlat.f90
@@ -0,0 +1,15 @@
+module assertions_unit_tests_grid_lonlat
+
+  ! The assertions/unit tests for simple lon/lat-grids.
+
+  use assertions_unit_tests_basic, only: ASSERTION, UNIT_TEST, process_test_result
+
+  implicit none
+
+  private
+
+  !public :: test_tol_grid_lonlat
+
+contains
+
+end module assertions_unit_tests_grid_lonlat
