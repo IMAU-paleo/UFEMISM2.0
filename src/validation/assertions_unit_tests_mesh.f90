8f442dff337658e567b7570f5b793bf8553d161e
TijnBerends <c.j.berends@uu.nl>
2024-08-17 16:32:57 +0200

    Add test_tol_mesh
    Test if two meshes are identical. Only check the primary mesh data: V, nC, C, niTri, iTri, Tri, Tricc, and TriC (and ofc the metadata).

--- src/validation/assertions_unit_tests_mesh.f90
+++ src/validation/assertions_unit_tests_mesh.f90
@@ -3,13 +3,70 @@ module assertions_unit_tests_mesh
   ! The assertions/unit tests for meshes.
 
   use assertions_unit_tests_basic, only: ASSERTION, UNIT_TEST, process_test_result
+  use precisions, only: dp
+  use mesh_types, only: type_mesh
 
   implicit none
 
   private
 
-  !public :: test_tol_mesh
+  public :: test_tol_mesh
 
 contains
+
+  subroutine test_tol_mesh( mesh1, mesh2, tol_dist, test_mode, message)
+    ! In/output variables:
+    type(type_mesh),  intent(in   ) :: mesh1, mesh2
+    real(dp),         intent(in   ) :: tol_dist
+    integer,          intent(in   ) :: test_mode
+    character(len=*), intent(in   ) :: message
+    ! Local variables:
+    logical :: test_result
+    real(dp), dimension(:), allocatable :: dist
+
+    test_result = .true.
+
+    test_result = test_result .and. mesh1%nV          == mesh2%nV
+    test_result = test_result .and. mesh1%nTri        == mesh2%nTri
+    test_result = test_result .and. (mesh1%lambda_M    - mesh2%lambda_M   ) <= tol_dist
+    test_result = test_result .and. (mesh1%phi_M       - mesh2%phi_M      ) <= tol_dist
+    test_result = test_result .and. (mesh1%beta_stereo - mesh2%beta_stereo) <= tol_dist
+    test_result = test_result .and. (mesh1%xmin        - mesh2%xmin       ) <= tol_dist
+    test_result = test_result .and. (mesh1%xmax        - mesh2%xmax       ) <= tol_dist
+    test_result = test_result .and. (mesh1%ymin        - mesh2%ymin       ) <= tol_dist
+    test_result = test_result .and. (mesh1%ymax        - mesh2%ymax       ) <= tol_dist
+
+    ! Vertex data
+    if (mesh1%nV == mesh2%nV) then
+
+      dist = hypot( mesh1%V(1:mesh1%nV,1) - mesh2%V(1:mesh2%nV,1), &
+                    mesh1%V(1:mesh1%nV,2) - mesh2%V(1:mesh2%nV,2))
+      test_result = test_result .and. all(dist <= tol_dist)
+      test_result = test_result .and. all( mesh1%nC   ( 1:mesh1%nV  ) == mesh2%nC   ( 1:mesh2%nV  ))
+      test_result = test_result .and. all( mesh1%C    ( 1:mesh1%nV,:) == mesh2%C    ( 1:mesh2%nV,:))
+      test_result = test_result .and. all( mesh1%niTri( 1:mesh1%nV  ) == mesh2%niTri( 1:mesh2%nV  ))
+      test_result = test_result .and. all( mesh1%iTri ( 1:mesh1%nV,:) == mesh2%iTri ( 1:mesh2%nV,:))
+      test_result = test_result .and. all( mesh1%VBI  ( 1:mesh1%nV  ) == mesh2%VBI  ( 1:mesh2%nV  ))
+
+    else
+      test_result = .false.
+    end if
+
+    ! Triangle data
+    if (mesh1%nTri == mesh2%nTri) then
+
+      dist = hypot( mesh1%Tricc(1:mesh1%nTri,1) - mesh2%Tricc(1:mesh2%nTri,1), &
+                    mesh1%Tricc(1:mesh1%nTri,2) - mesh2%Tricc(1:mesh2%nTri,2))
+      test_result = test_result .and. all(dist <= tol_dist)
+      test_result = test_result .and. all( mesh1%Tri ( 1:mesh1%nTri,:) == mesh2%Tri ( 1:mesh2%nTri,:))
+      test_result = test_result .and. all( mesh1%TriC( 1:mesh1%nTri,:) == mesh2%TriC( 1:mesh2%nTri,:))
+
+    else
+      test_result = .false.
+    end if
+
+    call process_test_result( test_mode, test_result, message)
+
+  end subroutine test_tol_mesh
 
 end module assertions_unit_tests_mesh
