bf06532311b9ad76892ae58425d7b4c690c73e95
TijnBerends <c.j.berends@uu.nl>
2024-08-25 12:33:10 +0200

    Update analyse_mapping_derivative_tests.m

--- automated_testing/component_tests/analyse_mapping_derivative_tests.m
+++ automated_testing/component_tests/analyse_mapping_derivative_tests.m
@@ -1,8 +1,9 @@
 function analyse_mapping_derivative_tests( foldername)
 
 % foldername = '../../results_component_tests/discretisation/mapping_derivatives/';
+% addpath('../../tools/matlab/')
 
-addpath('../../tools/matlab/')
+addpath('tools/matlab/')
 
 do_print = true;
 
@@ -18,7 +19,7 @@ fprintf( fid, '%s\n', '=========================================================
 fclose( fid);
 
 % List all the test results
-filenames = dir( foldername)
+filenames = dir( foldername);
 i = 1;
 while i <= length( filenames)
   if  contains( filenames(i).name, 'results_map_deriv_tests_') && ...
@@ -666,61 +667,6 @@ function plot_mesh_data_c( ax, mesh, d, clim, cmap)
   % lastly NaN values
   line('parent',ax,'xdata',mesh.E( isnan(d),1),'ydata',mesh.E( isnan(d),2),'linestyle','none',...
       'marker','x','markerfacecolor','r','markeredgecolor','r','markersize',8);
-
-end
-
-function H = setup_multipanel_figure( wa, ha, margins_hor, margins_ver)
-  % Set up a single figure with mulitple axeses
-  
-  %% Safety
-  
-  % margins_hor and margins_ver need at least two values each
-  if length( margins_hor) < 2
-    error('need two values (left and right) for margins_hor!')
-  end
-  if length( margins_ver) < 2
-    error('need two values (top and bottom) for margins_ver!')
-  end
-  
-  % If a single value for wa or ha is provided, make all axeses the same size
-  if length( wa) == 1
-    wa = zeros( 1, length( margins_hor) - 1) + wa;
-  else
-    if length( wa) ~= length( margins_hor) - 1
-      error('length( wa) should be length( margins_hor) - 1!')
-    end
-  end
-  if length( ha) == 1
-    ha = zeros( 1, length( margins_ver) - 1) + ha;
-  else
-    if length( ha) ~= length( margins_ver) - 1
-      error('length( ha) should be length( margins_ver) - 1!')
-    end
-  end
-  
-  %% Set up GUI
-  
-  % Number of axeses
-  nax = length( margins_hor) - 1;
-  nay = length( margins_ver) - 1;
-  
-  % Figure size
-  wf = sum( margins_hor) + sum( wa);
-  hf = sum( margins_ver) + sum( ha);
-  
-  % Create figure
-  H.Fig = figure('position',[20,20,wf,hf],'color','w');
-  
-  % Create axeses
-  for i = 1: nay
-    for j = 1: nax
-      x = sum( margins_hor( 1:j)) + sum( wa( 1:j-1));
-      y = sum( margins_ver( 1:i)) + sum( ha( 1:i-1));
-      yb = hf - y - ha( i);
-      H.Ax{ i,j} = axes('parent',H.Fig,'units','pixels','position',[x,yb,wa( j),ha( i)],'fontsize',20,...
-        'xgrid','on','ygrid','on');
-    end
-  end
 
 end
 
