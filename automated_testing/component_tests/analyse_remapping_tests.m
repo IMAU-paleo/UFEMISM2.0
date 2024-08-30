e228d267286a654a310513e29860a6e4656c492e
TijnBerends <c.j.berends@uu.nl>
2024-08-30 17:14:33 +0200

    Reorder Matlab component test analysis scripts
    That.

--- automated_testing/component_tests/analyse_remapping_tests.m
+++ automated_testing/component_tests/analyse_remapping_tests.m
@@ -1,11 +1,4 @@
-function analyse_remapping_tests( foldername)
-
-% foldername = '../../results_component_tests/remapping';
-% addpath('../../tools/matlab/')
-
-addpath('tools/matlab/')
-
-do_print = false;
+function analyse_remapping_tests( foldername, do_print)
 
 analyse_remapping_tests_grid_to_mesh( [foldername '/grid_to_mesh'], do_print);
 analyse_remapping_tests_mesh_to_grid( [foldername '/mesh_to_grid'], do_print);
