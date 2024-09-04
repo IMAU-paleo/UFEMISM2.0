52c06b207f3429b007146315ae923a09f4b42c76
TijnBerends <c.j.berends@uu.nl>
2024-09-04 17:55:45 +0200

    Split up analysis of component and integrated tests
    Cleaner.

--- automated_testing/analysis_scripts/component_tests/analyse_component_tests.m
+++ automated_testing/analysis_scripts/component_tests/analyse_component_tests.m
@@ -1,5 +1,35 @@
-function analyse_component_tests( foldername_component_tests, foldername_automated_testing, do_print_figures)
-% Analyse the results of all the component tests
+function analyse_component_tests( varargin)
+% Analyse the results of all component tests and add the results to the
+% scoreboard files.
+
+% In the GitHub Workflow, provide the component test results folders as
+% input; but retain the option of running without input (i.e. as a script)
+% locally, with user-defined folders
+
+input_args = varargin;
+if isempty( input_args)
+  % Assume this is a local run
+
+  foldername_component_tests = '/Users/Beren017/Documents/GitHub/UFEMISM2.0/results_component_tests';
+  addpath('/Users/Beren017/Documents/GitHub/UFEMISM2.0/tools/matlab/')
+  do_print_figures = false;
+  foldername_automated_testing = '.';
+
+elseif length( input_args) == 1
+  % Assume this is a GitHub Workflow run
+
+  foldername_component_tests = varargin{1};
+  addpath('tools/matlab/')
+  do_print_figures = true;
+  foldername_automated_testing = 'automated_testing';
+
+else
+  error('need either foldername_component_tests, or nothing as input!')
+end
+
+addpath('analysis_scripts')
+addpath('analysis_scripts/basic')
+addpath('analysis_scripts/component_tests')
 
 analyse_component_tests_discretisation( ...
   [foldername_component_tests '/discretisation'], foldername_automated_testing, do_print_figures);
