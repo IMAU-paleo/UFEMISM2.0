1ebdc8bca58b6ace3a2e4e7160f15779846102b1
TijnBerends <c.j.berends@uu.nl>
2024-10-03 12:40:00 +0200

    Split trace_line_grid unit tests into smaller modules
    Because it was getting too long

--- src/validation/unit_tests/ut_mesh_remapping.f90
+++ src/validation/unit_tests/ut_mesh_remapping.f90
@@ -11,7 +11,9 @@ module ut_mesh_remapping
   use grid_types, only: type_grid
   use grid_basic, only: setup_square_grid
   use mpi_basic, only: par
-  use ut_mesh_remapping_trace_line_grid, only: test_trace_line_grid
+  use ut_mesh_remapping_trace_line_grid_start, only: test_trace_line_grid_start
+  use ut_mesh_remapping_trace_line_grid_a, only: test_trace_line_grid_a
+  use ut_mesh_remapping_trace_line_grid_b, only: test_trace_line_grid_b
 
   implicit none
 
@@ -44,5 +46,43 @@ contains
     call finalise_routine( routine_name)
 
   end subroutine test_remapping
+
+  subroutine test_trace_line_grid( test_name_parent)
+    ! Test the trace_line_grid subroutines
+
+    ! In/output variables:
+    character(len=*), intent(in) :: test_name_parent
+
+    ! Local variables:
+    character(len=1024), parameter :: routine_name = 'test_trace_line_grid'
+    character(len=1024), parameter :: test_name_local = 'trace_line_grid'
+    character(len=1024)            :: test_name
+    real(dp)                       :: xmin, xmax, ymin, ymax, dx
+    character(len=1024)            :: name
+    type(type_grid)                :: grid
+
+    ! Add routine to call stack
+    call init_routine( routine_name)
+
+    ! Add test name to list
+    test_name = trim( test_name_parent) // '/' // trim( test_name_local)
+
+    ! Initialise square grid
+    name = 'test_grid'
+    xmin = 0._dp
+    xmax = 10._dp
+    ymin = 0._dp
+    ymax = 10._dp
+    dx   = 1._dp
+    call setup_square_grid( name, xmin, xmax, ymin, ymax, dx, grid)
+
+    call test_trace_line_grid_start( test_name, grid)
+    call test_trace_line_grid_a    ( test_name, grid)
+    call test_trace_line_grid_b    ( test_name, grid)
+
+    ! Remove routine from call stack
+    call finalise_routine( routine_name)
+
+  end subroutine test_trace_line_grid
 
 end module ut_mesh_remapping
