4311eab0111ea52c5788126cb74c600485740aa5
erwinlambert <45197956+erwinlambert@users.noreply.github.com>
2024-10-03 11:42:02 +0200

    implement isomip_warm and cold ocean forcing

--- src/ocean/ocean_idealised.f90
+++ src/ocean/ocean_idealised.f90
@@ -21,7 +21,7 @@ CONTAINS
 ! ===== Main routines =====
 ! =========================
 
-  SUBROUTINE run_ocean_model_idealised( mesh, ice, ocean, time)
+  SUBROUTINE run_ocean_model_idealised( mesh, ice, ocean)
     ! Calculate the ocean
     !
     ! Use an idealised ocean scheme
@@ -32,7 +32,6 @@ CONTAINS
     TYPE(type_mesh),                        INTENT(IN)    :: mesh
     TYPE(type_ice_model),                   INTENT(IN)    :: ice
     TYPE(type_ocean_model),                 INTENT(INOUT) :: ocean
-    REAL(dp),                               INTENT(IN)    :: time
 
     ! Local variables:
     CHARACTER(LEN=256), PARAMETER                         :: routine_name = 'run_ocean_model_idealised'
@@ -40,6 +39,13 @@ CONTAINS
     ! Add routine to path
     CALL init_routine( routine_name)
 
+    ! Run the chosen idealised ocean model
+    IF (C%choice_ocean_model_idealised == 'ISOMIP_WARM' .OR. &
+        C%choice_ocean_model_idealised == 'ISOMIP_COLD') THEN
+      ! No need to do anything 
+    ELSE
+      CALL crash('unknown choice_ocean_model_idealised "' // TRIM( C%choice_ocean_model_idealised) // '"')
+    END IF
 
     ! Finalise routine path
     CALL finalise_routine( routine_name)
@@ -67,10 +73,10 @@ CONTAINS
     IF (par%master)  WRITE(*,"(A)") '     Initialising idealised ocean model "' // &
       colour_string( TRIM( C%choice_ocean_model_idealised),'light blue') // '"...'
 
-    ! Run the chosen idealised climate model
-    IF (C%choice_climate_model_idealised == 'ISOMIP_WARM' .OR. &
-        C%choice_climate_model_idealised == 'ISOMIP_COLD') THEN
-      ! No need to do anything
+    ! Run the chosen idealised ocean model
+    IF (C%choice_ocean_model_idealised == 'ISOMIP_WARM' .OR. &
+        C%choice_ocean_model_idealised == 'ISOMIP_COLD') THEN
+      CALL initialise_ocean_model_idealised_ISOMIP( mesh, ocean)
     ELSE
       CALL crash('unknown choice_ocean_model_idealised "' // TRIM( C%choice_ocean_model_idealised) // '"')
     END IF
@@ -83,26 +89,46 @@ CONTAINS
   ! == ISOMIP ==
   ! ============
 
-  SUBROUTINE run_ocean_model_idealised_ISOMIP( mesh, ice, ocean, time)
+  SUBROUTINE initialise_ocean_model_idealised_ISOMIP( mesh, ocean)
     ! 
 
     IMPLICIT NONE
 
     TYPE(type_mesh),                      INTENT(IN)    :: mesh
-    TYPE(type_ice_model),                 INTENT(IN)    :: ice
-    TYPE(type_ocean_model),             INTENT(INOUT) :: ocean
-    REAL(dp),                             INTENT(IN)    :: time
+    TYPE(type_ocean_model),               INTENT(INOUT) :: ocean
 
     ! Local variables:
-    CHARACTER(LEN=256), PARAMETER                       :: routine_name = 'run_ocean_model_idealised_ISOMIP'
+    CHARACTER(LEN=256), PARAMETER                       :: routine_name = 'initialise_ocean_model_idealised_ISOMIP'
     INTEGER                                             :: vi
+    INTEGER                                             :: k
+    REAL(dp), PARAMETER                                 :: z1 = 720_dp  ! [m] Reference depth
+    REAL(dp), PARAMETER                                 :: T0 = -1.9_dp ! [degC] Surface temperature
+    REAL(dp)                                            :: T1           ! [degC] Reference temperature
+    REAL(dp), PARAMETER                                 :: S0 = 33.8_dp ! [PSU]  Surface salinity
+    REAL(dp)                                            :: S1           ! [PSU]  Reference salinity
 
     ! Add routine to path
     CALL init_routine( routine_name)
 
+    ! Define scenario-dependent parameters
+    IF (C%choice_ocean_model_idealised == 'ISOMIP_WARM') THEN
+      T1 = 1.0_dp
+      S1 = 34.7_dp
+    ELSE IF (C%choice_ocean_model_idealised == 'ISOMIP_COLD') THEN
+      T1 = -1.9_dp
+      S1 = 34.55_dp
+    END IF
+    
+    DO vi = mesh%vi1, mesh%vi2
+      DO k = 1, C%nz_ocean
+        ocean%T( vi, k) = T0 + (T1-T0)*C%z_ocean( k)/z1      
+        ocean%S( vi, k) = S0 + (S1-S0)*C%z_ocean( k)/z1      
+      END DO
+    END DO
+
     ! Finalise routine path
     CALL finalise_routine( routine_name)
 
-  END SUBROUTINE run_ocean_model_idealised_ISOMIP
+  END SUBROUTINE initialise_ocean_model_idealised_ISOMIP
 
 END MODULE ocean_idealised
