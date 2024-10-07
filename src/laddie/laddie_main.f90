ad331a4c682dacbe279edc0977f8b68a86011e93
erwinlambert <45197956+erwinlambert@users.noreply.github.com>
2024-10-07 17:08:13 +0200

    include dummy laddie entrainment

--- src/laddie/laddie_main.f90
+++ src/laddie/laddie_main.f90
@@ -17,7 +17,7 @@ MODULE laddie_main
   USE BMB_model_types                                        , ONLY: type_BMB_model
   USE reallocate_mod                                         , ONLY: reallocate_bounds
   USE laddie_utilities                                       , ONLY: compute_ambient_TS, allocate_laddie_model
-  USE laddie_physics                                         , ONLY: compute_melt_rate
+  USE laddie_physics                                         , ONLY: compute_melt_rate, compute_entrainment
   USE laddie_thickness                                       , ONLY: compute_H_np1 
   USE laddie_velocity                                        , ONLY: compute_UV_np1
   USE laddie_tracers                                         , ONLY: compute_TS_np1
@@ -76,6 +76,9 @@ CONTAINS
 
     ! Compute melt rate
     CALL compute_melt_rate( mesh, ice, ocean, laddie)
+    
+    ! Compute entrainment
+    CALL compute_entrainment( mesh, ice, ocean, laddie)
 
     ! == Main time loop ==
     ! ====================
--- src/laddie/laddie_physics.f90
+++ src/laddie/laddie_physics.f90
@@ -45,13 +45,47 @@ CONTAINS
     DO vi = mesh%vi1, mesh%vi2
        IF (ice%mask_floating_ice( vi)) THEN
          laddie%melt( vi) = C%uniform_laddie_gamma_T * (laddie%T( vi) - ocean%T_freezing_point( vi))
+       ELSE
+         laddie%melt( vi) = 0.0_dp
        END IF
+  
     END DO
 
     ! Finalise routine path
     CALL finalise_routine( routine_name)
 
   END SUBROUTINE compute_melt_rate
+
+  SUBROUTINE compute_entrainment( mesh, ice, ocean, laddie)
+    ! Compute entrainment rate
+
+    ! In- and output variables
+
+    TYPE(type_mesh),                        INTENT(IN)    :: mesh
+    TYPE(type_ocean_model),                 INTENT(IN)    :: ocean
+    TYPE(type_ice_model),                   INTENT(IN)    :: ice
+    TYPE(type_laddie_model),                INTENT(INOUT) :: laddie
+
+    ! Local variables:
+    CHARACTER(LEN=256), PARAMETER                         :: routine_name = 'compute_entrainment'
+    INTEGER                                               :: vi
+ 
+    ! Add routine to path
+    CALL init_routine( routine_name)
+
+    ! Get T and S at layer base
+    DO vi = mesh%vi1, mesh%vi2
+       IF (ice%mask_floating_ice( vi)) THEN
+         laddie%entr( vi) = 1.0E-6_dp
+       ELSE
+         laddie%entr( vi) = 0.0_dp
+       END IF
+    END DO
+
+    ! Finalise routine path
+    CALL finalise_routine( routine_name)
+
+  END SUBROUTINE compute_entrainment
 
 END MODULE laddie_physics
 
