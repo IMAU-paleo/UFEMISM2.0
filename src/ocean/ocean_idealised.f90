62d18a7b53020f8431e045afa138148df027ae0b
erwinlambert <45197956+erwinlambert@users.noreply.github.com>
2024-10-07 10:57:43 +0200

    include tanh idealised ocean forcing

--- src/ocean/ocean_idealised.f90
+++ src/ocean/ocean_idealised.f90
@@ -42,6 +42,8 @@ CONTAINS
     ! Run the chosen idealised ocean model
     IF (C%choice_ocean_model_idealised == 'ISOMIP') THEN
       ! No need to do anything 
+    ELSE IF (C%choice_ocean_model_idealised == 'TANH') THEN
+      ! No need to do anything 
     ELSE
       CALL crash('unknown choice_ocean_model_idealised "' // TRIM( C%choice_ocean_model_idealised) // '"')
     END IF
@@ -75,6 +77,8 @@ CONTAINS
     ! Run the chosen idealised ocean model
     IF (C%choice_ocean_model_idealised == 'ISOMIP') THEN
       CALL initialise_ocean_model_idealised_ISOMIP( mesh, ocean)
+    IF (C%choice_ocean_model_idealised == 'TANH') THEN
+      CALL initialise_ocean_model_idealised_TANH( mesh, ocean)
     ELSE
       CALL crash('unknown choice_ocean_model_idealised "' // TRIM( C%choice_ocean_model_idealised) // '"')
     END IF
@@ -123,6 +127,48 @@ CONTAINS
       DO k = 1, C%nz_ocean
         ocean%T( vi, k) = T0 + (T1-T0)*C%z_ocean( k)/z1      
         ocean%S( vi, k) = S0 + (S1-S0)*C%z_ocean( k)/z1      
+      END DO
+    END DO
+
+    ! Finalise routine path
+    CALL finalise_routine( routine_name)
+
+  END SUBROUTINE initialise_ocean_model_idealised_ISOMIP
+
+  ! == TANH ==
+  ! ============
+
+  SUBROUTINE initialise_ocean_model_idealised_TANH( mesh, ocean)
+    ! Tangent hyperbolic function representing a two-layer ocean forcing separated by a smooth thermocline
+
+    IMPLICIT NONE
+
+    TYPE(type_mesh),                      INTENT(IN)    :: mesh
+    TYPE(type_ocean_model),               INTENT(INOUT) :: ocean
+
+    ! Local variables:
+    CHARACTER(LEN=256), PARAMETER                       :: routine_name = 'initialise_ocean_model_idealised_TANH'
+    INTEGER                                             :: vi
+    INTEGER                                             :: k
+    REAL(dp), PARAMETER                                 :: z1 = 100_dp     ! [m] Depth scale for thermocline sharpness
+    REAL(dp), PARAMETER                                 :: drho0 = 0.01_dp ! [kg m^-5] Density scale factor to set quadratic stratification
+    REAL(dp), PARAMETER                                 :: S0 = 34.0_dp    ! [PSU]  Surface salinity
+    REAL(dp),                                           :: T0              ! [deg C]  Surface freezing temperature
+
+    ! Add routine to path
+    CALL init_routine( routine_name)
+
+    DO vi = mesh%vi1, mesh%vi2
+      ! Get surface freezing temperature
+      T0 = freezing_lambda_1*S0 + freezing_lambda_2 
+
+      DO k = 1, C%nz_ocean
+        ! Get temperature value
+        ocean%T( vi, k) = T0 + (C%ocean_tanh_deep_temperature-T0)*(1+tanh((C%z_ocean( k)-C%ocean_tanh_thermocline_depth)/z1))/2      
+
+        ! Get salinity value at this depth based on quadratic density profile and linear equation of state
+        ocean%S( vi, k) = S0 + C%uniform_laddie_eos_linear_alpha * (ocean%T( vi, k)-T0)/C%uniform_laddie_eos_linear_beta &
+                        + drho0*C%z_ocean( k)**.5/(C%uniform_laddie_eos_linear_beta * density_seawater)      
       END DO
     END DO
 
