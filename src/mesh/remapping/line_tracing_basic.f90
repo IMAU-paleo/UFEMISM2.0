6fa62959ff92a05b9bf4fc32679bc3cd8f9b6a91
TijnBerends <c.j.berends@uu.nl>
2024-10-08 14:33:50 +0200

    Clean up trace_line_grid
    Now the old coincidence indicators are completely gone.

--- src/mesh/remapping/line_tracing_basic.f90
+++ src/mesh/remapping/line_tracing_basic.f90
@@ -12,7 +12,6 @@ module line_tracing_basic
 
   public :: add_integrals_to_single_row
   public :: type_coinc_ind_grid, no_value, a_grid, b_grid, cx_grid, cy_grid
-  public :: old2new_coinc_ind, coinc_ind2aij_in, coinc_ind2bij_on, coinc_ind2cxij_on, coinc_ind2cyij_on
 
   type type_coinc_ind_grid
     integer :: grid
@@ -120,86 +119,5 @@ contains
     single_row%LI_xydy(    1:n) = LI_xydy_temp
 
   end subroutine extend_single_row_memory
-
-  function old2new_coinc_ind( aij_in, bij_on, cxij_on, cyij_on, finished) result( coinc_ind)
-    integer, dimension(2), intent(in) :: aij_in, bij_on, cxij_on, cyij_on
-    logical,               intent(in) :: finished
-    type(type_coinc_ind_grid) :: coinc_ind
-
-    if (aij_in( 1) > 0 .or. aij_in( 2) > 0) then
-      coinc_ind%grid = a_grid
-      coinc_ind%i = aij_in( 1)
-      coinc_ind%j = aij_in( 2)
-    elseif (bij_on(  1) > 0 .or. bij_on(  2) > 0) then
-      coinc_ind%grid = b_grid
-      coinc_ind%i = bij_on( 1)
-      coinc_ind%j = bij_on( 2)
-    elseif (cxij_on( 1) > 0 .or. cxij_on( 2) > 0) then
-      coinc_ind%grid = cx_grid
-      coinc_ind%i = cxij_on( 1)
-      coinc_ind%j = cxij_on( 2)
-    elseif (cyij_on( 1) > 0 .or. cyij_on( 2) > 0) then
-      coinc_ind%grid = cy_grid
-      coinc_ind%i = cyij_on( 1)
-      coinc_ind%j = cyij_on( 2)
-    else
-      if (finished) then
-        coinc_ind%grid = no_value
-        coinc_ind%i    = 0
-        coinc_ind%j    = 0
-      else
-        call crash('old2new_coinc_ind found no coincidence indicators!')
-      end if
-    end if
-
-  end function old2new_coinc_ind
-
-  function coinc_ind2aij_in( coinc_ind) result( aij_in)
-    type(type_coinc_ind_grid), intent(in) :: coinc_ind
-    integer, dimension(2) :: aij_in
-
-    if (coinc_ind%grid == a_grid) then
-      aij_in = [coinc_ind%i, coinc_ind%j]
-    else
-      aij_in = [0,0]
-    end if
-
-  end function coinc_ind2aij_in
-
-  function coinc_ind2bij_on( coinc_ind) result( bij_on)
-    type(type_coinc_ind_grid), intent(in) :: coinc_ind
-    integer, dimension(2) :: bij_on
-
-    if (coinc_ind%grid == b_grid) then
-      bij_on = [coinc_ind%i, coinc_ind%j]
-    else
-      bij_on = [0,0]
-    end if
-
-  end function coinc_ind2bij_on
-
-  function coinc_ind2cxij_on( coinc_ind) result( cxij_on)
-    type(type_coinc_ind_grid), intent(in) :: coinc_ind
-    integer, dimension(2) :: cxij_on
-
-    if (coinc_ind%grid == cx_grid) then
-      cxij_on = [coinc_ind%i, coinc_ind%j]
-    else
-      cxij_on = [0,0]
-    end if
-
-  end function coinc_ind2cxij_on
-
-  function coinc_ind2cyij_on( coinc_ind) result( cyij_on)
-    type(type_coinc_ind_grid), intent(in) :: coinc_ind
-    integer, dimension(2) :: cyij_on
-
-    if (coinc_ind%grid == cy_grid) then
-      cyij_on = [coinc_ind%i, coinc_ind%j]
-    else
-      cyij_on = [0,0]
-    end if
-
-  end function coinc_ind2cyij_on
 
 end module line_tracing_basic
