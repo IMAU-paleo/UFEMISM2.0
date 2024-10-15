c4333d6d466784f616e0b05f028169dc8e463898
TijnBerends <c.j.berends@uu.nl>
2024-10-15 11:04:44 +0200

    Add unit tests for trace_line_tri_start
    Hurray

--- src/validation/unit_tests/ut_mesh_remapping.f90
+++ src/validation/unit_tests/ut_mesh_remapping.f90
@@ -6,18 +6,24 @@ module ut_mesh_remapping
   use assertions_basic
   use ut_basic
   use precisions, only: dp
+  use mpi_basic, only: par
   use control_resources_and_error_messaging, only: warning, crash, happy, init_routine, finalise_routine, colour_string
   use model_configuration, only: C
+  use parameters, only: pi
   use grid_types, only: type_grid
   use grid_basic, only: setup_square_grid
-  use mpi_basic, only: par
-  use parameters, only: pi
+  use mesh_types, only: type_mesh
+  use mesh_memory, only: allocate_mesh_primary
+  use mesh_dummy_meshes, only: initialise_dummy_mesh_5
+  use mesh_refinement_basic, only: refine_mesh_uniform
+  use mesh_secondary, only: calc_all_secondary_mesh_data
   use ut_mesh_remapping_trace_line_grid_start, only: test_trace_line_grid_start
   use ut_mesh_remapping_trace_line_grid_a, only: test_trace_line_grid_a
   use ut_mesh_remapping_trace_line_grid_b, only: test_trace_line_grid_b
   use ut_mesh_remapping_trace_line_grid_cx, only: test_trace_line_grid_cx
   use ut_mesh_remapping_trace_line_grid_cy, only: test_trace_line_grid_cy
   use ut_mesh_remapping_trace_line_grid, only: test_trace_line_grid
+  use ut_mesh_remapping_trace_line_tri_start, only: test_trace_line_tri_start
 
   implicit none
 
@@ -44,7 +50,8 @@ contains
     ! Add test name to list
     test_name = trim( test_name_parent) // '/' // trim( test_name_local)
 
-    call test_trace_line_grid_main( test_name)
+    ! call test_trace_line_grid_main( test_name)
+    call test_trace_line_tri_main ( test_name)
 
     ! Remove routine from call stack
     call finalise_routine( routine_name)
@@ -91,5 +98,47 @@ contains
     call finalise_routine( routine_name)
 
   end subroutine test_trace_line_grid_main
+
+  subroutine test_trace_line_tri_main( test_name_parent)
+    ! Test the trace_line_tri subroutines
+
+    ! In/output variables:
+    character(len=*), intent(in) :: test_name_parent
+
+    ! Local variables:
+    character(len=1024), parameter :: routine_name = 'test_trace_line_tri_main'
+    character(len=1024), parameter :: test_name_local = 'trace_line_tri'
+    character(len=1024)            :: test_name
+    real(dp)                       :: xmin, xmax, ymin, ymax, alpha_min, res_max
+    character(len=1024)            :: name
+    type(type_mesh)                :: mesh
+
+    ! Add routine to call stack
+    call init_routine( routine_name)
+
+    ! Add test name to list
+    test_name = trim( test_name_parent) // '/' // trim( test_name_local)
+
+    ! Create a simple test mesh
+    name = 'test_mesh'
+    xmin = 0._dp
+    xmax = pi
+    ymin = 0._dp
+    ymax = pi
+    alpha_min = 25._dp * pi / 180._dp
+    res_max = pi / 20._dp
+
+    call allocate_mesh_primary( mesh, name, 100, 200, C%nC_mem)
+    call initialise_dummy_mesh_5( mesh, xmin, xmax, ymin, ymax)
+    call refine_mesh_uniform( mesh, res_max, alpha_min)
+    call calc_all_secondary_mesh_data( mesh, C%lambda_M_ANT, C%phi_M_ANT, C%beta_stereo_ANT)
+
+    ! Run unit tests on this mesh
+    call test_trace_line_tri_start( test_name, mesh)
+
+    ! Remove routine from call stack
+    call finalise_routine( routine_name)
+
+  end subroutine test_trace_line_tri_main
 
 end module ut_mesh_remapping
