9785b63514701a932de1a8c44a5c0437df579102
TijnBerends <c.j.berends@uu.nl>
2024-10-20 15:12:16 +0200

    Add basics for tracer tracking model
    A types module containing a general tracer tracking model type and a sub-type for the particles-based model, a main model module containing an initialisation routine, and add it to the region type and call the init routine during region initialisation.

---
+++ src/types/tracer_tracking_model_types.f90
@@ -0,0 +1,34 @@
+module tracer_tracking_model_types
+
+  use precisions, only: dp
+
+  implicit none
+
+  private
+
+  public :: type_tracer_tracking_model, type_tracer_tracking_model_particles
+
+  type type_tracer_tracking_model_particles
+    ! The data structure for the particle-based tracer tracking model
+
+    logical,  dimension(:  ), allocatable :: is_in_use     ! Whether or not memory slot i is in use
+    real(dp), dimension(:,:), allocatable :: r             ! Particle coordinates (x,y,z)
+    real(dp), dimension(:,:), allocatable :: r_origin      ! Coordinates of particle origin (x,y,z)
+    real(dp), dimension(:  ), allocatable :: t_origin      ! Time of particle origin (i.e. time of snow deposition)
+    real(dp), dimension(:,:), allocatable :: tracers       ! Values of different tracers
+
+  end type type_tracer_tracking_model_particles
+
+  type type_tracer_tracking_model
+    ! The main tracer tracking model data structure.
+
+    real(dp), dimension(:,:  ), allocatable :: age           ! Age of ice [nV, zeta]
+    real(dp), dimension(:,:,:), allocatable :: tracers       ! Age of ice [nV, zeta, i_tracer]
+
+    type(type_tracer_tracking_model_particles) :: particles
+
+  end type type_tracer_tracking_model
+
+contains
+
+end module tracer_tracking_model_types
