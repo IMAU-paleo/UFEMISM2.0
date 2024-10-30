bf9fe61d8e9d326fd6acde0b956aae03d3299d6c
TijnBerends <c.j.berends@uu.nl>
2024-10-30 09:34:44 +0100

    Turn on other unit tests again
    Oopsie

--- src/validation/unit_tests/ut_mesh.f90
+++ src/validation/unit_tests/ut_mesh.f90
@@ -33,8 +33,8 @@ contains
     test_name = trim( test_name_parent) // '/' // trim( test_name_local)
 
     ! Run all unit tests for the mesh creation subroutines
-    ! call test_Delaunay  ( test_name)
-    ! call test_refinement( test_name)
+    call test_Delaunay  ( test_name)
+    call test_refinement( test_name)
     call test_remapping ( test_name)
 
     ! Remove routine from call stack
