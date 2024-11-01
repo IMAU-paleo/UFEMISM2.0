b1ab50b9439568c3d2730b6869d596a326d1d0b1
erwinlambert <45197956+erwinlambert@users.noreply.github.com>
2024-11-01 09:16:52 +0100

    routine update_laddie_masks

--- src/laddie/laddie_main.f90
+++ src/laddie/laddie_main.f90
@@ -44,12 +44,9 @@ CONTAINS
 
     ! Local variables:
     CHARACTER(LEN=256), PARAMETER                         :: routine_name = 'run_laddie_model'
-    INTEGER                                               :: vi, ti, i, no
+    INTEGER                                               :: vi, ti
     REAL(dp)                                              :: tl               ! [s] Laddie time
     REAL(dp)                                              :: dt               ! [s] Laddie time step
-    LOGICAL, DIMENSION(mesh%nV)                           :: mask_a_tot
-    LOGICAL, DIMENSION(mesh%nV)                           :: mask_gr_a_tot
-    LOGICAL, DIMENSION(mesh%nV)                           :: mask_oc_a_tot
  
     ! Add routine to path
     CALL init_routine( routine_name)
@@ -62,93 +59,7 @@ CONTAINS
     dt = C%dt_laddie
 
     ! == Update masks ==
-    ! Mask on a grid
-    DO vi = mesh%vi1, mesh%vi2
-      ! Check whether vertex on border
-      IF (mesh%VBI( vi) > 0) THEN
-        laddie%mask_a( vi)    = .false.
-        laddie%mask_gr_a( vi) = .true.
-      ELSE IF (ice%Hib( vi) - ice%Hb( vi) < 2*C%laddie_thickness_minimum) THEN
-        laddie%mask_a( vi)    = .false.
-        laddie%mask_gr_a( vi) = .true.
-      ELSE
-        ! Inherit regular masks
-        laddie%mask_a( vi)    = ice%mask_floating_ice( vi)
-        laddie%mask_gr_a( vi) = ice%mask_grounded_ice( vi) .OR. ice%mask_icefree_land( vi)
-        laddie%mask_oc_a( vi) = ice%mask_icefree_ocean( vi)
-      END IF
-    END DO
-      
-    ! Mask on b grid
-    CALL gather_to_all_logical_1D( laddie%mask_a, mask_a_tot)
-    CALL gather_to_all_logical_1D( laddie%mask_gr_a, mask_gr_a_tot)
-    CALL gather_to_all_logical_1D( laddie%mask_oc_a, mask_oc_a_tot)
-
-    DO ti = mesh%ti1, mesh%ti2
-      ! Initialise as false to overwrite previous mask
-      laddie%mask_b( ti)    = .false.
-      laddie%mask_gl_b( ti) = .false.
-      laddie%mask_cf_b( ti) = .false.
-      laddie%mask_oc_b( ti) = .false.
-
-      ! Define floating mask if any of the three vertices is floating
-      DO i = 1, 3
-        vi = mesh%Tri( ti, i)
-        IF (mask_a_tot( vi)) THEN
-          ! Set true if any of the three vertices is floating
-          laddie%mask_b( ti) = .true.
-        END IF
-      END DO
-
-      ! Define grounding line triangles 
-      DO i = 1, 3
-        vi = mesh%Tri( ti, i)
-        ! Check if any connected vertex is grounded
-        IF (mask_gr_a_tot( vi)) THEN
-          ! Omit triangle from floating mask. Adjust for no slip conditions
-          laddie%mask_b( ti) = .false.
-          ! Define as grounding line triangle
-          laddie%mask_gl_b( ti) = .true.
-        END IF
-      END DO
-
-      ! Also define border triangles as grounding line
-      IF (mesh%TriBI( ti) > 0) THEN
-        ! Omit triangle from floating mask. Adjust for no slip conditions
-        laddie%mask_b( ti) = .false.
-        ! Define as grounding line triangle
-        laddie%mask_gl_b( ti) = .true.
-      END IF
-
-      ! For non-grounding line triangles:
-      IF (.NOT. laddie%mask_gl_b( ti)) THEN
-        ! Define calving front triangles
-        DO i = 1, 3
-          vi = mesh%Tri( ti, i)
-          ! Check if any vertex is icefree ocean 
-          IF (mask_oc_a_tot( vi)) THEN
-            ! Define as calving front triangle
-            laddie%mask_cf_b( ti) = .true.
-          END IF
-        END DO
-      END IF
-
-      ! Define ocean triangles
-      no = 0 ! Number of ice free ocean vertices
-      DO i = 1, 3
-        vi = mesh%Tri( ti, i)
-        ! Check if vertex is icefree ocean
-        IF (mask_oc_a_tot( vi)) THEN
-          no = no + 1
-        END IF
-      END DO
-      ! Check whether all vertices are icefree ocean
-      IF (no == 3) THEN
-        ! Define as ocean triangle
-        laddie%mask_oc_b( ti) = .true.
-      END IF
-
-    END DO !ti = mesh%ti1, mesh%ti2
+    CALL update_laddie_masks( mesh, ice, laddie)
 
     ! Extrapolate new cells
     ! TODO
@@ -500,6 +411,116 @@ CONTAINS
     CALL finalise_routine( routine_name)
 
   END SUBROUTINE update_diffusive_terms
+
+  SUBROUTINE update_laddie_masks( mesh, ice, laddie)
+    ! Update bunch of masks for laddie
+
+    ! In- and output variables
+
+    TYPE(type_mesh),                        INTENT(IN)    :: mesh
+    TYPE(type_ice_model),                   INTENT(IN)    :: ice
+    TYPE(type_laddie_model),                INTENT(INOUT) :: laddie
+
+    ! Local variables:
+    CHARACTER(LEN=256), PARAMETER                         :: routine_name = 'update_laddie_masks'
+    INTEGER                                               :: vi, ti, i, no
+    LOGICAL, DIMENSION(mesh%nV)                           :: mask_a_tot, mask_gr_a_tot, mask_oc_a_tot
+
+    ! Add routine to path
+    CALL init_routine( routine_name)
+
+    ! Mask on a grid
+    DO vi = mesh%vi1, mesh%vi2
+      ! Check whether vertex on border
+      IF (mesh%VBI( vi) > 0) THEN
+        laddie%mask_a( vi)    = .false.
+        laddie%mask_gr_a( vi) = .true.
+      ELSE IF (ice%Hib( vi) - ice%Hb( vi) < 2*C%laddie_thickness_minimum) THEN
+        laddie%mask_a( vi)    = .false.
+        laddie%mask_gr_a( vi) = .true.
+      ELSE
+        ! Inherit regular masks
+        laddie%mask_a( vi)    = ice%mask_floating_ice( vi)
+        laddie%mask_gr_a( vi) = ice%mask_grounded_ice( vi) .OR. ice%mask_icefree_land( vi)
+        laddie%mask_oc_a( vi) = ice%mask_icefree_ocean( vi)
+      END IF
+    END DO
+      
+    ! Mask on b grid
+    CALL gather_to_all_logical_1D( laddie%mask_a, mask_a_tot)
+    CALL gather_to_all_logical_1D( laddie%mask_gr_a, mask_gr_a_tot)
+    CALL gather_to_all_logical_1D( laddie%mask_oc_a, mask_oc_a_tot)
+
+    DO ti = mesh%ti1, mesh%ti2
+      ! Initialise as false to overwrite previous mask
+      laddie%mask_b( ti)    = .false.
+      laddie%mask_gl_b( ti) = .false.
+      laddie%mask_cf_b( ti) = .false.
+      laddie%mask_oc_b( ti) = .false.
+
+      ! Define floating mask if any of the three vertices is floating
+      DO i = 1, 3
+        vi = mesh%Tri( ti, i)
+        IF (mask_a_tot( vi)) THEN
+          ! Set true if any of the three vertices is floating
+          laddie%mask_b( ti) = .true.
+        END IF
+      END DO
+
+      ! Define grounding line triangles 
+      DO i = 1, 3
+        vi = mesh%Tri( ti, i)
+        ! Check if any connected vertex is grounded
+        IF (mask_gr_a_tot( vi)) THEN
+          ! Omit triangle from floating mask. Adjust for no slip conditions
+          laddie%mask_b( ti) = .false.
+          ! Define as grounding line triangle
+          laddie%mask_gl_b( ti) = .true.
+        END IF
+      END DO
+
+      ! Also define border triangles as grounding line
+      IF (mesh%TriBI( ti) > 0) THEN
+        ! Omit triangle from floating mask. Adjust for no slip conditions
+        laddie%mask_b( ti) = .false.
+        ! Define as grounding line triangle
+        laddie%mask_gl_b( ti) = .true.
+      END IF
+
+      ! For non-grounding line triangles:
+      IF (.NOT. laddie%mask_gl_b( ti)) THEN
+        ! Define calving front triangles
+        DO i = 1, 3
+          vi = mesh%Tri( ti, i)
+          ! Check if any vertex is icefree ocean 
+          IF (mask_oc_a_tot( vi)) THEN
+            ! Define as calving front triangle
+            laddie%mask_cf_b( ti) = .true.
+          END IF
+        END DO
+      END IF
+
+      ! Define ocean triangles
+      no = 0 ! Number of ice free ocean vertices
+      DO i = 1, 3
+        vi = mesh%Tri( ti, i)
+        ! Check if vertex is icefree ocean
+        IF (mask_oc_a_tot( vi)) THEN
+          no = no + 1
+        END IF
+      END DO
+      ! Check whether all vertices are icefree ocean
+      IF (no == 3) THEN
+        ! Define as ocean triangle
+        laddie%mask_oc_b( ti) = .true.
+      END IF
+
+    END DO !ti = mesh%ti1, mesh%ti2
+
+    ! Finalise routine path
+    CALL finalise_routine( routine_name)
+
+  END SUBROUTINE update_laddie_masks
 
 END MODULE laddie_main
 
