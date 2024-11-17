c0b194a78ca0ba65f6f48765d0dda8e89ca10e57
TijnBerends <c.j.berends@uu.nl>
2024-11-17 18:35:21 +0100

    Add static Halfar dome SMB
    Hurray

--- src/surface_mass_balance/SMB_idealised.f90
+++ src/surface_mass_balance/SMB_idealised.f90
@@ -38,6 +38,8 @@ contains
       call crash('unknown choice_SMB_model_idealised "' // TRIM( C%choice_SMB_model_idealised) // '"')
     case ('EISMINT1_A', 'EISMINT1_B', 'EISMINT1_C', 'EISMINT1_D', 'EISMINT1_E', 'EISMINT1_F')
       call run_SMB_model_idealised_EISMINT1( mesh, SMB, time)
+    case ('Halfar_static')
+      call run_SMB_model_idealised_Halfar_static( mesh, SMB)
     end select
 
     ! Finalise routine path
@@ -162,6 +164,67 @@ contains
 
   end subroutine run_SMB_model_idealised_EISMINT1
 
+  subroutine run_SMB_model_idealised_Halfar_static( mesh, SMB)
+    ! Calculate the surface mass balance
+
+    ! In/output variables
+    type(type_mesh),      intent(in)    :: mesh
+    type(type_SMB_model), intent(inout) :: SMB
+
+    ! Local variables:
+    character(len=256), parameter :: routine_name = 'run_SMB_model_idealised_Halfar_static'
+    integer                       :: vi
+    real(dp)                      :: n, A, Gamma, R0, H0, t0, p1, p2, p3, p4
+    real(dp)                      :: x, y, r
+    real(dp)                      :: f1, f2, f3
+    real(dp)                      :: G
+    real(dp)                      :: df1_dt, df2_dt, dG_dt, dH_dt
+    real(dp), parameter           :: t = 0._dp
+
+    ! Add routine to path
+    call init_routine( routine_name)
+
+    n  = C%Glens_flow_law_exponent
+    A  = C%uniform_Glens_flow_factor
+    R0 = C%refgeo_idealised_Halfar_R0
+    H0 = C%refgeo_idealised_Halfar_H0
+
+    Gamma = (2._dp / 5._dp) * (A / sec_per_year) * (ice_density * grav)**n
+    t0 = 1._dp / ((5._dp * n + 3._dp) * Gamma) * ((2._dp * n + 1._dp)/(n + 1._dp))**n * (R0**(n + 1._dp))/(H0**(2._dp * n  + 1))
+
+    p1 = -2._dp / (5._dp * n + 3._dp)
+    p2 = -1._dp / (5._dp * n + 3._dp)
+    p3 = (n + 1._dp) / n
+    p4 = n / (2._dp * n + 1._dp)
+
+    do vi = mesh%vi1, mesh%vi2
+
+      x = mesh%V( vi,1)
+      y = mesh%V( vi,2)
+      r = sqrt( x**2 + y**2)
+
+      f1 = ((t0 + t) / t0)**p1
+      f2 = ((t0 + t) / t0)**p2
+      f3 = r / R0
+
+      G = 1._dp - (f2 * f3)**p3
+
+      df1_dt = p1 / t0 * ((t0 + t) / t0 )**(p1 - 1._dp)
+      df2_dt = p2 / t0 * ((t0 + t) / t0 )**(p2 - 1._dp)
+
+      dG_dt  =  -p3 * f2**(p3 - 1._dp) * df2_dt * f3**p3
+
+      dH_dt = H0 * (df1_dt * G**p4 + f1 * p4 * G**(p4 - 1._dp) * dG_dt)
+
+      SMB%SMB( vi) = -dH_dt
+
+    end do
+
+    ! Finalise routine path
+    call finalise_routine( routine_name)
+
+  end subroutine run_SMB_model_idealised_Halfar_static
+
   subroutine initialise_SMB_model_idealised( mesh, SMB)
     ! Initialise the SMB model
     !
@@ -186,6 +249,8 @@ contains
     case default
       call crash('unknown choice_SMB_model_idealised "' // TRIM( C%choice_SMB_model_idealised) // '"')
     case ('EISMINT1_A', 'EISMINT1_B', 'EISMINT1_C', 'EISMINT1_D', 'EISMINT1_E', 'EISMINT1_F')
+      ! No need to do anything
+    case ('Halfar_static')
       ! No need to do anything
     end select
 
