3bbdbfdeea582f5139838d690549cf139721c8a5
TijnBerends <c.j.berends@uu.nl>
2024-11-17 18:34:56 +0100

    Decapitalise Fortran intrinsics
    Cleaner

--- src/surface_mass_balance/SMB_idealised.f90
+++ src/surface_mass_balance/SMB_idealised.f90
@@ -1,89 +1,78 @@
-MODULE SMB_idealised
+module SMB_idealised
 
   ! Idealised SMB models
 
-! ===== Preamble =====
-! ====================
+  use precisions, only: dp
+  use mpi_basic, only: par, sync
+  use control_resources_and_error_messaging, only: crash, init_routine, finalise_routine, colour_string
+  use model_configuration, only: C
+  use parameters
+  use mesh_types, only: type_mesh
+  use ice_model_types, only: type_ice_model
+  use SMB_model_types, only: type_SMB_model
 
-  USE precisions                                             , ONLY: dp
-  USE mpi_basic                                              , ONLY: par, sync
-  USE control_resources_and_error_messaging                  , ONLY: crash, init_routine, finalise_routine, colour_string
-  USE model_configuration                                    , ONLY: C
-  USE parameters
-  USE mesh_types                                             , ONLY: type_mesh
-  USE ice_model_types                                        , ONLY: type_ice_model
-  USE SMB_model_types                                        , ONLY: type_SMB_model
+  implicit none
 
-  IMPLICIT NONE
+contains
 
-CONTAINS
-
-! ===== Main routines =====
-! =========================
-
-  SUBROUTINE run_SMB_model_idealised( mesh, ice, SMB, time)
+  subroutine run_SMB_model_idealised( mesh, ice, SMB, time)
     ! Calculate the surface mass balance
     !
-    ! Use an idealised SMB scheme
-
-    IMPLICIT NONE
+    ! use an idealised SMB scheme
 
     ! In/output variables:
-    TYPE(type_mesh),                        INTENT(IN)    :: mesh
-    TYPE(type_ice_model),                   INTENT(IN)    :: ice
-    TYPE(type_SMB_model),                   INTENT(INOUT) :: SMB
-    REAL(dp),                               INTENT(IN)    :: time
+    type(type_mesh),      intent(in)    :: mesh
+    type(type_ice_model), intent(in)    :: ice
+    type(type_SMB_model), intent(inout) :: SMB
+    real(dp),             intent(in)    :: time
 
     ! Local variables:
-    CHARACTER(LEN=256), PARAMETER                         :: routine_name = 'run_SMB_model_idealised'
+    character(len=256), parameter :: routine_name = 'run_SMB_model_idealised'
 
     ! Add routine to path
-    CALL init_routine( routine_name)
+    call init_routine( routine_name)
 
     ! Run the chosen idealised SMB model
-    IF     (C%choice_SMB_model_idealised == 'EISMINT1_A' .OR. &
-            C%choice_SMB_model_idealised == 'EISMINT1_B' .OR. &
-            C%choice_SMB_model_idealised == 'EISMINT1_C' .OR. &
-            C%choice_SMB_model_idealised == 'EISMINT1_D' .OR. &
-            C%choice_SMB_model_idealised == 'EISMINT1_E' .OR. &
-            C%choice_SMB_model_idealised == 'EISMINT1_F') THEN
-      CALL run_SMB_model_idealised_EISMINT1( mesh, SMB, time)
-    ELSE
-      CALL crash('unknown choice_SMB_model_idealised "' // TRIM( C%choice_SMB_model_idealised) // '"')
-    END IF
+    select case (C%choice_SMB_model_idealised)
+    case default
+      call crash('unknown choice_SMB_model_idealised "' // TRIM( C%choice_SMB_model_idealised) // '"')
+    case ('EISMINT1_A', 'EISMINT1_B', 'EISMINT1_C', 'EISMINT1_D', 'EISMINT1_E', 'EISMINT1_F')
+      call run_SMB_model_idealised_EISMINT1( mesh, SMB, time)
+    end select
 
     ! Finalise routine path
-    CALL finalise_routine( routine_name)
+    call finalise_routine( routine_name)
 
-  END SUBROUTINE run_SMB_model_idealised
+  end subroutine run_SMB_model_idealised
 
-  SUBROUTINE run_SMB_model_idealised_EISMINT1( mesh, SMB, time)
+  subroutine run_SMB_model_idealised_EISMINT1( mesh, SMB, time)
     ! Calculate the surface mass balance
     !
-    ! Use an idealised SMB scheme
+    ! use an idealised SMB scheme
     !
     ! SMB for the EISMINT1 experiments (Huybrechts et al., 1996)
 
-    IMPLICIT NONE
-
     ! In/output variables
-    TYPE(type_mesh),                     INTENT(IN)    :: mesh
-    TYPE(type_SMB_model),                INTENT(INOUT) :: SMB
-    REAL(dp),                            INTENT(IN)    :: time
+    type(type_mesh),      intent(in)    :: mesh
+    type(type_SMB_model), intent(inout) :: SMB
+    real(dp),             intent(in)    :: time
 
     ! Local variables:
-    CHARACTER(LEN=256), PARAMETER                      :: routine_name = 'run_SMB_model_idealised_EISMINT1'
-    INTEGER                                            :: vi
-    REAL(dp), PARAMETER                                :: s = 1E-2_dp    ! Mass balance change with distance from divide [m yr^-1 km^-1]
-    REAL(dp)                                           :: x, y, d, R_el, T
+    character(len=256), parameter :: routine_name = 'run_SMB_model_idealised_EISMINT1'
+    integer                       :: vi
+    real(dp), parameter           :: s = 1E-2_dp    ! Mass balance change with distance from divide [m yr^-1 km^-1]
+    real(dp)                      :: x, y, d, R_el, T
 
     ! Add routine to path
-    CALL init_routine( routine_name)
+    call init_routine( routine_name)
 
-    IF     (C%choice_SMB_model_idealised == 'EISMINT1_A' ) THEN
+    select case (C%choice_SMB_model_idealised)
+    case default
+      call crash('unknown choice_SMB_model_idealised "' // TRIM( C%choice_SMB_model_idealised) // '"')
+    case ('EISMINT1_A')
       ! Moving margin, no cyclicity
 
-      DO vi = mesh%vi1, mesh%vi2
+      do vi = mesh%vi1, mesh%vi2
 
         ! Calculate distance from ice divide (for moving margin experiments, use Euclidean distance)
         x = mesh%V( vi,1)
@@ -96,14 +85,14 @@ CONTAINS
         ! Calculate SMB (Huybrechts et al., Eq. 10)
         SMB%SMB( vi) = MIN( 0.5_dp, s * (R_el - d))
 
-      END DO
+      end do
 
-    ELSEIF (C%choice_SMB_model_idealised == 'EISMINT1_B' ) THEN
+    case ('EISMINT1_B')
       ! Moving margin, 20,000-yr cyclicity
 
       T = 20E3_dp
 
-      DO vi = mesh%vi1, mesh%vi2
+      do vi = mesh%vi1, mesh%vi2
 
         ! Calculate distance from ice divide (for moving margin experiments, use Euclidean distance)
         x = mesh%V( vi,1)
@@ -116,14 +105,14 @@ CONTAINS
         ! Calculate SMB (Huybrechts et al., Eq. 10)
         SMB%SMB( vi) = MIN( 0.5_dp, s * (R_el - d))
 
-      END DO
+      end do
 
-    ELSEIF (C%choice_SMB_model_idealised == 'EISMINT1_C' ) THEN
+    case ('EISMINT1_C')
       ! Moving margin, 40,000-yr cyclicity
 
       T = 40E3_dp
 
-      DO vi = mesh%vi1, mesh%vi2
+      do vi = mesh%vi1, mesh%vi2
 
         ! Calculate distance from ice divide (for moving margin experiments, use Euclidean distance)
         x = mesh%V( vi,1)
@@ -136,81 +125,73 @@ CONTAINS
         ! Calculate SMB (Huybrechts et al., Eq. 10)
         SMB%SMB( vi) = MIN( 0.5_dp, s * (R_el - d))
 
-      END DO
+      end do
 
-    ELSEIF (C%choice_SMB_model_idealised == 'EISMINT1_D' ) THEN
+    case ('EISMINT1_D')
       ! Fixed margin, no cyclicity
 
       ! Calculate SMB (Huybrechts et al., Eq. 8)
-      DO vi = mesh%vi1, mesh%vi2
+      do vi = mesh%vi1, mesh%vi2
         SMB%SMB( vi) = 0.3_dp
-      END DO
+      end do
 
-    ELSEIF (C%choice_SMB_model_idealised == 'EISMINT1_E' ) THEN
+    case ('EISMINT1_E')
       ! Fixed margin, 20,000-yr cyclicity
 
       T = 20E3_dp
 
       ! Calculate SMB (Huybrechts et al., Eq. 13)
-      DO vi = mesh%vi1, mesh%vi2
+      do vi = mesh%vi1, mesh%vi2
         SMB%SMB( vi) = 0.3_dp + 0.2_dp * SIN( 2._dp * pi * time / T)
-      END DO
+      end do
 
-    ELSEIF (C%choice_SMB_model_idealised == 'EISMINT1_F' ) THEN
+    case ('EISMINT1_F')
       ! Fixed margin, 40,000-yr cyclicity
 
       T = 40E3_dp
 
       ! Calculate SMB (Huybrechts et al., Eq. 13)
-      DO vi = mesh%vi1, mesh%vi2
+      do vi = mesh%vi1, mesh%vi2
         SMB%SMB( vi) = 0.3_dp + 0.2_dp * SIN( 2._dp * pi * time / T)
-      END DO
+      end do
 
-    ELSE
-      CALL crash('unknown choice_SMB_model_idealised "' // TRIM( C%choice_SMB_model_idealised) // '"!')
-    END IF
+    end select
 
     ! Finalise routine path
-    CALL finalise_routine( routine_name)
+    call finalise_routine( routine_name)
 
-    END SUBROUTINE run_SMB_model_idealised_EISMINT1
+  end subroutine run_SMB_model_idealised_EISMINT1
 
-  SUBROUTINE initialise_SMB_model_idealised( mesh, SMB)
+  subroutine initialise_SMB_model_idealised( mesh, SMB)
     ! Initialise the SMB model
     !
-    ! Use an idealised SMB scheme
-
-    IMPLICIT NONE
+    ! use an idealised SMB scheme
 
     ! In- and output variables
-    TYPE(type_mesh),                        INTENT(IN)    :: mesh
-    TYPE(type_SMB_model),                   INTENT(INOUT) :: SMB
+    type(type_mesh),      intent(in)    :: mesh
+    type(type_SMB_model), intent(inout) :: SMB
 
     ! Local variables:
-    CHARACTER(LEN=256), PARAMETER                         :: routine_name = 'initialise_SMB_model_idealised'
+    character(len=256), parameter :: routine_name = 'initialise_SMB_model_idealised'
 
     ! Add routine to path
-    CALL init_routine( routine_name)
+    call init_routine( routine_name)
 
     ! Print to terminal
-    IF (par%master)  WRITE(*,"(A)") '   Initialising idealised SMB model "' // &
-      colour_string( TRIM( C%choice_SMB_model_idealised),'light blue') // '"...'
+    if (par%master) write(*,"(a)") '   Initialising idealised SMB model "' // &
+      colour_string( trim( C%choice_SMB_model_idealised),'light blue') // '"...'
 
     ! Run the chosen idealised SMB model
-    IF (C%choice_SMB_model_idealised == 'EISMINT1_A' .OR. &
-        C%choice_SMB_model_idealised == 'EISMINT1_B' .OR. &
-        C%choice_SMB_model_idealised == 'EISMINT1_C' .OR. &
-        C%choice_SMB_model_idealised == 'EISMINT1_D' .OR. &
-        C%choice_SMB_model_idealised == 'EISMINT1_E' .OR. &
-        C%choice_SMB_model_idealised == 'EISMINT1_F') THEN
+    select case (C%choice_SMB_model_idealised)
+    case default
+      call crash('unknown choice_SMB_model_idealised "' // TRIM( C%choice_SMB_model_idealised) // '"')
+    case ('EISMINT1_A', 'EISMINT1_B', 'EISMINT1_C', 'EISMINT1_D', 'EISMINT1_E', 'EISMINT1_F')
       ! No need to do anything
-    ELSE
-      CALL crash('unknown choice_SMB_model_idealised "' // TRIM( C%choice_SMB_model_idealised) // '"')
-    END IF
+    end select
 
     ! Finalise routine path
-    CALL finalise_routine( routine_name)
+    call finalise_routine( routine_name)
 
-  END SUBROUTINE initialise_SMB_model_idealised
+  end subroutine initialise_SMB_model_idealised
 
-END MODULE SMB_idealised
+end module SMB_idealised
