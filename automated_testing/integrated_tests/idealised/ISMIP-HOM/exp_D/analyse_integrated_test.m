64773592dc658b2e1bd3ea4e83aebac391b5bd8a
TijnBerends <c.j.berends@uu.nl>
2024-12-15 13:12:37 +0100

    Add ISMIP-HOM experiment D
    Hurray

---
+++ automated_testing/integrated_tests/idealised/ISMIP-HOM/exp_D/analyse_integrated_test.m
@@ -0,0 +1,99 @@
+function analyse_integrated_test( varargin)
+% Analyse the results of the ISMIP-HOM experiment D integrated test
+
+disp('Analysing integrated test idealised/ISMIP-HOM/experiment_D...')
+disp('')
+
+%%
+
+% In the GitHub Workflow, provide the component test results folders as
+% input; but retain the option of running without input (i.e. as a script)
+% locally, with user-defined folders
+
+input_args = varargin;
+if isempty( input_args)
+  % Assume this is a local run
+
+  foldername_automated_testing = '/Users/Beren017/Documents/GitHub/UFEMISM2.0/automated_testing';
+  addpath('/Users/Beren017/Documents/GitHub/UFEMISM2.0/tools/matlab/')
+  foldername_ISMIP_HOM = '/Users/Beren017/Documents/GitHub/data/model_ensembles/ISMIP-HOM';
+
+elseif isscalar( input_args)
+  % Assume this is a GitHub Workflow run
+
+  foldername_automated_testing = varargin{1};
+  addpath('tools/matlab/')
+  foldername_ISMIP_HOM = 'data/model_ensembles/ISMIP-HOM';
+
+else
+  error('need either foldername_automated_testing, or nothing as input!')
+end
+
+addpath([foldername_automated_testing '/scoreboard/scripts'])
+addpath(foldername_ISMIP_HOM)
+
+foldername_test = [foldername_automated_testing '/integrated_tests/idealised/' ...
+  'ISMIP-HOM/exp_D'];
+
+%%
+
+% Read and process the Pattyn et al. (2008) ISMIP-HOM model ensemble
+[HO,FS] = process_ISMIP_HOM_ensemble_experiment_D( [foldername_ISMIP_HOM '/ismip_all']);
+
+% Read and process UFEMISM output
+Ls = [5,10,20,40,80,160];
+approxs = {'SIASSA','DIVA','BPA'};
+
+for Li = 1:6
+  L = Ls( Li);
+  
+  if L<10
+    ex = ['L00' num2str(L)];
+  elseif L<100
+    ex = ['L0'  num2str(L)];
+  else
+    ex = ['L'   num2str(L)];
+  end
+
+  for approxi = 1:3
+    approx = approxs{ approxi};
+
+    foldername = [foldername_test '/results_ISMIP_HOM_D_' num2str(L) '_' approx];
+    filename = [foldername '/main_output_ANT_00001.nc'];
+    mesh = read_mesh_from_file( filename);
+
+    % Calculate velocity transect
+    xt = HO.(ex).x * L*1e3;
+    yt = xt*0 + L*1e3 / 4;
+    A = calc_transect_matrix( mesh, xt, yt);
+    u_surf = ncread( filename,'u_surf');
+    u_surf = A * u_surf;
+
+    % Calculate error with respect to HO model ensemble
+    err_pos = max( 0, u_surf - HO.(ex).u_max);
+    err_neg = max( 0, HO.(ex).u_min - u_surf);
+    err_abs = max( err_pos, err_neg);
+    RMSE_u_surf = sqrt( mean( err_abs.^2));
+  
+    % Write to scoreboard file
+    test_path = ['integrated_tests/idealised/ISMIP_HOM/experiment_D/' approx];
+    test_name = ex;
+    write_to_scoreboard_file( RMSE_u_surf)
+
+  end
+end
+
+  function write_to_scoreboard_file( RMSE_u_surf)
+
+    % Set up a scoreboard results structure
+    single_run = initialise_single_test_run( test_name, test_path);
+  
+    % Add cost functions to results structure
+    single_run = add_cost_function_to_single_run( single_run, ...
+      'rmse', 'sqrt( mean( (u_surf - u_ensemble).^2 ))', RMSE_u_surf);
+    
+    % Write to scoreboard file
+    write_scoreboard_file( foldername_automated_testing, single_run);
+  
+  end
+end
---
+++ automated_testing/integrated_tests/idealised/ISMIP-HOM/exp_D/plot_ISMIP_HOM_D.m
@@ -0,0 +1,169 @@
+clc
+clear all
+close all
+
+% Read Pattyn et al. (2008) ensemble results
+foldername_ensemble = '/Users/Beren017/Documents/GitHub/data/model_ensembles/ISMIP-HOM/ismip_all';
+[HO,FS] = process_ISMIP_HOM_ensemble_experiment_D( foldername_ensemble);
+
+%% Set up GUI
+
+close all
+
+c_HO     = [0.1 ,0.5 ,0.2];
+c_FS     = [0.2 ,0.5 ,1.0];
+c_SIASSA = [1.0, 0.0, 0.0];
+c_DIVA   = [0.9, 0.7, 0.0];
+c_BPA    = [0.5, 0.2, 0.7];
+
+wa = 250;
+ha = 200;
+
+margins_hor = [90,50,50,25];
+margins_ver = [50,50,90];
+
+H = setup_multipanel_figure( wa, ha, margins_hor, margins_ver);
+
+set( H.Ax{ 1,1},'xtick',0:0.2:1,'xticklabels','');
+set( H.Ax{ 1,2},'xtick',0:0.2:1,'xticklabels','');
+set( H.Ax{ 1,3},'xtick',0:0.2:1,'xticklabels','');
+set( H.Ax{ 2,1},'xtick',0:0.2:1);
+set( H.Ax{ 2,2},'xtick',0:0.2:1);
+set( H.Ax{ 2,3},'xtick',0:0.2:1);
+
+xlabel( H.Ax{ 2,1},'x / L');
+xlabel( H.Ax{ 2,2},'x / L');
+xlabel( H.Ax{ 2,3},'x / L');
+
+ylabel( H.Ax{ 1,1},'Surface x-velocity (m/yr)');
+ylabel( H.Ax{ 2,1},'Surface x-velocity (m/yr)');
+
+title( H.Ax{ 1,1},'5 km');
+title( H.Ax{ 1,2},'10 km');
+title( H.Ax{ 1,3},'20 km');
+title( H.Ax{ 2,1},'40 km');
+title( H.Ax{ 2,2},'80 km');
+title( H.Ax{ 2,3},'160 km');
+
+% Empty objects for legend
+patch( 'parent',H.Ax{ 2,3},'vertices',[],'faces',[],'facecolor',c_HO,...
+  'edgecolor','none','facealpha',0.3);
+patch( 'parent',H.Ax{ 2,3},'vertices',[],'faces',[],'facecolor',c_FS,...
+  'edgecolor','none','facealpha',0.7);
+% line( 'parent',H.Ax{ 2,3},'xdata',[],'ydata',[],'color',c_HO,'linewidth',2);
+% line( 'parent',H.Ax{ 2,3},'xdata',[],'ydata',[],'color',c_FS,'linewidth',2);
+line( 'parent',H.Ax{ 2,3},'xdata',[],'ydata',[],'color',c_SIASSA,'linewidth',2);
+line( 'parent',H.Ax{ 2,3},'xdata',[],'ydata',[],'color',c_DIVA  ,'linewidth',2);
+line( 'parent',H.Ax{ 2,3},'xdata',[],'ydata',[],'color',c_BPA   ,'linewidth',2);
+
+set( H.Ax{ 1,1},'ylim',[0,20]);
+set( H.Ax{ 1,2},'ylim',[0,24]);
+set( H.Ax{ 1,3},'ylim',[0,30]);
+set( H.Ax{ 2,1},'ylim',[0,60]);
+set( H.Ax{ 2,2},'ylim',[0,200]);
+set( H.Ax{ 2,3},'ylim',[0,400]);
+
+for Li = 1: 6
+
+  if Li==1
+    ex = 'L005';
+    ax = H.Ax{ 1,1};
+  elseif Li == 2
+    ex = 'L010';
+    ax = H.Ax{ 1,2};
+  elseif Li == 3
+    ex = 'L020';
+    ax = H.Ax{ 1,3};
+  elseif Li == 4
+    ex = 'L040';
+    ax = H.Ax{ 2,1};
+  elseif Li == 5
+    ex = 'L080';
+    ax = H.Ax{ 2,2};
+  elseif Li == 6
+    ex = 'L160';
+    ax = H.Ax{ 2,3};
+  end
+
+  % Ensemble ranges
+  xdata = [HO.(ex).x    ; flipud( HO.(ex).x    )];
+  ydata = [HO.(ex).u_min; flipud( HO.(ex).u_max)];
+  patch('parent',ax,'xdata',xdata,'ydata',ydata,'facecolor',c_HO,...
+    'edgecolor','none','facealpha',0.3);
+
+  xdata = [FS.(ex).x    ; flipud( FS.(ex).x    )];
+  ydata = [FS.(ex).u_min; flipud( FS.(ex).u_max)];
+  patch('parent',ax,'xdata',xdata,'ydata',ydata,'facecolor',c_FS,...
+    'edgecolor','none','facealpha',0.7);
+
+  % % Ensemble means
+  % line('parent',ax,'xdata',HO.(ex).x,'ydata',HO.(ex).u_av,...
+  %   'color',c_HO,'linewidth',2)
+  % line('parent',ax,'xdata',FS.(ex).x,'ydata',FS.(ex).u_av,...
+  %   'color',c_FS,'linewidth',2)
+end
+
+%% Read and plot UFEMISM results
+
+
+Ls = [5,10,20,40,80,160];
+approxs = {'SIASSA','DIVA','BPA'};
+
+for Li = 1:6
+  L = Ls( Li);
+  
+  if L<10
+    ex = ['L00' num2str(L)];
+  elseif L<100
+    ex = ['L0'  num2str(L)];
+  else
+    ex = ['L'   num2str(L)];
+  end
+
+  if     L == 5
+    ax = H.Ax{ 1,1};
+  elseif L == 10
+    ax = H.Ax{ 1,2};
+  elseif L == 20
+    ax = H.Ax{ 1,3};
+  elseif L == 40
+    ax = H.Ax{ 2,1};
+  elseif L == 80
+    ax = H.Ax{ 2,2};
+  elseif L == 160
+    ax = H.Ax{ 2,3};
+  end
+
+  for approxi = 1:3
+    approx = approxs{ approxi};
+
+    if strcmpi( approx,'SIASSA')
+      colour = c_SIASSA;
+    elseif strcmpi( approx,'DIVA')
+      colour = c_DIVA;
+    elseif strcmpi( approx,'BPA')
+      colour = c_BPA;
+    end
+
+    foldername = ['results_ISMIP_HOM_D_' num2str(L) '_' approx];
+    filename = [foldername '/main_output_ANT_00001.nc'];
+    mesh = read_mesh_from_file( filename);
+
+    % Calculate velocity transect
+    xt = HO.(ex).x * L*1e3;
+    yt = xt*0 + L*1e3 / 4;
+    A = calc_transect_matrix( mesh, xt, yt);
+    u_surf = ncread( filename,'u_surf');
+    u_surf = A * u_surf;
+
+    % Plot results
+    line('parent',ax,'xdata',HO.(ex).x,'ydata',u_surf,'color',colour,...
+      'linewidth',2)
+    drawnow('update')
+
+  end
+end
+
+%% Legend
+
+legend( H.Ax{ 2,3},'Full-Stokes','Higher-Order','SIA/SSA','DIVA','BPA','location','northwest')
