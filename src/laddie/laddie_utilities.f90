0c505c336433fa7d5cfdabcf4fe8a5eace3ef3d6
erwinlambert <45197956+erwinlambert@users.noreply.github.com>
2025-01-07 14:53:00 +0100

    clean up allocation in laddie_utils

--- src/laddie/laddie_utilities.f90
+++ src/laddie/laddie_utilities.f90
@@ -166,132 +166,75 @@ CONTAINS
     CALL init_routine( routine_name)
 
     ! Thickness
-    ALLOCATE( laddie%dH_dt              ( mesh%vi1:mesh%vi2              )) ! [m]             change
-
-    laddie%dH_dt          = 0._dp
+    ALLOCATE( laddie%dH_dt              ( mesh%vi1:mesh%vi2), source=0._dp) ! [m]             change
 
     ! Temperatures
-    ALLOCATE( laddie%T_amb              ( mesh%vi1:mesh%vi2              )) ! [degC]          Temperature layer bottom
-    ALLOCATE( laddie%T_base             ( mesh%vi1:mesh%vi2              )) ! [degC]          Temperature ice shelf base
-    ALLOCATE( laddie%T_freeze           ( mesh%vi1:mesh%vi2              )) ! [degC]          Temperature freezing
-
-    laddie%T_amb          = 0._dp
-    laddie%T_base         = 0._dp
-    laddie%T_freeze       = 0._dp
+    ALLOCATE( laddie%T_amb              ( mesh%vi1:mesh%vi2), source=0._dp) ! [degC]          Temperature layer bottom
+    ALLOCATE( laddie%T_base             ( mesh%vi1:mesh%vi2), source=0._dp) ! [degC]          Temperature ice shelf base
+    ALLOCATE( laddie%T_freeze           ( mesh%vi1:mesh%vi2), source=0._dp) ! [degC]          Temperature freezing
 
     ! Salinities
-    ALLOCATE( laddie%S_amb              ( mesh%vi1:mesh%vi2              )) ! [PSU]           Salinity layer bottom
-    ALLOCATE( laddie%S_base             ( mesh%vi1:mesh%vi2              )) ! [PSU]           Salinity ice shelf base
-
-    laddie%S_amb          = 0._dp
-    laddie%S_base         = 0._dp
+    ALLOCATE( laddie%S_amb              ( mesh%vi1:mesh%vi2), source=0._dp) ! [PSU]           Salinity layer bottom
+    ALLOCATE( laddie%S_base             ( mesh%vi1:mesh%vi2), source=0._dp) ! [PSU]           Salinity ice shelf base
 
     ! Densities and buoyancies
-    ALLOCATE( laddie%rho                ( mesh%vi1:mesh%vi2              )) ! [kg m^-3]       Layer density
-    ALLOCATE( laddie%rho_amb            ( mesh%vi1:mesh%vi2              )) ! [kg m^-3]       Ambient water density
-    ALLOCATE( laddie%drho_amb           ( mesh%vi1:mesh%vi2              )) ! []              Buoyancy at layer bottom
-    ALLOCATE( laddie%Hdrho_amb          ( mesh%vi1:mesh%vi2              )) ! []              Depth-integrated buoyancy at layer bottom
-    ALLOCATE( laddie%Hdrho_amb_b        ( mesh%ti1:mesh%ti2              )) ! []              Depth-integrated buoyancy at layer bottom
-    ALLOCATE( laddie%drho_base          ( mesh%vi1:mesh%vi2              )) ! []              Buoyancy at ice base
-
-    laddie%rho            = 0._dp
-    laddie%rho_amb        = 0._dp
-    laddie%drho_amb       = 0._dp
-    laddie%Hdrho_amb      = 0._dp
-    laddie%Hdrho_amb_b    = 0._dp
-    laddie%drho_base      = 0._dp
+    ALLOCATE( laddie%rho                ( mesh%vi1:mesh%vi2), source=0._dp) ! [kg m^-3]       Layer density
+    ALLOCATE( laddie%rho_amb            ( mesh%vi1:mesh%vi2), source=0._dp) ! [kg m^-3]       Ambient water density
+    ALLOCATE( laddie%drho_amb           ( mesh%vi1:mesh%vi2), source=0._dp) ! []              Buoyancy at layer bottom
+    ALLOCATE( laddie%Hdrho_amb          ( mesh%vi1:mesh%vi2), source=0._dp) ! []              Depth-integrated buoyancy at layer bottom
+    ALLOCATE( laddie%Hdrho_amb_b        ( mesh%ti1:mesh%ti2), source=0._dp) ! []              Depth-integrated buoyancy at layer bottom
+    ALLOCATE( laddie%drho_base          ( mesh%vi1:mesh%vi2), source=0._dp) ! []              Buoyancy at ice base
 
     ! Friction velocity
-    ALLOCATE( laddie%u_star             ( mesh%vi1:mesh%vi2              )) ! [m s^-1]        Friction velocity
-
-    laddie%u_star         = 0._dp
+    ALLOCATE( laddie%u_star             ( mesh%vi1:mesh%vi2), source=0._dp) ! [m s^-1]        Friction velocity
 
     ! Physical parameter fields
-    ALLOCATE( laddie%gamma_T            ( mesh%vi1:mesh%vi2              )) ! []              Turbulent heat exchange coefficient
-    ALLOCATE( laddie%gamma_S            ( mesh%vi1:mesh%vi2              )) ! []              Turbulent salt exchange coefficient
-    ALLOCATE( laddie%A_h                ( mesh%ti1:mesh%ti2              )) ! [m^2 s^-1]      Horizontal laplacian viscosity
-    ALLOCATE( laddie%K_h                ( mesh%vi1:mesh%vi2              )) ! [m^2 s^-1]      Horizontal diffusivity
-
-    laddie%gamma_T        = 0._dp
-    laddie%gamma_S        = 0._dp
-    laddie%A_h            = 0._dp
-    laddie%K_h            = 0._dp
+    ALLOCATE( laddie%gamma_T            ( mesh%vi1:mesh%vi2), source=0._dp) ! []              Turbulent heat exchange coefficient
+    ALLOCATE( laddie%gamma_S            ( mesh%vi1:mesh%vi2), source=0._dp) ! []              Turbulent salt exchange coefficient
+    ALLOCATE( laddie%A_h                ( mesh%ti1:mesh%ti2), source=0._dp) ! [m^2 s^-1]      Horizontal laplacian viscosity
+    ALLOCATE( laddie%K_h                ( mesh%vi1:mesh%vi2), source=0._dp) ! [m^2 s^-1]      Horizontal diffusivity
 
     ! Vertical rates
-    ALLOCATE( laddie%melt               ( mesh%vi1:mesh%vi2              )) ! [m s^-1]        Melting / freezing rate
-    ALLOCATE( laddie%entr               ( mesh%vi1:mesh%vi2              )) ! [m s^-1]        Entrainment
-    ALLOCATE( laddie%entr_dmin          ( mesh%vi1:mesh%vi2              )) ! [m s^-1]        Entrainment for D_min
-    ALLOCATE( laddie%detr               ( mesh%vi1:mesh%vi2              )) ! [m s^-1]        Detrainment
-    ALLOCATE( laddie%entr_tot           ( mesh%vi1:mesh%vi2              )) ! [m s^-1]        Total (net) entrainment
-
-    laddie%melt           = 0._dp
-    laddie%entr           = 0._dp
-    laddie%entr_dmin      = 0._dp
-    laddie%detr           = 0._dp
-    laddie%entr_tot       = 0._dp
+    ALLOCATE( laddie%melt               ( mesh%vi1:mesh%vi2), source=0._dp) ! [m s^-1]        Melting / freezing rate
+    ALLOCATE( laddie%entr               ( mesh%vi1:mesh%vi2), source=0._dp) ! [m s^-1]        Entrainment
+    ALLOCATE( laddie%entr_dmin          ( mesh%vi1:mesh%vi2), source=0._dp) ! [m s^-1]        Entrainment for D_min
+    ALLOCATE( laddie%detr               ( mesh%vi1:mesh%vi2), source=0._dp) ! [m s^-1]        Detrainment
+    ALLOCATE( laddie%entr_tot           ( mesh%vi1:mesh%vi2), source=0._dp) ! [m s^-1]        Total (net) entrainment
 
     ! Horizontal fluxes
-    ALLOCATE( laddie%divQH              ( mesh%vi1:mesh%vi2              )) ! [m^3 s^-1]      Divergence of layer thickness
-    ALLOCATE( laddie%divQU              ( mesh%ti1:mesh%ti2              )) ! [m^4 s^-2]      Divergence of momentum
-    ALLOCATE( laddie%divQV              ( mesh%ti1:mesh%ti2              )) ! [m^4 s^-2]   
-    ALLOCATE( laddie%divQT              ( mesh%vi1:mesh%vi2              )) ! [degC m^3 s^-1] Divergence of heat
-    ALLOCATE( laddie%divQS              ( mesh%vi1:mesh%vi2              )) ! [PSU m^3 s^-1]  Divergence of salt
-
-    laddie%divQH          = 0._dp
-    laddie%divQU          = 0._dp
-    laddie%divQV          = 0._dp
-    laddie%divQT          = 0._dp
-    laddie%divQS          = 0._dp
+    ALLOCATE( laddie%divQH              ( mesh%vi1:mesh%vi2), source=0._dp) ! [m^3 s^-1]      Divergence of layer thickness
+    ALLOCATE( laddie%divQU              ( mesh%ti1:mesh%ti2), source=0._dp) ! [m^4 s^-2]      Divergence of momentum
+    ALLOCATE( laddie%divQV              ( mesh%ti1:mesh%ti2), source=0._dp) ! [m^4 s^-2]   
+    ALLOCATE( laddie%divQT              ( mesh%vi1:mesh%vi2), source=0._dp) ! [degC m^3 s^-1] Divergence of heat
+    ALLOCATE( laddie%divQS              ( mesh%vi1:mesh%vi2), source=0._dp) ! [PSU m^3 s^-1]  Divergence of salt
 
     ! Viscosities
-    ALLOCATE( laddie%viscU              ( mesh%ti1:mesh%ti2              )) ! [m^2 s^-2]      Horizontal viscosity term
-    ALLOCATE( laddie%viscV              ( mesh%ti1:mesh%ti2              )) ! [m^2 s^-2]      
-
-    laddie%viscU          = 0._dp
-    laddie%viscV          = 0._dp
+    ALLOCATE( laddie%viscU              ( mesh%ti1:mesh%ti2), source=0._dp) ! [m^2 s^-2]      Horizontal viscosity term
+    ALLOCATE( laddie%viscV              ( mesh%ti1:mesh%ti2), source=0._dp) ! [m^2 s^-2]      
 
     ! Diffusivities
-    ALLOCATE( laddie%diffT              ( mesh%vi1:mesh%vi2              )) ! [degC m s^-1]   Horizontal diffusivity of heat
-    ALLOCATE( laddie%diffS              ( mesh%vi1:mesh%vi2              )) ! [PSU m s^-1]    Horizontal diffusivity of salt
-
-    laddie%diffT          = 0._dp
-    laddie%diffS          = 0._dp
+    ALLOCATE( laddie%diffT              ( mesh%vi1:mesh%vi2), source=0._dp) ! [degC m s^-1]   Horizontal diffusivity of heat
+    ALLOCATE( laddie%diffS              ( mesh%vi1:mesh%vi2), source=0._dp) ! [PSU m s^-1]    Horizontal diffusivity of salt
 
     ! RHS terms
-    ALLOCATE( laddie%ddrho_amb_dx_b     ( mesh%ti1:mesh%ti2              )) ! [m^-1]          Horizontal derivative of buoyancy
-    ALLOCATE( laddie%ddrho_amb_dy_b     ( mesh%ti1:mesh%ti2              )) ! [m^-1]          
-    ALLOCATE( laddie%dH_dx_b            ( mesh%ti1:mesh%ti2              )) ! [m^-2]          Horizontal derivative of thickness
-    ALLOCATE( laddie%dH_dy_b            ( mesh%ti1:mesh%ti2              )) ! [m^-2]          
-    ALLOCATE( laddie%detr_b             ( mesh%ti1:mesh%ti2              )) ! [m s^-1]        Detrainment on b grid
-
-    laddie%ddrho_amb_dx_b = 0._dp
-    laddie%ddrho_amb_dy_b = 0._dp
-    laddie%dH_dx_b        = 0._dp
-    laddie%dH_dy_b        = 0._dp
-    laddie%detr_b         = 0._dp
+    ALLOCATE( laddie%ddrho_amb_dx_b     ( mesh%ti1:mesh%ti2), source=0._dp) ! [m^-1]          Horizontal derivative of buoyancy
+    ALLOCATE( laddie%ddrho_amb_dy_b     ( mesh%ti1:mesh%ti2), source=0._dp) ! [m^-1]          
+    ALLOCATE( laddie%dH_dx_b            ( mesh%ti1:mesh%ti2), source=0._dp) ! [m^-2]          Horizontal derivative of thickness
+    ALLOCATE( laddie%dH_dy_b            ( mesh%ti1:mesh%ti2), source=0._dp) ! [m^-2]          
+    ALLOCATE( laddie%detr_b             ( mesh%ti1:mesh%ti2), source=0._dp) ! [m s^-1]        Detrainment on b grid
 
     ! Mapped main variables
-    ALLOCATE( laddie%H_c                ( mesh%ei1:mesh%ei2              )) ! [m]             Layer thickness on c grid 
-
-    laddie%H_c            = 0._dp
+    ALLOCATE( laddie%H_c                ( mesh%ei1:mesh%ei2), source=0._dp) ! [m]             Layer thickness on c grid 
 
     ! Masks
 
-    ALLOCATE( laddie%mask_a             ( mesh%vi1:mesh%vi2              )) !                 Mask on a-grid
-    ALLOCATE( laddie%mask_gr_a          ( mesh%vi1:mesh%vi2              )) !                 Grounded mask on a-grid
-    ALLOCATE( laddie%mask_oc_a          ( mesh%vi1:mesh%vi2              )) !                 Icefree ocean mask on a-grid
-    ALLOCATE( laddie%mask_b             ( mesh%ti1:mesh%ti2              )) !                 Mask on b-grid
-    ALLOCATE( laddie%mask_gl_b          ( mesh%ti1:mesh%ti2              )) !                 Grounding line mask on b-grid
-    ALLOCATE( laddie%mask_cf_b          ( mesh%ti1:mesh%ti2              )) !                 Calving front mask on b-grid
-    ALLOCATE( laddie%mask_oc_b          ( mesh%ti1:mesh%ti2              )) !                 Icefree ocean mask on b-grid
-
-    laddie%mask_a         = .false.
-    laddie%mask_gr_a      = .false.
-    laddie%mask_oc_a      = .false.
-    laddie%mask_b         = .false.
-    laddie%mask_gl_b      = .false.
-    laddie%mask_cf_b      = .false.
-    laddie%mask_oc_b      = .false.
+    ALLOCATE( laddie%mask_a             ( mesh%vi1:mesh%vi2), source=.false.) !                 Mask on a-grid
+    ALLOCATE( laddie%mask_gr_a          ( mesh%vi1:mesh%vi2), source=.false.) !                 Grounded mask on a-grid
+    ALLOCATE( laddie%mask_oc_a          ( mesh%vi1:mesh%vi2), source=.false.) !                 Icefree ocean mask on a-grid
+    ALLOCATE( laddie%mask_b             ( mesh%ti1:mesh%ti2), source=.false.) !                 Mask on b-grid
+    ALLOCATE( laddie%mask_gl_b          ( mesh%ti1:mesh%ti2), source=.false.) !                 Grounding line mask on b-grid
+    ALLOCATE( laddie%mask_cf_b          ( mesh%ti1:mesh%ti2), source=.false.) !                 Calving front mask on b-grid
+    ALLOCATE( laddie%mask_oc_b          ( mesh%ti1:mesh%ti2), source=.false.) !                 Icefree ocean mask on b-grid
 
     ! Finalise routine path
     CALL finalise_routine( routine_name)
@@ -311,29 +254,17 @@ CONTAINS
     ! Add routine to path
     CALL init_routine( routine_name)
 
-    ALLOCATE( npx%H                  ( mesh%vi1:mesh%vi2              )) ! [m]             Layer thickness
-    ALLOCATE( npx%H_b                ( mesh%ti1:mesh%ti2              )) ! [m]             Layer thickness on b grid
-    ALLOCATE( npx%H_c                ( mesh%ei1:mesh%ei2              )) ! [m]             Layer thickness on c grid
-    ALLOCATE( npx%U                  ( mesh%ti1:mesh%ti2              )) ! [m s^-1]        2D velocity
-    ALLOCATE( npx%U_a                ( mesh%vi1:mesh%vi2              )) ! [m s^-1]        2D velocity on a grid
-    ALLOCATE( npx%U_c                ( mesh%ei1:mesh%ei2              )) ! [m s^-1]        2D velocity on b grid
-    ALLOCATE( npx%V                  ( mesh%ti1:mesh%ti2              )) ! [m s^-1]  
-    ALLOCATE( npx%V_a                ( mesh%vi1:mesh%vi2              )) ! [m s^-1]  
-    ALLOCATE( npx%V_c                ( mesh%ei1:mesh%ei2              )) ! [m s^-1]  
-    ALLOCATE( npx%T                  ( mesh%vi1:mesh%vi2              )) ! [degC]          Temperature
-    ALLOCATE( npx%S                  ( mesh%vi1:mesh%vi2              )) ! [PSU]           Salinity   
-
-    npx%H              = 0._dp
-    npx%H_b            = 0._dp
-    npx%H_c            = 0._dp
-    npx%U              = 0._dp
-    npx%U_a            = 0._dp
-    npx%U_c            = 0._dp
-    npx%V              = 0._dp
-    npx%V_a            = 0._dp
-    npx%V_c            = 0._dp
-    npx%T              = 0._dp
-    npx%S              = 0._dp
+    ALLOCATE( npx%H                  ( mesh%vi1:mesh%vi2), source=0._dp) ! [m]             Layer thickness
+    ALLOCATE( npx%H_b                ( mesh%ti1:mesh%ti2), source=0._dp) ! [m]             Layer thickness on b grid
+    ALLOCATE( npx%H_c                ( mesh%ei1:mesh%ei2), source=0._dp) ! [m]             Layer thickness on c grid
+    ALLOCATE( npx%U                  ( mesh%ti1:mesh%ti2), source=0._dp) ! [m s^-1]        2D velocity
+    ALLOCATE( npx%U_a                ( mesh%vi1:mesh%vi2), source=0._dp) ! [m s^-1]        2D velocity on a grid
+    ALLOCATE( npx%U_c                ( mesh%ei1:mesh%ei2), source=0._dp) ! [m s^-1]        2D velocity on b grid
+    ALLOCATE( npx%V                  ( mesh%ti1:mesh%ti2), source=0._dp) ! [m s^-1]  
+    ALLOCATE( npx%V_a                ( mesh%vi1:mesh%vi2), source=0._dp) ! [m s^-1]  
+    ALLOCATE( npx%V_c                ( mesh%ei1:mesh%ei2), source=0._dp) ! [m s^-1]  
+    ALLOCATE( npx%T                  ( mesh%vi1:mesh%vi2), source=0._dp) ! [degC]          Temperature
+    ALLOCATE( npx%S                  ( mesh%vi1:mesh%vi2), source=0._dp) ! [PSU]           Salinity   
 
     ! Finalise routine path
     CALL finalise_routine( routine_name)
