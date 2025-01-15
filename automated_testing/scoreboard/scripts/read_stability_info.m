d3e16e148caed821191fc81156eb624c3075e845
TijnBerends <c.j.berends@uu.nl>
2025-01-15 09:43:07 +0100

    Add stability info to ISMIP-HOM cost functions
    Hurray

--- automated_testing/scoreboard/scripts/read_stability_info.m
+++ automated_testing/scoreboard/scripts/read_stability_info.m
@@ -1,7 +1,7 @@
 function stab = read_stability_info( filename, nskip)
 
-stab.n_dt_ice   = length( ncread( filename,'dt_ice'    ,nskip,Inf));
-stab.n_visc_its = sum(    ncread( filename,'n_visc_its',nskip,Inf));
-stab.n_Axb_its  = sum(    ncread( filename,'n_Axb_its' ,nskip,Inf));
+stab.n_dt_ice   = length( ncread( filename,'dt_ice'    ,nskip+1,Inf));
+stab.n_visc_its = sum(    ncread( filename,'n_visc_its',nskip+1,Inf));
+stab.n_Axb_its  = sum(    ncread( filename,'n_Axb_its' ,nskip+1,Inf));
   
 end
