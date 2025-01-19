8a302cd436f819cdb2b723e335086a96041154c3
TijnBerends <c.j.berends@uu.nl>
2025-01-19 08:07:19 +0100

    Move direct scheme to separate module
    Also clean up the use statements in ice_dynamics_main

--- src/ice_dynamics/ice_dynamics_main.f90
+++ src/ice_dynamics/ice_dynamics_main.f90
@@ -2,15 +2,14 @@ module ice_dynamics_main
 
   !< The main ice-dynamical model
 
-  use mpi
   use mpi_basic, only: par
   use precisions, only: dp
-  use control_resources_and_error_messaging, only: init_routine, finalise_routine, crash, warning, colour_string
+  use control_resources_and_error_messaging, only: init_routine, finalise_routine, crash, warning
   use model_configuration, only: C
   use parameters, only: grav, ice_density, seawater_density
   use region_types, only: type_model_region
   use mesh_types, only: type_mesh
-  use ice_model_types, only: type_ice_model, type_ice_pc
+  use ice_model_types, only: type_ice_model
   use reference_geometry_types, only: type_reference_geometry
   use GIA_model_types, only: type_GIA_model
   use SMB_model_types, only: type_SMB_model
@@ -19,33 +18,32 @@ module ice_dynamics_main
   use AMB_model_types, only: type_AMB_model
   use CSR_sparse_matrix_type, only: type_sparse_matrix_CSR_dp
   use remapping_main, only: Atlas
+  use conservation_of_momentum_main, only: solve_stress_balance, remap_velocity_solver, &
+    create_restart_file_ice_velocity, write_to_restart_file_ice_velocity, initialise_velocity_solver
   use conservation_of_mass_main, only: calc_dHi_dt, apply_ice_thickness_BC_explicit, &
     apply_mask_noice_direct
+  use ice_geometry_basics, only: ice_surface_elevation, thickness_above_floatation, &
+    Hi_from_Hb_Hs_and_SL
+  use masks_mod, only: determine_masks, calc_mask_noice
+  use subgrid_ice_margin, only: calc_effective_thickness
+  use zeta_gradients, only: calc_zeta_gradients
+  use subgrid_grounded_fractions_main, only: calc_grounded_fractions
   use mpi_distributed_memory, only: gather_to_all, distribute_from_master
-  use conservation_of_momentum_main, only: solve_stress_balance, remap_velocity_solver, &
-    create_restart_file_ice_velocity, write_to_restart_file_ice_velocity, &
-    initialise_velocity_solver
-  use map_velocities_to_c_grid, only: map_velocities_from_b_to_c_2D
-  use mesh_utilities, only: extrapolate_Gaussian
+  use remapping_main, only: map_from_mesh_to_mesh_2D, map_from_mesh_to_mesh_with_reallocation_2D, &
+    map_from_mesh_to_mesh_with_reallocation_3D
   use reallocate_mod, only: reallocate_bounds
-  use netcdf_io_main
-  use ice_geometry_basics, only: ice_surface_elevation, thickness_above_floatation, Hi_from_Hb_Hs_and_SL
-  use mesh_disc_apply_operators, only: ddx_a_a_2D, ddy_a_a_2D, ddx_a_b_2D, ddy_a_b_2D
+  use petsc_basic, only: mat_petsc2CSR
+  use inversion_utilities, only: initialise_dHi_dt_target, initialise_uabs_surf_target
+  use mesh_disc_apply_operators, only: ddx_a_a_2D, ddy_a_a_2D
+  use bedrock_cumulative_density_functions, only: calc_bedrock_CDFs, initialise_bedrock_CDFs
   use geothermal_heat_flux, only: initialise_geothermal_heat_flux
   use basal_hydrology, only: initialise_basal_hydrology_model
   use bed_roughness, only: initialise_bed_roughness
+  use predictor_corrector_scheme, only: remap_pc_scheme, create_restart_file_pc_scheme, &
+    write_to_restart_file_pc_scheme, initialise_pc_scheme, run_ice_dynamics_model_pc
+  use direct_scheme, only: run_ice_dynamics_model_direct
   use ice_model_memory, only: allocate_ice_model
-  use inversion_utilities, only: MB_inversion, initialise_dHi_dt_target, initialise_uabs_surf_target
-  use ice_thickness_safeties, only: alter_ice_thickness
-  use masks_mod, only: determine_masks, calc_mask_noice
-  use subgrid_grounded_fractions_main, only: calc_grounded_fractions
-  use bedrock_cumulative_density_functions, only: calc_bedrock_CDFs, initialise_bedrock_CDFs
-  use subgrid_ice_margin, only: calc_effective_thickness
-  use zeta_gradients, only: calc_zeta_gradients
-  use inversion_utilities, only: SMB_inversion, BMB_inversion, LMB_inversion
-  use time_step_criteria, only: calc_critical_timestep_SIA, calc_critical_timestep_adv
-  use predictor_corrector_scheme, only: run_ice_dynamics_model_pc, initialise_pc_scheme, &
-    remap_pc_scheme, create_restart_file_pc_scheme, write_to_restart_file_pc_scheme
+  use mesh_disc_apply_operators, only: ddx_a_b_2D, ddy_a_b_2D
 
   implicit none
 
@@ -463,7 +461,7 @@ contains
 
     ! Local variables:
     character(len=1024), parameter                 :: routine_name = 'remap_ice_dynamics_model'
-    integer                                        :: vi, ti, k
+    integer                                        :: vi, ti
     real(dp), dimension(mesh_new%vi1:mesh_new%vi2) :: dHs_dx, dHs_dy
     real(dp)                                       :: Ti_min
 
@@ -1312,7 +1310,7 @@ contains
     real(dp)                                              :: visc_it_norm_dUV_tol_config
     real(dp), dimension(region%mesh%vi1:region%mesh%vi2)  :: Hi_new, dHi_dt_new
     real(dp), dimension(region%mesh%vi1:region%mesh%vi2)  :: SMB_dummy, BMB_dummy, LMB_dummy, AMB_dummy, dHi_dt_target_dummy
-    character(len=256)                                    :: t_years, r_time, r_step, r_adv, t_format
+    character(len=256)                                    :: r_time, r_step, r_adv
     integer                                               :: n_visc_its
     integer                                               :: n_Axb_its
 
@@ -1488,86 +1486,5 @@ contains
     call finalise_routine( routine_name)
 
   end subroutine apply_geometry_relaxation
-
-! ===== Direct scheme =====
-! =========================
-
-  subroutine run_ice_dynamics_model_direct( region, dt_max)
-    !< Calculate a new next modelled ice thickness
-
-    ! In/output variables:
-    type(type_model_region), intent(inout) :: region
-    real(dp),                intent(in   ) :: dt_max
-
-    ! Local variables:
-    character(len=1024), parameter :: routine_name = 'run_ice_dynamics_model_direct'
-    real(dp)                       :: dt_crit_SIA, dt_crit_adv, dt
-    integer                        :: vi
-    integer                        :: n_visc_its
-    integer                        :: n_Axb_its
-
-    ! Add routine to path
-    call init_routine( routine_name)
-
-    ! Safety
-    if (.not. (C%choice_stress_balance_approximation == 'SIA' .OR. &
-               C%choice_stress_balance_approximation == 'SSA' .OR. &
-               C%choice_stress_balance_approximation == 'SIA/SSA')) then
-      call crash('direct timestepping only works for SIA, SSA, or SIA/SSA ice dynamics!')
-    end if
-
-    ! Store previous ice model state
-    region%ice%t_Hi_prev  = region%ice%t_Hi_next
-    region%ice%Hi_prev    = region%ice%Hi_next
-
-    ! Calculate ice velocities
-    call solve_stress_balance( region%mesh, region%ice, region%BMB%BMB, region%name, &
-      n_visc_its, n_Axb_its)
-
-    ! Calculate time step
-
-    ! Start with the maximum allowed time step
-    dt = dt_max
-
-    ! Limit to the SIA critical time step
-    if (C%choice_stress_balance_approximation == 'SIA' .OR. &
-        C%choice_stress_balance_approximation == 'SIA/SSA') then
-      call calc_critical_timestep_SIA( region%mesh, region%ice, dt_crit_SIA)
-      dt = MIN( dt, dt_crit_SIA)
-    end if
-
-    ! Limit to the advective critical time step
-    if (C%choice_stress_balance_approximation == 'SSA' .OR. &
-        C%choice_stress_balance_approximation == 'SIA/SSA') then
-      call calc_critical_timestep_adv( region%mesh, region%ice, dt_crit_adv)
-      dt = MIN( dt, dt_crit_adv)
-    end if
-
-    ! Limit to the smallest allowed time step
-    dt = MAX( C%dt_ice_min, dt)
-
-    ! Calculate thinning rates and predicted geometry
-    call calc_dHi_dt( region%mesh, region%ice%Hi, region%ice%Hb, region%ice%SL, region%ice%u_vav_b, region%ice%v_vav_b, region%SMB%SMB, region%BMB%BMB, region%LMB%LMB, region%AMB%AMB, region%ice%fraction_margin, &
-                      region%ice%mask_noice, dt, region%ice%dHi_dt, region%ice%Hi_next, region%ice%divQ, region%ice%dHi_dt_target)
-
-    ! if so desired, invert/adjust mass balance fluxes to get an equilibrium state
-    call MB_inversion( region%mesh, region%ice, region%refgeo_PD, region%SMB, region%BMB, region%LMB, region%AMB, region%ice%dHi_dt, region%ice%Hi_next, dt, region%time, region%name)
-
-    ! Save the "raw" dynamical dH/dt before any alterations
-    region%ice%dHi_dt_raw = region%ice%dHi_dt
-
-    ! Modify predicted ice thickness if desired
-    call alter_ice_thickness( region%mesh, region%ice, region%ice%Hi_prev, region%ice%Hi_next, region%refgeo_PD, region%time)
-
-    ! Compute residual between the "raw" and final thinning rates
-    region%ice%dHi_dt_residual = region%ice%dHi_dt_raw - (region%ice%Hi_next - region%ice%Hi_prev) / dt
-
-    ! Set next modelled ice thickness timestamp
-    region%ice%t_Hi_next = region%ice%t_Hi_prev + dt
-
-    ! Finalise routine path
-    call finalise_routine( routine_name)
-
-  end subroutine run_ice_dynamics_model_direct
 
 end module ice_dynamics_main
