72b398657ad6f77a74de0853b06660c264b46053
erwinlambert <45197956+erwinlambert@users.noreply.github.com>
2025-01-28 13:12:06 +0100

    add type_grid_orca

--- src/types/grid_types.f90
+++ src/types/grid_types.f90
@@ -63,6 +63,31 @@ module grid_types
 
   end type type_grid_lonlat
 
+  type type_grid_orca
+    ! An ORCA or eORCA grid as used in the NEMO ocean model
+    ! This is a x,y grid with irregular longitude-latitude values attributed to each grid point
+
+    ! Basic properties
+    character(len=256)                      :: name         !           A nice name tag
+    integer                                 :: nx           !           Number of grid cells in the x-direction
+    integer                                 :: ny           !           Number of grid cells in the y-direction
+    integer                                 :: n            !           Total number of grid cells (= nx * ny)
+    real(dp), dimension(:,:  ), allocatable :: lon          ! [degrees east ] Longitude of each grid point
+    real(dp), dimension(:,:  ), allocatable :: lat          ! [degrees north] Latitude  of each grid point
+    real(dp)                                :: xmin         ! []        x and y range covered by the grid
+    real(dp)                                :: xmax         ! []
+    real(dp)                                :: ymin         ! []
+    real(dp)                                :: ymax         ! []
+
+    ! Remapping data
+    real(dp)                                :: tol_dist     ! [m]       Horizontal distance tolerance; points closer together than this are assumed to be identical (typically set to a billionth of linear domain size)
+    integer,  dimension(:,:  ), allocatable :: ij2n, n2ij   !           Conversion tables for grid-form vs. vector-form data
+
+    ! Parallelisation
+    integer                                 :: n1,n2,n_loc  ! Matrix rows owned by each process
+
+  end type type_grid_orca
+
 contains
 
 end module grid_types
