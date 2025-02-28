6a5beb718f96989624b221fcd9d0d7a02d47bb72
Martim Mas e Braga <mabrag@bas.ac.uk>
2025-02-28 13:32:10 +0000

    now imports lat-only grid

--- src/basic/grid_lonlat_basic.f90
+++ src/basic/grid_lonlat_basic.f90
@@ -2,7 +2,7 @@ MODULE grid_lonlat_basic
 
   ! Functions for working with simple lon/lat-grids
   USE precisions                                             , ONLY: dp
-  use grid_types                                             , only: type_grid_lonlat
+  use grid_types                                             , only: type_grid_lonlat, type_grid_lat
   USE mpi_basic                                              , ONLY: par, cerr, ierr, recv_status, sync
   USE control_resources_and_error_messaging                  , ONLY: warning, crash, happy, init_routine, finalise_routine, colour_string
   USE parameters
