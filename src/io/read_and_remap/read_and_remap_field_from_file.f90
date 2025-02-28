805866f82b6d106550f8d2bb01ebd05a9393026d
Martim Mas e Braga <mabrag@bas.ac.uk>
2025-02-28 17:35:11 +0000

    importing missing module

--- src/io/read_and_remap/read_and_remap_field_from_file.f90
+++ src/io/read_and_remap/read_and_remap_field_from_file.f90
@@ -19,6 +19,7 @@ module read_and_remap_field_from_file
   use netcdf_basic
   use netcdf_setup_grid_mesh_from_file
   use netcdf_read_field_from_mesh_file
+  use netcdf_read_field_from_series_file
   use netcdf_read_field_from_lonlat_grid_file
   use netcdf_read_field_from_xy_grid_file
   use netcdf_read_field_from_series_file
