6970c191decb90843949f60a8f2238d8dd8958fd
Erwin Lambert <45197956+erwinlambert@users.noreply.github.com>
2025-03-11 08:27:02 +0100

    add GL diagnostics to misomip test

--- automated_testing/integrated_tests/idealised/MISMIPplus/analyse_integrated_test_misomip.py
+++ automated_testing/integrated_tests/idealised/MISMIPplus/analyse_integrated_test_misomip.py
@@ -1,4 +1,5 @@
 import sys
+import xarray as xr
 
 print('Analysing integrated test idealised/MISMIPplus/MISOMIP ...')
 print('')
@@ -16,13 +17,26 @@ else:
   foldername_automated_testing = sys.argv[1] 
 
 # Import python script
-
 sys.path.append(f'{foldername_automated_testing}/scoreboard/scripts')
 from single_test_run import *
 
+# Basic info of run
 test_name = 'MISOMIP'
 test_path = f'integrated_tests/idealised/MISMIPplus'
 
+# Initialise run
 single_run = Single_run(test_name, test_path) 
 
+# Add cost functions
+ds = xr.open_dataset('results_5km_iceocean1r/transect_westeast.nc')
+x_GL = ds.grounding_line_distance_from_start.values
+ds.close()
+
+err_x_GL_final_lo = abs( min( 0, x_GL[-1] - 430e3));
+err_x_GL_final_hi = abs( max( 0, x_GL[-1] - 450e3));
+
+single_run.add_cost_function('err_x_GL_final_lo', 'abs( min( 0, x_GL[-1] - 430e3))', err_x_GL_final_lo)
+single_run.add_cost_function('err_x_GL_final_hi', 'abs( max( 0, x_GL[-1] - 450e3))', err_x_GL_final_hi)
+
+# Write scoreboard file
 single_run.write_scoreboard_file(foldername_automated_testing)
