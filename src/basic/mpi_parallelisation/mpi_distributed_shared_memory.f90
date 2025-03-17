cd045e490912d61cabc3700a0abccbc830499e36
TijnBerends <c.j.berends@uu.nl>
2025-03-17 20:03:06 +0100

    Add distribute_dist_shared_from_primary
    And corresponding unit tests

--- src/basic/mpi_parallelisation/mpi_distributed_shared_memory.f90
+++ src/basic/mpi_parallelisation/mpi_distributed_shared_memory.f90
@@ -4,13 +4,14 @@ module mpi_distributed_shared_memory
   use deallocate_dist_shared_mod, only: deallocate_dist_shared
   use gather_dist_shared_to_primary_mod, only: gather_dist_shared_to_primary
   use gather_dist_shared_to_all_mod, only: gather_dist_shared_to_all
+  use distribute_dist_shared_from_primary_mod, only: distribute_dist_shared_from_primary
 
   implicit none
 
   private
 
   public :: allocate_dist_shared, deallocate_dist_shared, gather_dist_shared_to_primary, &
-    gather_dist_shared_to_all
+    gather_dist_shared_to_all, distribute_dist_shared_from_primary
 
 contains
 
