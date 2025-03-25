f30b8163a6310f5e9d7baf05044a353f2b94dac8
TijnBerends <c.j.berends@uu.nl>
2025-03-25 08:40:31 +0100

    Add reallocate_dist_shared
    Like reallocate, but for hybrid distributed/shared memory.

--- src/basic/mpi_parallelisation/mpi_distributed_shared_memory.f90
+++ src/basic/mpi_parallelisation/mpi_distributed_shared_memory.f90
@@ -2,6 +2,7 @@ module mpi_distributed_shared_memory
 
   use allocate_dist_shared_mod, only: allocate_dist_shared
   use deallocate_dist_shared_mod, only: deallocate_dist_shared
+  use reallocate_dist_shared_mod, only: reallocate_dist_shared
   use gather_dist_shared_to_primary_mod, only: gather_dist_shared_to_primary
   use gather_dist_shared_to_all_mod, only: gather_dist_shared_to_all
   use distribute_dist_shared_from_primary_mod, only: distribute_dist_shared_from_primary
@@ -10,8 +11,8 @@ module mpi_distributed_shared_memory
 
   private
 
-  public :: allocate_dist_shared, deallocate_dist_shared, gather_dist_shared_to_primary, &
-    gather_dist_shared_to_all, distribute_dist_shared_from_primary
+  public :: allocate_dist_shared, deallocate_dist_shared, reallocate_dist_shared, &
+    gather_dist_shared_to_primary, gather_dist_shared_to_all, distribute_dist_shared_from_primary
 
 contains
 
