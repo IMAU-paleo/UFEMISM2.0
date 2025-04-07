c572f21d49ebc7f53dd7377d4317fe9d034558f4
Erwin Lambert <45197956+erwinlambert@users.noreply.github.com>
2025-04-07 11:36:52 +0200

    set up ice and ocean in unit test

--- src/validation/unit_tests/ut_ocean_extrapolation.f90
+++ src/validation/unit_tests/ut_ocean_extrapolation.f90
@@ -16,8 +16,11 @@ module ut_ocean_extrapolation
   use mesh_secondary, only: calc_all_secondary_mesh_data
   use grid_basic, only: setup_square_grid
   use ice_model_types, only: type_ice_model
+  use ice_model_memory, only: allocate_ice_model
   use ocean_model_types, only: type_ocean_model
+  use ocean_utilities , only: initialise_ocean_vertical_grid
   use mesh_translation_tables, only: calc_field_to_vector_form_translation_tables
+  use, intrinsic :: ieee_arithmetic, only: ieee_value, ieee_signaling_nan
 
   implicit none
 
@@ -37,10 +40,10 @@ subroutine unit_tests_ocean_extrapolation_main( test_name_parent)
   character(len=1024), parameter :: routine_name = 'unit_tests_ocean_extrapolation_main'
   character(len=1024), parameter :: test_name_local = 'ocean_extrapolation'
   character(len=1024)            :: test_name
-  real(dp)                       :: xmin, xmax, ymin, ymax, alpha_min, res_max
+  real(dp)                       :: xmin, xmax, ymin, ymax, alpha_min, res_max, NaN
   character(len=1024)            :: name
   type(type_mesh)                :: mesh
-  integer                        :: vi, ierr
+  integer                        :: vi, k, ierr
   type(type_ice_model)           :: ice
   type(type_ocean_model)         :: ocean
   logical                        :: test_result
@@ -50,6 +53,9 @@ subroutine unit_tests_ocean_extrapolation_main( test_name_parent)
 
   ! Add test name to list
   test_name = trim( test_name_parent) // '/' // trim( test_name_local)
+
+  ! Define NaN
+  NaN = ieee_value( NaN, ieee_signaling_nan)
 
   ! Create a simple test mesh
   name = 'test_mesh'
@@ -68,8 +74,60 @@ subroutine unit_tests_ocean_extrapolation_main( test_name_parent)
   ! Set up ice and bed geometry
   ! ========
 
+  call allocate_ice_model( mesh, ice)
+
+  do vi = mesh%vi1, mesh%vi2
+    if (vi == 1) then
+      ! Open ocean
+      ice%Hi( vi) = 0._dp
+      ice%Hb( vi) = -2000._dp
+    elseif (vi == 2) then
+      ! Open ocean, shallower bedrock
+      ice%Hi( vi) = 0._dp
+      ice%Hb( vi) = -1000._dp
+    elseif (vi == 3) then
+      ! Cavity, intermediate bedrock
+      ice%Hi( vi) = 100._dp
+      ice%Hb( vi) = -1500._dp
+    elseif (vi == 4) then
+      ! Grounded ice below sea level
+      ice%Hi( vi) = 1000._dp
+      ice%Hb( vi) = -100._dp
+    elseif (vi == 5) then
+      ! Grounded ice above sea level
+      ice%Hi( vi) = 1000._dp
+      ice%Hb( vi) = 100._dp
+    end if
+  end do
+
   ! Set up offshore ocean forcing
   ! ========
+
+  ! Vertical grid
+  C%ocean_vertical_grid_max_depth = 5000._dp
+  C%ocean_vertical_grid_dz = 100._dp
+
+  if (allocated( C%z_ocean)) deallocate( C%z_ocean)
+  call initialise_ocean_vertical_grid
+
+  ! Ocean temperatures
+  if (allocated( ocean%T)) deallocate( ocean%T)
+  allocate( ocean%T( mesh%vi1:mesh%vi2,C%nz_ocean))
+
+  do vi = mesh%vi1, mesh%vi2
+    if (vi == 1) then
+      ! Warm ocean
+      do k = 1, C%nz_ocean
+        ocean%T( vi, k) = 1.e-3*C%z_ocean( k)
+      end do
+    elseif (vi == 2) then
+      ! Cold ocean
+      ocean%T( vi, :) = 0._dp
+    else
+      ! No data available
+      ocean%T( vi, :) = NaN
+    end if
+  end do
 
   ! Step 1
   ! ======
