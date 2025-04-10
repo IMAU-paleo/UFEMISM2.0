64a525c15bc8c29bc1d7e74994cf54e21062c7ec
TijnBerends <c.j.berends@uu.nl>
2025-04-10 08:24:54 +0200

    Rename modules
    And put the CSR matrix algebra tests after the halo exchange tests.

--- src/validation/unit_tests/ut_mpi_CSR_matrix_algebra.f90
+++ src/validation/unit_tests/ut_mpi_CSR_matrix_algebra.f90
@@ -1,4 +1,4 @@
-module ut_mpi_multiply_CSR_matrix_vector
+module ut_mpi_CSR_matrix_algebra
 
   ! Unit tests for different MPI routines
 
@@ -18,9 +18,36 @@ module ut_mpi_multiply_CSR_matrix_vector
 
   private
 
-  public :: test_multiply_CSR_matrix_with_vector
+  public :: test_CSR_matrix_algebra_main
 
 contains
+
+  subroutine test_CSR_matrix_algebra_main( test_name_parent)
+    ! Test the parallelised CSR matrix algebra subroutines
+
+    ! In/output variables:
+    character(len=*), intent(in) :: test_name_parent
+
+    ! Local variables:
+    character(len=1024), parameter :: routine_name = 'test_CSR_matrix_algebra_main'
+    character(len=1024), parameter :: test_name_local = 'CSR_matrix_algebra_main'
+    character(len=1024)            :: test_name
+
+    ! Add routine to call stack
+    call init_routine( routine_name)
+
+    ! Safety - should be run on 7 cores
+    call assert( test_eq( par%n, 7), 'should be run on 7 cores')
+
+    ! Add test name to list
+    test_name = trim( test_name_parent) // '/' // trim( test_name_local)
+
+    call test_multiply_CSR_matrix_with_vector( test_name)
+
+    ! Remove routine from call stack
+    call finalise_routine( routine_name)
+
+  end subroutine test_CSR_matrix_algebra_main
 
   subroutine test_multiply_CSR_matrix_with_vector( test_name_parent)
     ! Test the multiply_CSR_matrix_with_vector subroutines
@@ -440,4 +467,4 @@ contains
 
   end subroutine test_multiply_CSR_matrix_with_vector_1D_hybrid_hybrid
 
-end module ut_mpi_multiply_CSR_matrix_vector
+end module ut_mpi_CSR_matrix_algebra
