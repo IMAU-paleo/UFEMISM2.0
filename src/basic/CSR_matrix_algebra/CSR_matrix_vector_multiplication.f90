c214d672625a5109bf2428ef2a2cd32bc31cd498
TijnBerends <c.j.berends@uu.nl>
2025-04-10 11:41:59 +0200

    Remove mixed dist/hybrid CSR matrix code
    Going to rewrite this.

--- src/basic/CSR_matrix_algebra/CSR_matrix_vector_multiplication.f90
+++ src/basic/CSR_matrix_algebra/CSR_matrix_vector_multiplication.f90
@@ -4,16 +4,10 @@ module CSR_matrix_vector_multiplication
 
   use assertions_basic, only: assert
   use CSR_sparse_matrix_type, only: type_sparse_matrix_CSR_dp
-  use mpi_f08, only: MPI_ALLGATHER, MPI_INTEGER, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_SEND, MPI_RECV, &
-    MPI_STATUS, MPI_DOUBLE_PRECISION, MPI_SUM, MPI_ALLREDUCE, MPI_BCAST, MPI_WIN
   use precisions, only: dp
-  use mpi_basic, only: par, sync
-  use control_resources_and_error_messaging, only: warning, crash, happy, init_routine, finalise_routine, colour_string
-  use parameters
-  use reallocate_mod, only: reallocate
-  use mpi_distributed_memory, only: partition_list, gather_to_all
-  use mpi_distributed_shared_memory, only: allocate_dist_shared, gather_dist_shared_to_all, &
-    deallocate_dist_shared
+  use mpi_basic, only: par
+  use control_resources_and_error_messaging, only: init_routine, finalise_routine, crash
+  use mpi_distributed_memory, only: gather_to_all
 
   implicit none
 
@@ -23,289 +17,51 @@ module CSR_matrix_vector_multiplication
 
 contains
 
-  subroutine multiply_CSR_matrix_with_vector_1D( AA, xx, yy, xx_is_hybrid, yy_is_hybrid)
+  subroutine multiply_CSR_matrix_with_vector_1D( AA, xx, yy)
     !< Multiply a CSR matrix with a FORTRAN vector: yy = AA*xx
 
     ! In- and output variables:
     type(type_sparse_matrix_CSR_dp), intent(in   ) :: AA
     real(dp), dimension(:),          intent(in   ) :: xx
     real(dp), dimension(:),          intent(  out) :: yy
-    logical, optional,               intent(in   ) :: xx_is_hybrid, yy_is_hybrid
 
     ! Local variables:
-    character(len=1024), parameter :: routine_name = 'multiply_CSR_matrix_with_vector_1D'
-    logical                        :: xx_is_hybrid_, yy_is_hybrid_
+    character(len=1024), parameter      :: routine_name = 'multiply_CSR_matrix_with_vector_1D'
+    real(dp), dimension(:), allocatable :: xx_tot
+    integer                             :: i,k1,k2,k,j
 
     ! Add routine to path
     call init_routine( routine_name)
 
-    if (present( xx_is_hybrid)) then
-      xx_is_hybrid_ = xx_is_hybrid
-    else
-      xx_is_hybrid_ = .false.
-    end if
-    if (present( yy_is_hybrid)) then
-      yy_is_hybrid_ = yy_is_hybrid
-    else
-      yy_is_hybrid_ = .false.
-    end if
+#if (DO_ASSERTIONS)
+    call assert( size(xx,1) == AA%n_loc, 'matrix and vector sizes dont match!')
+    call assert( size(yy,1) == AA%m_loc, 'matrix and vector sizes dont match!')
+#endif
 
-    if ((.not. xx_is_hybrid_) .and. (.not. yy_is_hybrid_)) then
-      call multiply_CSR_matrix_with_vector_1D_dist_dist( AA, xx, yy)
-    elseif ((.not. xx_is_hybrid_) .and. yy_is_hybrid_) then
-      call multiply_CSR_matrix_with_vector_1D_dist_hybrid( AA, xx, yy)
-    elseif (xx_is_hybrid_ .and. (.not. yy_is_hybrid_)) then
-      call multiply_CSR_matrix_with_vector_1D_hybrid_dist( AA, xx, yy)
-    elseif (xx_is_hybrid_ .and. yy_is_hybrid_) then
-      call multiply_CSR_matrix_with_vector_1D_hybrid_hybrid( AA, xx, yy)
-    end if
+    allocate( xx_tot( AA%n))
+    call gather_to_all( xx, xx_tot)
+
+    ! Perform CSR matrix multiplication
+    do i = AA%i1, AA%i2
+
+      yy( i) = 0._dp
+
+      k1 = AA%ptr( i)
+      k2 = AA%ptr( i+1)-1
+
+      do k = k1, k2
+        j = AA%ind( k)
+        yy( i) = yy( i) + AA%val( k) * xx_tot( j)
+      end do
+
+    end do
 
     ! Finalise routine path
     call finalise_routine( routine_name)
 
   end subroutine multiply_CSR_matrix_with_vector_1D
 
-  subroutine multiply_CSR_matrix_with_vector_1D_dist_dist( AA, xx, yy)
-    !< Multiply a CSR matrix with a FORTRAN vector: yy = AA*xx
-
-    ! NOTE: AA, xx, and yy are stored as distributed memory
-
-    ! In- and output variables:
-    type(type_sparse_matrix_CSR_dp),  intent(in   ) :: AA
-    real(dp), dimension(AA%j1:AA%j2), intent(in   ) :: xx
-    real(dp), dimension(AA%i1:AA%i2), intent(  out) :: yy
-
-    ! Local variables:
-    character(len=1024), parameter      :: routine_name = 'multiply_CSR_matrix_with_vector_1D_dist_dist'
-    real(dp), dimension(:), allocatable :: xx_tot
-    integer                             :: i,k1,k2,k,j
-
-    ! Add routine to path
-    call init_routine( routine_name)
-
-#if (DO_ASSERTIONS)
-    call assert( size(xx,1) == AA%n_loc, 'matrix and vector sizes dont match!')
-    call assert( size(yy,1) == AA%m_loc, 'matrix and vector sizes dont match!')
-#endif
-
-    allocate( xx_tot( AA%n))
-    call gather_to_all( xx, xx_tot)
-
-    ! Perform CSR matrix multiplication
-    do i = AA%i1, AA%i2
-
-      yy( i) = 0._dp
-
-      k1 = AA%ptr( i)
-      k2 = AA%ptr( i+1)-1
-
-      do k = k1, k2
-        j = AA%ind( k)
-        yy( i) = yy( i) + AA%val( k) * xx_tot( j)
-      end do
-
-    end do
-
-    ! Finalise routine path
-    call finalise_routine( routine_name)
-
-  end subroutine multiply_CSR_matrix_with_vector_1D_dist_dist
-
-  subroutine multiply_CSR_matrix_with_vector_1D_dist_hybrid( AA, xx, yy)
-    !< Multiply a CSR matrix with a FORTRAN vector: yy = AA*xx
-
-    ! NOTE: AA and xx are stored as distributed memory, yy as hybrid distributed/shared memory
-
-    ! In- and output variables:
-    type(type_sparse_matrix_CSR_dp),            intent(in   ) :: AA
-    real(dp), dimension(AA%j1     :AA%j2     ), intent(in   ) :: xx
-    real(dp), dimension(AA%i1_node:AA%i2_node), intent(  out) :: yy
-
-    ! Local variables:
-    character(len=1024), parameter      :: routine_name = 'multiply_CSR_matrix_with_vector_1D_dist_hybrid'
-    real(dp), dimension(:), allocatable :: xx_tot
-    integer                             :: i,k1,k2,k,j
-
-    ! Add routine to path
-    call init_routine( routine_name)
-
-#if (DO_ASSERTIONS)
-    call assert( size(xx,1) == AA%n_loc, 'matrix and vector sizes dont match!')
-    call assert( size(yy,1) == AA%m_node, 'matrix and vector sizes dont match!')
-#endif
-
-    allocate( xx_tot( AA%n))
-    call gather_to_all( xx, xx_tot)
-
-    ! Perform CSR matrix multiplication
-    do i = AA%i1, AA%i2
-
-      yy( i) = 0._dp
-
-      k1 = AA%ptr( i)
-      k2 = AA%ptr( i+1)-1
-
-      do k = k1, k2
-        j = AA%ind( k)
-        yy( i) = yy( i) + AA%val( k) * xx_tot( j)
-      end do
-
-    end do
-
-    ! Finalise routine path
-    call finalise_routine( routine_name)
-
-  end subroutine multiply_CSR_matrix_with_vector_1D_dist_hybrid
-
-  subroutine multiply_CSR_matrix_with_vector_1D_hybrid_dist( AA, xx, yy)
-    !< Multiply a CSR matrix with a FORTRAN vector: yy = AA*xx
-
-    ! NOTE: AA and yy are stored as distributed memory, xx as hybrid distributed/shared memory
-
-    ! In- and output variables:
-    type(type_sparse_matrix_CSR_dp),            intent(in   ) :: AA
-    real(dp), dimension(AA%j1_node:AA%j2_node), intent(in   ) :: xx
-    real(dp), dimension(AA%i1     :AA%i2     ), intent(  out) :: yy
-
-    ! Local variables:
-    character(len=1024), parameter  :: routine_name = 'multiply_CSR_matrix_with_vector_1D_hybrid_dist'
-    real(dp), dimension(:), pointer :: xx_tot => null()
-    type(MPI_WIN)                   :: wxx_tot
-    integer                         :: i,k1,k2,k,j
-
-    ! Add routine to path
-    call init_routine( routine_name)
-
-#if (DO_ASSERTIONS)
-    call assert( size(xx,1) == AA%n_node, 'matrix and vector sizes dont match!')
-    call assert( size(yy,1) == AA%m_loc, 'matrix and vector sizes dont match!')
-#endif
-
-    ! Gather xx to all nodes
-    call allocate_dist_shared( xx_tot, wxx_tot, AA%n)
-    call gather_dist_shared_to_all( xx, xx_tot)
-
-    ! Perform CSR matrix multiplication
-    do i = AA%i1, AA%i2
-
-      yy( i) = 0._dp
-
-      k1 = AA%ptr( i)
-      k2 = AA%ptr( i+1)-1
-
-      do k = k1, k2
-        j = AA%ind( k)
-        yy( i) = yy( i) + AA%val( k) * xx_tot( j)
-      end do
-
-    end do
-
-    ! Clean up after yourself
-    call deallocate_dist_shared( xx_tot, wxx_tot)
-
-    ! Finalise routine path
-    call finalise_routine( routine_name)
-
-  end subroutine multiply_CSR_matrix_with_vector_1D_hybrid_dist
-
-  subroutine multiply_CSR_matrix_with_vector_1D_hybrid_hybrid( AA, xx, yy)
-    !< Multiply a CSR matrix with a FORTRAN vector: yy = AA*xx
-
-    ! NOTE: AA is as distributed memory, xx and yy as hybrid distributed/shared memory
-
-    ! In- and output variables:
-    type(type_sparse_matrix_CSR_dp),            intent(in   ) :: AA
-    real(dp), dimension(AA%j1_node:AA%j2_node), intent(in   ) :: xx
-    real(dp), dimension(AA%i1_node:AA%i2_node), intent(  out) :: yy
-
-    ! Local variables:
-    character(len=1024), parameter  :: routine_name = 'multiply_CSR_matrix_with_vector_1D_hybrid_hybrid'
-    real(dp), dimension(:), pointer :: xx_tot => null()
-    type(MPI_WIN)                   :: wxx_tot
-    integer                         :: i,k1,k2,k,j
-
-    ! Add routine to path
-    call init_routine( routine_name)
-
-#if (DO_ASSERTIONS)
-    call assert( size(xx,1) == AA%n_node, 'matrix and vector sizes dont match!')
-    call assert( size(yy,1) == AA%m_node, 'matrix and vector sizes dont match!')
-#endif
-
-    ! Gather xx to all nodes
-    call allocate_dist_shared( xx_tot, wxx_tot, AA%n)
-    call gather_dist_shared_to_all( xx, xx_tot)
-
-    ! Perform CSR matrix multiplication
-    do i = AA%i1, AA%i2
-
-      yy( i) = 0._dp
-
-      k1 = AA%ptr( i)
-      k2 = AA%ptr( i+1)-1
-
-      do k = k1, k2
-        j = AA%ind( k)
-        yy( i) = yy( i) + AA%val( k) * xx_tot( j)
-      end do
-
-    end do
-
-    ! Clean up after yourself
-    call deallocate_dist_shared( xx_tot, wxx_tot)
-
-    ! Finalise routine path
-    call finalise_routine( routine_name)
-
-  end subroutine multiply_CSR_matrix_with_vector_1D_hybrid_hybrid
-
-  subroutine multiply_CSR_matrix_with_vector_2D( AA, xx, yy, xx_is_hybrid, yy_is_hybrid)
-    !< Multiply a CSR matrix with a FORTRAN vector: yy = AA*xx
-
-    ! NOTE: AA, xx, and yy are stored as distributed memory
-
-    ! In- and output variables:
-    type(type_sparse_matrix_CSR_dp), intent(in   ) :: AA
-    real(dp), dimension(:,:),        intent(in   ) :: xx
-    real(dp), dimension(:,:),        intent(  out) :: yy
-    logical, optional,               intent(in   ) :: xx_is_hybrid, yy_is_hybrid
-
-    ! Local variables:
-    character(len=1024), parameter :: routine_name = 'multiply_CSR_matrix_with_vector_2D'
-    logical                        :: xx_is_hybrid_, yy_is_hybrid_
-
-    ! Add routine to path
-    call init_routine( routine_name)
-
-    if (present( xx_is_hybrid)) then
-      xx_is_hybrid_ = xx_is_hybrid
-    else
-      xx_is_hybrid_ = .false.
-    end if
-    if (present( yy_is_hybrid)) then
-      yy_is_hybrid_ = yy_is_hybrid
-    else
-      yy_is_hybrid_ = .false.
-    end if
-
-    if ((.not. xx_is_hybrid_) .and. (.not. yy_is_hybrid_)) then
-      call multiply_CSR_matrix_with_vector_2D_dist_dist( AA, xx, yy)
-    elseif ((.not. xx_is_hybrid_) .and. yy_is_hybrid_) then
-      call crash('xx dist, yy hybrid not implemented yet!')
-      ! call multiply_CSR_matrix_with_vector_2D_dist_hybrid( AA, xx, yy)
-    elseif (xx_is_hybrid_ .and. (.not. yy_is_hybrid_)) then
-      call multiply_CSR_matrix_with_vector_2D_hybrid_dist( AA, xx, yy)
-    elseif (xx_is_hybrid_ .and. yy_is_hybrid_) then
-      call crash('xx hybrid, yy hybrid not implemented yet!')
-      ! call multiply_CSR_matrix_with_vector_2D_hybrid_hybrid( AA, xx, yy)
-    end if
-
-    ! Finalise routine path
-    call finalise_routine( routine_name)
-
-  end subroutine multiply_CSR_matrix_with_vector_2D
-
-  subroutine multiply_CSR_matrix_with_vector_2D_dist_dist( AA, xx, yy)
+  subroutine multiply_CSR_matrix_with_vector_2D( AA, xx, yy)
     !< Multiply a CSR matrix with a FORTRAN vector: yy = AA*xx
 
     ! NOTE: AA, xx, and yy are stored as distributed memory
@@ -316,7 +72,7 @@ contains
     real(dp), dimension(:,:),        intent(  out) :: yy
 
     ! Local variables:
-    character(len=1024), parameter      :: routine_name = 'multiply_CSR_matrix_with_vector_2D_dist_dist'
+    character(len=1024), parameter      :: routine_name = 'multiply_CSR_matrix_with_vector_2D'
     integer                             :: j
     real(dp), dimension(:), allocatable :: xx_1D, yy_1D
 
@@ -343,52 +99,6 @@ contains
     ! Finalise routine path
     call finalise_routine( routine_name)
 
-  end subroutine multiply_CSR_matrix_with_vector_2D_dist_dist
-
-  subroutine multiply_CSR_matrix_with_vector_2D_hybrid_dist( AA, xx, yy)
-    !< Multiply a CSR matrix with a FORTRAN vector: yy = AA*xx
-
-    ! NOTE: AA and yy are stored as distributed memory, xx as hybrid distributed/shared memory
-
-    ! In- and output variables:
-    type(type_sparse_matrix_CSR_dp), intent(in   ) :: AA
-    real(dp), dimension(:,:),        intent(in   ) :: xx
-    real(dp), dimension(:,:),        intent(  out) :: yy
-
-    ! Local variables:
-    character(len=1024), parameter      :: routine_name = 'multiply_CSR_matrix_with_vector_2D_hybrid_dist'
-    integer                             :: j
-    real(dp), dimension(:), pointer     :: xx_1D => null()
-    type(MPI_WIN)                       :: wxx_1D
-    real(dp), dimension(:), allocatable :: yy_1D
-
-    ! Add routine to path
-    call init_routine( routine_name)
-
-#if (DO_ASSERTIONS)
-    call assert( size(xx,1) == AA%n_node, 'matrix and vector sizes dont match!')
-    call assert( size(yy,1) == AA%m_loc, 'matrix and vector sizes dont match!')
-    call assert( size(xx,2) == size(yy,2), 'vector sizes dont match!')
-#endif
-
-    ! Allocate memory
-    call allocate_dist_shared( xx_1D, wxx_1D, AA%n_node)
-    allocate( yy_1D( AA%m_node), source = 0._dp)
-
-    ! Calculate each column separately
-    do j = 1, size(xx,2)
-      xx_1D( AA%j1_node:AA%j2_node) = xx( AA%j1_node:AA%j2_node,j)
-      call sync
-      call multiply_CSR_matrix_with_vector_1D( AA, xx_1D, yy_1D)
-      yy( :,j) = yy_1D
-    end do
-
-    ! Clean up after yourself
-    call deallocate_dist_shared( xx_1D, wxx_1D)
-
-    ! Finalise routine path
-    call finalise_routine( routine_name)
-
-  end subroutine multiply_CSR_matrix_with_vector_2D_hybrid_dist
+  end subroutine multiply_CSR_matrix_with_vector_2D
 
 end module CSR_matrix_vector_multiplication
--- src/validation/unit_tests/ut_mpi_CSR_matrix_algebra.f90
+++ src/validation/unit_tests/ut_mpi_CSR_matrix_algebra.f90
@@ -70,8 +70,6 @@ contains
     test_name = trim( test_name_parent) // '/' // trim( test_name_local)
 
     call test_multiply_CSR_matrix_with_vector_1D_dist_dist    ( test_name)
-    call test_multiply_CSR_matrix_with_vector_1D_dist_hybrid  ( test_name)
-    call test_multiply_CSR_matrix_with_vector_1D_hybrid_dist  ( test_name)
     call test_multiply_CSR_matrix_with_vector_1D_hybrid_hybrid( test_name)
 
     ! Remove routine from call stack
@@ -157,7 +155,7 @@ contains
       yy_correct(7) = 28
     end if
 
-    call multiply_CSR_matrix_with_vector_1D( AA, xx, yy, xx_is_hybrid = .false., yy_is_hybrid = .false.)
+    call multiply_CSR_matrix_with_vector_1D( AA, xx, yy)
 
     test_result = test_eq( yy( AA%i1:AA%i2), yy_correct( AA%i1:AA%i2))
     call MPI_ALLREDUCE( MPI_IN_PLACE, test_result, 1, MPI_LOGICAL, MPI_LAND, MPI_COMM_WORLD, ierr)
@@ -168,202 +166,6 @@ contains
     call finalise_routine( routine_name)
 
   end subroutine test_multiply_CSR_matrix_with_vector_1D_dist_dist
-
-  subroutine test_multiply_CSR_matrix_with_vector_1D_dist_hybrid( test_name_parent)
-    ! Test the multiply_CSR_matrix_with_vector subroutines
-
-    ! In/output variables:
-    character(len=*), intent(in) :: test_name_parent
-
-    ! Local variables:
-    character(len=1024), parameter      :: routine_name = 'test_multiply_CSR_matrix_with_vector_1D_dist_hybrid'
-    character(len=1024), parameter      :: test_name_local = 'dist_hybrid_1D'
-    character(len=1024)                 :: test_name
-    type(type_sparse_matrix_CSR_dp)     :: AA
-    real(dp), dimension(:), allocatable :: xx
-    real(dp), dimension(:), pointer     :: yy => null()
-    real(dp), dimension(:), pointer     :: yy_correct => null()
-    type(MPI_WIN)                       :: wyy, wyy_correct
-    logical                             :: test_result
-    integer                             :: ierr
-
-    ! Add routine to call stack
-    call init_routine( routine_name)
-
-    ! Safety - should be run on 7 cores
-    call assert( test_eq( par%n, 7), 'should be run on 7 cores')
-
-    ! Add test name to list
-    test_name = trim( test_name_parent) // '/' // trim( test_name_local)
-
-    ! Verify that we can solve the following matrix equation:
-    !
-    !              A             x   =    y
-    !
-    !    [1,  ,  ,  ,  ,  ,  ]  [1]     [ 1]
-    !    [ , 2,  ,  ,  ,  ,  ]  [2]     [ 4]
-    !    [ , 3,  ,  , 4,  ,  ]  [3]     [26]
-    !    [ ,  , 2,  ,  , 1,  ]  [4]  =  [12]
-    !    [ ,  ,  , 5,  ,  , 2]  [5]     [34]
-    !    [ , 1,  ,  , 3,  ,  ]  [6]     [17]
-    !    [ ,  , 7,  ,  ,  , 1]  [7]     [28]
-
-    call allocate_matrix_CSR_dist( AA, 7, 7, 1, 1, 2)
-    allocate( xx        ( AA%j1:AA%j2))
-
-    call allocate_dist_shared( yy, wyy, AA%m_node)
-    yy( AA%i1_node:AA%i2_node) => yy
-    call allocate_dist_shared( yy_correct, wyy_correct, AA%m_node)
-    yy_correct( AA%i1_node:AA%i2_node) => yy_correct
-
-    ! Define A, x, and y_correct
-
-    if     (par%i == 0) then
-      call add_entry_CSR_dist( AA, 1, 1, 1._dp)
-      xx        (1) = 1
-      yy_correct(1) = 1
-    elseif (par%i == 1) then
-      call add_entry_CSR_dist( AA, 2, 2, 2._dp)
-      xx        (2) = 2
-      yy_correct(2) = 4
-    elseif (par%i == 2) then
-      call add_entry_CSR_dist( AA, 3, 2, 3._dp)
-      call add_entry_CSR_dist( AA, 3, 5, 4._dp)
-      xx        (3) = 3
-      yy_correct(3) = 26
-    elseif (par%i == 3) then
-      call add_entry_CSR_dist( AA, 4, 3, 2._dp)
-      call add_entry_CSR_dist( AA, 4, 6, 1._dp)
-      xx        (4) = 4
-      yy_correct(4) = 12
-    elseif (par%i == 4) then
-      call add_entry_CSR_dist( AA, 5, 4, 5._dp)
-      call add_entry_CSR_dist( AA, 5, 7, 2._dp)
-      xx        (5) = 5
-      yy_correct(5) = 34
-    elseif (par%i == 5) then
-      call add_entry_CSR_dist( AA, 6, 2, 1._dp)
-      call add_entry_CSR_dist( AA, 6, 5, 3._dp)
-      xx        (6) = 6
-      yy_correct(6) = 17
-    elseif (par%i == 6) then
-      call add_entry_CSR_dist( AA, 7, 3, 7._dp)
-      call add_entry_CSR_dist( AA, 7, 7, 1._dp)
-      xx        (7) = 7
-      yy_correct(7) = 28
-    end if
-
-    call multiply_CSR_matrix_with_vector_1D( AA, xx, yy, xx_is_hybrid = .false., yy_is_hybrid = .true.)
-
-    test_result = test_eq( yy( AA%i1:AA%i2), yy_correct( AA%i1:AA%i2))
-    call MPI_ALLREDUCE( MPI_IN_PLACE, test_result, 1, MPI_LOGICAL, MPI_LAND, MPI_COMM_WORLD, ierr)
-
-    call unit_test( test_result, test_name)
-
-    ! Clean up after yourself
-    call deallocate_dist_shared( yy, wyy)
-    call deallocate_dist_shared( yy_correct, wyy_correct)
-
-    ! Remove routine from call stack
-    call finalise_routine( routine_name)
-
-  end subroutine test_multiply_CSR_matrix_with_vector_1D_dist_hybrid
-
-  subroutine test_multiply_CSR_matrix_with_vector_1D_hybrid_dist( test_name_parent)
-    ! Test the multiply_CSR_matrix_with_vector subroutines
-
-    ! In/output variables:
-    character(len=*), intent(in) :: test_name_parent
-
-    ! Local variables:
-    character(len=1024), parameter      :: routine_name = 'test_multiply_CSR_matrix_with_vector_1D_hybrid_dist'
-    character(len=1024), parameter      :: test_name_local = 'hybrid_dist_1D'
-    character(len=1024)                 :: test_name
-    type(type_sparse_matrix_CSR_dp)     :: AA
-    real(dp), dimension(:), pointer     :: xx => null()
-    type(MPI_WIN)                       :: wxx
-    real(dp), dimension(:), allocatable :: yy, yy_correct
-    logical                             :: test_result
-    integer                             :: ierr
-
-    ! Add routine to call stack
-    call init_routine( routine_name)
-
-    ! Safety - should be run on 7 cores
-    call assert( test_eq( par%n, 7), 'should be run on 7 cores')
-
-    ! Add test name to list
-    test_name = trim( test_name_parent) // '/' // trim( test_name_local)
-
-    ! Verify that we can solve the following matrix equation:
-    !
-    !              A             x   =    y
-    !
-    !    [1,  ,  ,  ,  ,  ,  ]  [1]     [ 1]
-    !    [ , 2,  ,  ,  ,  ,  ]  [2]     [ 4]
-    !    [ , 3,  ,  , 4,  ,  ]  [3]     [26]
-    !    [ ,  , 2,  ,  , 1,  ]  [4]  =  [12]
-    !    [ ,  ,  , 5,  ,  , 2]  [5]     [34]
-    !    [ , 1,  ,  , 3,  ,  ]  [6]     [17]
-    !    [ ,  , 7,  ,  ,  , 1]  [7]     [28]
-
-    call allocate_matrix_CSR_dist( AA, 7, 7, 1, 1, 2)
-    call allocate_dist_shared( xx, wxx, AA%n_node)
-    xx( AA%j1_node:AA%j2_node) => xx
-    allocate( yy        ( AA%i1:AA%i2))
-    allocate( yy_correct( AA%i1:AA%i2))
-
-    ! Define A, x, and y_correct
-
-    if     (par%i == 0) then
-      call add_entry_CSR_dist( AA, 1, 1, 1._dp)
-      xx        (1) = 1
-      yy_correct(1) = 1
-    elseif (par%i == 1) then
-      call add_entry_CSR_dist( AA, 2, 2, 2._dp)
-      xx        (2) = 2
-      yy_correct(2) = 4
-    elseif (par%i == 2) then
-      call add_entry_CSR_dist( AA, 3, 2, 3._dp)
-      call add_entry_CSR_dist( AA, 3, 5, 4._dp)
-      xx        (3) = 3
-      yy_correct(3) = 26
-    elseif (par%i == 3) then
-      call add_entry_CSR_dist( AA, 4, 3, 2._dp)
-      call add_entry_CSR_dist( AA, 4, 6, 1._dp)
-      xx        (4) = 4
-      yy_correct(4) = 12
-    elseif (par%i == 4) then
-      call add_entry_CSR_dist( AA, 5, 4, 5._dp)
-      call add_entry_CSR_dist( AA, 5, 7, 2._dp)
-      xx        (5) = 5
-      yy_correct(5) = 34
-    elseif (par%i == 5) then
-      call add_entry_CSR_dist( AA, 6, 2, 1._dp)
-      call add_entry_CSR_dist( AA, 6, 5, 3._dp)
-      xx        (6) = 6
-      yy_correct(6) = 17
-    elseif (par%i == 6) then
-      call add_entry_CSR_dist( AA, 7, 3, 7._dp)
-      call add_entry_CSR_dist( AA, 7, 7, 1._dp)
-      xx        (7) = 7
-      yy_correct(7) = 28
-    end if
-
-    call multiply_CSR_matrix_with_vector_1D( AA, xx, yy, xx_is_hybrid = .true., yy_is_hybrid = .false.)
-
-    test_result = test_eq( yy( AA%i1:AA%i2), yy_correct( AA%i1:AA%i2))
-    call MPI_ALLREDUCE( MPI_IN_PLACE, test_result, 1, MPI_LOGICAL, MPI_LAND, MPI_COMM_WORLD, ierr)
-
-    call unit_test( test_result, test_name)
-
-    ! Clean up after yourself
-    call deallocate_dist_shared( xx, wxx)
-
-    ! Remove routine from call stack
-    call finalise_routine( routine_name)
-
-  end subroutine test_multiply_CSR_matrix_with_vector_1D_hybrid_dist
 
   subroutine test_multiply_CSR_matrix_with_vector_1D_hybrid_hybrid( test_name_parent)
     ! Test the multiply_CSR_matrix_with_vector subroutines
@@ -392,75 +194,75 @@ contains
     ! Add test name to list
     test_name = trim( test_name_parent) // '/' // trim( test_name_local)
 
-    ! Verify that we can solve the following matrix equation:
-    !
-    !              A             x   =    y
-    !
-    !    [1,  ,  ,  ,  ,  ,  ]  [1]     [ 1]
-    !    [ , 2,  ,  ,  ,  ,  ]  [2]     [ 4]
-    !    [ , 3,  ,  , 4,  ,  ]  [3]     [26]
-    !    [ ,  , 2,  ,  , 1,  ]  [4]  =  [12]
-    !    [ ,  ,  , 5,  ,  , 2]  [5]     [34]
-    !    [ , 1,  ,  , 3,  ,  ]  [6]     [17]
-    !    [ ,  , 7,  ,  ,  , 1]  [7]     [28]
+    ! ! Verify that we can solve the following matrix equation:
+    ! !
+    ! !              A             x   =    y
+    ! !
+    ! !    [1,  ,  ,  ,  ,  ,  ]  [1]     [ 1]
+    ! !    [ , 2,  ,  ,  ,  ,  ]  [2]     [ 4]
+    ! !    [ , 3,  ,  , 4,  ,  ]  [3]     [26]
+    ! !    [ ,  , 2,  ,  , 1,  ]  [4]  =  [12]
+    ! !    [ ,  ,  , 5,  ,  , 2]  [5]     [34]
+    ! !    [ , 1,  ,  , 3,  ,  ]  [6]     [17]
+    ! !    [ ,  , 7,  ,  ,  , 1]  [7]     [28]
 
-    call allocate_matrix_CSR_dist( AA, 7, 7, 1, 1, 2)
+    ! call allocate_matrix_CSR_dist( AA, 7, 7, 1, 1, 2)
 
-    call allocate_dist_shared( xx, wxx, AA%n_node)
-    xx( AA%j1_node:AA%j2_node) => xx
-    call allocate_dist_shared( yy, wyy, AA%m_node)
-    yy( AA%i1_node:AA%i2_node) => yy
-    call allocate_dist_shared( yy_correct, wyy_correct, AA%m_node)
-    yy_correct( AA%i1_node:AA%i2_node) => yy_correct
+    ! call allocate_dist_shared( xx, wxx, AA%n_node)
+    ! xx( AA%j1_node:AA%j2_node) => xx
+    ! call allocate_dist_shared( yy, wyy, AA%m_node)
+    ! yy( AA%i1_node:AA%i2_node) => yy
+    ! call allocate_dist_shared( yy_correct, wyy_correct, AA%m_node)
+    ! yy_correct( AA%i1_node:AA%i2_node) => yy_correct
 
-    ! Define A, x, and y_correct
+    ! ! Define A, x, and y_correct
 
-    if     (par%i == 0) then
-      call add_entry_CSR_dist( AA, 1, 1, 1._dp)
-      xx        (1) = 1
-      yy_correct(1) = 1
-    elseif (par%i == 1) then
-      call add_entry_CSR_dist( AA, 2, 2, 2._dp)
-      xx        (2) = 2
-      yy_correct(2) = 4
-    elseif (par%i == 2) then
-      call add_entry_CSR_dist( AA, 3, 2, 3._dp)
-      call add_entry_CSR_dist( AA, 3, 5, 4._dp)
-      xx        (3) = 3
-      yy_correct(3) = 26
-    elseif (par%i == 3) then
-      call add_entry_CSR_dist( AA, 4, 3, 2._dp)
-      call add_entry_CSR_dist( AA, 4, 6, 1._dp)
-      xx        (4) = 4
-      yy_correct(4) = 12
-    elseif (par%i == 4) then
-      call add_entry_CSR_dist( AA, 5, 4, 5._dp)
-      call add_entry_CSR_dist( AA, 5, 7, 2._dp)
-      xx        (5) = 5
-      yy_correct(5) = 34
-    elseif (par%i == 5) then
-      call add_entry_CSR_dist( AA, 6, 2, 1._dp)
-      call add_entry_CSR_dist( AA, 6, 5, 3._dp)
-      xx        (6) = 6
-      yy_correct(6) = 17
-    elseif (par%i == 6) then
-      call add_entry_CSR_dist( AA, 7, 3, 7._dp)
-      call add_entry_CSR_dist( AA, 7, 7, 1._dp)
-      xx        (7) = 7
-      yy_correct(7) = 28
-    end if
+    ! if     (par%i == 0) then
+    !   call add_entry_CSR_dist( AA, 1, 1, 1._dp)
+    !   xx        (1) = 1
+    !   yy_correct(1) = 1
+    ! elseif (par%i == 1) then
+    !   call add_entry_CSR_dist( AA, 2, 2, 2._dp)
+    !   xx        (2) = 2
+    !   yy_correct(2) = 4
+    ! elseif (par%i == 2) then
+    !   call add_entry_CSR_dist( AA, 3, 2, 3._dp)
+    !   call add_entry_CSR_dist( AA, 3, 5, 4._dp)
+    !   xx        (3) = 3
+    !   yy_correct(3) = 26
+    ! elseif (par%i == 3) then
+    !   call add_entry_CSR_dist( AA, 4, 3, 2._dp)
+    !   call add_entry_CSR_dist( AA, 4, 6, 1._dp)
+    !   xx        (4) = 4
+    !   yy_correct(4) = 12
+    ! elseif (par%i == 4) then
+    !   call add_entry_CSR_dist( AA, 5, 4, 5._dp)
+    !   call add_entry_CSR_dist( AA, 5, 7, 2._dp)
+    !   xx        (5) = 5
+    !   yy_correct(5) = 34
+    ! elseif (par%i == 5) then
+    !   call add_entry_CSR_dist( AA, 6, 2, 1._dp)
+    !   call add_entry_CSR_dist( AA, 6, 5, 3._dp)
+    !   xx        (6) = 6
+    !   yy_correct(6) = 17
+    ! elseif (par%i == 6) then
+    !   call add_entry_CSR_dist( AA, 7, 3, 7._dp)
+    !   call add_entry_CSR_dist( AA, 7, 7, 1._dp)
+    !   xx        (7) = 7
+    !   yy_correct(7) = 28
+    ! end if
 
-    call multiply_CSR_matrix_with_vector_1D( AA, xx, yy, xx_is_hybrid = .true., yy_is_hybrid = .true.)
+    ! call multiply_CSR_matrix_with_vector_1D( AA, xx, yy, xx_is_hybrid = .true., yy_is_hybrid = .true.)
 
-    test_result = test_eq( yy( AA%i1:AA%i2), yy_correct( AA%i1:AA%i2))
-    call MPI_ALLREDUCE( MPI_IN_PLACE, test_result, 1, MPI_LOGICAL, MPI_LAND, MPI_COMM_WORLD, ierr)
+    ! test_result = test_eq( yy( AA%i1:AA%i2), yy_correct( AA%i1:AA%i2))
+    ! call MPI_ALLREDUCE( MPI_IN_PLACE, test_result, 1, MPI_LOGICAL, MPI_LAND, MPI_COMM_WORLD, ierr)
 
-    call unit_test( test_result, test_name)
+    ! call unit_test( test_result, test_name)
 
-    ! Clean up after yourself
-    call deallocate_dist_shared( xx, wxx)
-    call deallocate_dist_shared( yy, wyy)
-    call deallocate_dist_shared( yy_correct, wyy_correct)
+    ! ! Clean up after yourself
+    ! call deallocate_dist_shared( xx, wxx)
+    ! call deallocate_dist_shared( yy, wyy)
+    ! call deallocate_dist_shared( yy_correct, wyy_correct)
 
     ! Remove routine from call stack
     call finalise_routine( routine_name)
