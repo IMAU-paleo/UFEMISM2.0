e965d538872ea5b83ff538eed116a70b1879c2f8
Martim Mas e Braga <mabrag@bas.ac.uk>
2025-04-15 15:57:22 +0100

    omitted unecessary prints

--- src/surface_mass_balance/SMB_main.f90
+++ src/surface_mass_balance/SMB_main.f90
@@ -96,12 +96,12 @@ CONTAINS
       CASE ('idealised')
         CALL run_SMB_model_idealised( mesh, ice, SMB, time)
       CASE ('prescribed')
-        IF (par%primary)  WRITE(*,"(A)") '   Running prescribed SMB...'
+        !IF (par%primary)  WRITE(*,"(A)") '   Running prescribed SMB...'
         CALL run_SMB_model_prescribed( mesh, ice, SMB, region_name, time)
       CASE ('reconstructed')
         CALL run_SMB_model_reconstructed( mesh, grid_smooth, ice, SMB, region_name, time)
       CASE ('parameterised')  
-        IF (par%primary)  WRITE(*,"(A)") '   Running parameterised SMB...'
+        !IF (par%primary)  WRITE(*,"(A)") '   Running parameterised SMB...'
         CALL run_SMB_model_parameterised( mesh, ice, SMB, climate, time)
       CASE DEFAULT
         CALL crash('unknown choice_SMB_model "' // TRIM( choice_SMB_model) // '"')
@@ -357,7 +357,7 @@ CONTAINS
     ! Set the filename
     filename_base = TRIM( C%output_dir) // 'restart_SMB_' // region_name
     CALL generate_filename_XXXXXdotnc( filename_base, SMB%restart_filename)
-
+    
     ! Print to terminal
     IF (par%primary) WRITE(0,'(A)') '   Creating SMB model restart file "' // &
       colour_string( TRIM( SMB%restart_filename), 'light blue') // '"...'
