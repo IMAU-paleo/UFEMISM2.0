b673b621b6bfc20fce12d3552d21064afb8445da
TijnBerends <c.j.berends@uu.nl>
2025-06-05 16:52:32 +0200

    Update basal_hydrology.f90
    Lowercase Fortran intrinsics, indentation

--- src/basal_conditions/basal_hydrology.f90
+++ src/basal_conditions/basal_hydrology.f90
@@ -1,133 +1,110 @@
-MODULE basal_hydrology
+module basal_hydrology
 
   ! Contains all the different basal hydrology models.
 
-! ===== Preamble =====
-! ====================
-
   use mpi_basic, only: par
-  USE precisions                                             , ONLY: dp
-  USE control_resources_and_error_messaging                  , ONLY: warning, crash, happy, init_routine, finalise_routine, colour_string
-  USE model_configuration                                    , ONLY: C
+  use precisions, only: dp
+  use control_resources_and_error_messaging, only: crash, init_routine, finalise_routine
+  use model_configuration, only: C
   use parameters, only: grav, ice_density
-  USE mesh_types                                             , ONLY: type_mesh
-  USE ice_model_types                                        , ONLY: type_ice_model
+  use mesh_types, only: type_mesh
+  use ice_model_types, only: type_ice_model
 
-  IMPLICIT NONE
+  implicit none
 
-CONTAINS
+contains
 
-! ===== Main routines =====
-! =========================
+  subroutine run_basal_hydrology_model( mesh, ice)
 
-  SUBROUTINE run_basal_hydrology_model( mesh, ice)
-    ! Run the chosen basal hydrology model
-
-    IMPLICIT NONE
-
-    ! Input variables:
-    TYPE(type_mesh),                     INTENT(IN)    :: mesh
-    TYPE(type_ice_model),                INTENT(INOUT) :: ice
+    ! In/output variables:
+    type(type_mesh),      intent(in   ) :: mesh
+    type(type_ice_model), intent(inout) :: ice
 
     ! Local variables:
-    CHARACTER(LEN=256), PARAMETER                      :: routine_name = 'run_basal_hydrology_model'
-    INTEGER                                            :: vi
+    character(len=1024), parameter :: routine_name = 'run_basal_hydrology_model'
+    integer                        :: vi
 
     ! Add routine to path
-    CALL init_routine( routine_name)
+    call init_routine( routine_name)
 
     ! Calculate pore water pressure using the chosen basal hydrology model
     ! ====================================================================
 
-    IF     (C%choice_basal_hydrology_model == 'none') THEN
-      ! No pore water pressure
-      CALL calc_pore_water_pressure_none( mesh, ice)
-    ELSEIF (C%choice_basal_hydrology_model == 'Martin2011') THEN
-      ! The Martin et al. (2011) parameterisation of pore water pressure
-      CALL calc_pore_water_pressure_Martin2011( mesh, ice)
-    ELSE
-      CALL crash('unknown choice_basal_hydrology_model "' // TRIM( C%choice_basal_hydrology_model) // '"!')
-    END IF
+    select case (C%choice_basal_hydrology_model)
+    case default
+      call crash('unknown choice_basal_hydrology_model "' // trim( C%choice_basal_hydrology_model) // '"')
+    case ('none')
+      call calc_pore_water_pressure_none( mesh, ice)
+    case ('Martin2011')
+      call calc_pore_water_pressure_Martin2011( mesh, ice)
+    end select
 
     ! Calculate overburden and effective pressure
     ! ===========================================
 
-    DO vi = mesh%vi1, mesh%vi2
+    do vi = mesh%vi1, mesh%vi2
       ice%overburden_pressure( vi) = ice_density * grav * ice%Hi_eff( vi)
-      ice%effective_pressure(  vi) = MAX( 0._dp, ice%overburden_pressure( vi) - ice%pore_water_pressure( vi))
-    END DO
+      ice%effective_pressure(  vi) = max( 0._dp, ice%overburden_pressure( vi) - ice%pore_water_pressure( vi))
+    end do
 
     ! Finalise routine path
-    CALL finalise_routine( routine_name)
+    call finalise_routine( routine_name)
 
-  END SUBROUTINE run_basal_hydrology_model
+  end subroutine run_basal_hydrology_model
 
-! ===== Different basal hydrology models ====
-! ===========================================
+  subroutine calc_pore_water_pressure_none( mesh, ice)
 
-  ! == No subglacial hydrology
-  SUBROUTINE calc_pore_water_pressure_none( mesh, ice)
-    ! Calculate the pore water pressure
-    !
-    ! Use the parameterisation from Martin et al. (2011)
-
-    IMPLICIT NONE
-
-    ! Input variables:
-    TYPE(type_mesh),                     INTENT(IN)    :: mesh
-    TYPE(type_ice_model),                INTENT(INOUT) :: ice
+    ! In/output variables:
+    type(type_mesh),      intent(in   ) :: mesh
+    type(type_ice_model), intent(inout) :: ice
 
     ! Local variables:
-    CHARACTER(LEN=256), PARAMETER                      :: routine_name = 'calc_pore_water_pressure_none'
-    INTEGER                                            :: vi
-    REAL(dp)                                           :: weight_gr
+    character(len=1024), parameter :: routine_name = 'calc_pore_water_pressure_none'
+    integer                        :: vi
+    real(dp)                       :: weight_gr
 
     ! Add routine to path
-    CALL init_routine( routine_name)
+    call init_routine( routine_name)
 
     ! Compute pore water pressure based on the pore water fraction as
     ! the fraction of the overburden pressure supported by basal water
-    DO vi = mesh%vi1, mesh%vi2
+    do vi = mesh%vi1, mesh%vi2
       ice%pore_water_pressure( vi) = ice%pore_water_fraction(vi) * ice_density * grav * ice%Hi_eff( vi)
-    END DO
+    end do
 
     ! Finalise routine path
-    CALL finalise_routine( routine_name)
+    call finalise_routine( routine_name)
 
-  END SUBROUTINE calc_pore_water_pressure_none
+  end subroutine calc_pore_water_pressure_none
 
-  ! == Martin et al. (2011)
-  SUBROUTINE calc_pore_water_pressure_Martin2011( mesh, ice)
-    ! Calculate the pore water pressure
-    !
-    ! Use the parameterisation from Martin et al. (2011)
+  subroutine calc_pore_water_pressure_Martin2011( mesh, ice)
+    ! Calculate pore water pressure according to the parameterisation from Martin et al. (2011)
 
-    IMPLICIT NONE
-
-    ! Input variables:
-    TYPE(type_mesh),                     INTENT(IN)    :: mesh
-    TYPE(type_ice_model),                INTENT(INOUT) :: ice
+    ! In/output variables:
+    type(type_mesh),      intent(in   ) :: mesh
+    type(type_ice_model), intent(inout) :: ice
 
     ! Local variables:
-    CHARACTER(LEN=256), PARAMETER                      :: routine_name = 'calc_pore_water_pressure_Martin2011'
-    INTEGER                                            :: vi
+    character(len=1024), parameter :: routine_name = 'calc_pore_water_pressure_Martin2011'
+    integer                        :: vi
 
     ! Add routine to path
-    CALL init_routine( routine_name)
+    call init_routine( routine_name)
 
-    DO vi = mesh%vi1, mesh%vi2
+    do vi = mesh%vi1, mesh%vi2
 
       ! Pore water pressure scaling factor (Martin et al., 2011, Eq. 12)
-      ice%pore_water_fraction( vi) = MIN( 1._dp, MAX( 0._dp, 1._dp - (ice%Hb( vi) - ice%SL( vi) - C%Martin2011_hydro_Hb_min) / (C%Martin2011_hydro_Hb_max - C%Martin2011_hydro_Hb_min) ))
+      ice%pore_water_fraction( vi) = min( 1._dp, max( 0._dp, &
+        1._dp - (ice%Hb( vi) - ice%SL( vi) - C%Martin2011_hydro_Hb_min) / (C%Martin2011_hydro_Hb_max - C%Martin2011_hydro_Hb_min) ))
 
       ! Pore water pressure (Martin et al., 2011, Eq. 11)
       ice%pore_water_pressure( vi) = 0.96_dp * ice_density * grav * ice%Hi_eff( vi) * ice%pore_water_fraction( vi)
 
-    END DO
+    end do
 
     ! Finalise routine path
-    CALL finalise_routine( routine_name)
+    call finalise_routine( routine_name)
 
-  END SUBROUTINE calc_pore_water_pressure_Martin2011
+  end subroutine calc_pore_water_pressure_Martin2011
 
-END MODULE basal_hydrology
+end module basal_hydrology
