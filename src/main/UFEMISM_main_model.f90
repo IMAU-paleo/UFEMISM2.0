3d6405935da297ddb1e6082365e1aef2d6d2cac0
Martim Mas e Braga <mabrag@bas.ac.uk>
2025-06-17 13:40:45 +0100

    added region%ice to remap_ocean_model used in the 'realistic' ocean case

--- src/main/UFEMISM_main_model.f90
+++ src/main/UFEMISM_main_model.f90
@@ -1205,7 +1205,7 @@ CONTAINS
     ! Remap all the model data from the old mesh to the new mesh
     CALL remap_ice_dynamics_model(    region%mesh, mesh_new, region%ice, region%bed_roughness, region%refgeo_PD, region%SMB, region%BMB, region%LMB, region%AMB, region%GIA, region%time, region%name)
     CALL remap_climate_model(         region%mesh, mesh_new,             region%climate, region%name)
-    CALL remap_ocean_model(           region%mesh, mesh_new,             region%ocean  , region%name)
+    CALL remap_ocean_model(           region%mesh, mesh_new, region%ice, region%ocean  , region%name)
     CALL remap_SMB_model(             region%mesh, mesh_new,             region%SMB    , region%name)
     CALL remap_BMB_model(             region%mesh, mesh_new, region%ice, region%ocean, region%BMB    , region%name, region%time)
     CALL remap_LMB_model(             region%mesh, mesh_new,             region%LMB    , region%name)
