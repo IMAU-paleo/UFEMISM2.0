160a409bb18fa5f3548fa6facf17647e57b6696a
Martim Mas e Braga <mabrag@bas.ac.uk>
2025-06-17 13:08:01 +0100

    timeframe-interpolating function made more general to support all time series records

--- src/global_forcings/global_forcings_main.f90
+++ src/global_forcings/global_forcings_main.f90
@@ -17,6 +17,7 @@ MODULE global_forcings_main
   USE global_forcing_types                                   , ONLY: type_global_forcing
   USE netcdf_io_main
   USE netcdf_basic
+  USE series_utilities
 
   IMPLICIT NONE
 
@@ -110,7 +111,7 @@ CONTAINS
     select case (C%choice_sealevel_model)
       case ('prescribed')
         call read_field_from_series_file( C%filename_prescribed_sealevel, field_name_options_sealevel, forcing%sea_level_record, forcing%sea_level_time)
-        call update_sealevel_timeframes_from_curve( forcing, time)
+        call update_timeframes_from_record(forcing%sea_level_time, forcing%sea_level_record, forcing%sl_t0, forcing%sl_t1, forcing%sl_at_t0,forcing%sl_at_t1, time)
       case default
         call crash('Unknown choice of sea level!')
     end select
@@ -139,93 +140,13 @@ CONTAINS
     ! if not, read the two relevant timeframes from the NetCDF file
     IF (time < forcing%sl_t0 .OR. time > forcing%sl_t1) THEN
       !IF (par%primary)  WRITE(0,*) '   Model time is out of the current sea level timeframes. Updating timeframes...'
-      CALL update_sealevel_timeframes_from_curve( forcing, time)
+      call update_timeframes_from_record(forcing%sea_level_time, forcing%sea_level_record, forcing%sl_t0, forcing%sl_t1, forcing%sl_at_t0, forcing%sl_at_t1, time)
     END IF
     
     ! Finalise routine path
     CALL finalise_routine( routine_name)
 
   END SUBROUTINE update_sealevel_at_model_time
-
-
-  SUBROUTINE update_sealevel_timeframes_from_curve( forcing, time)
-    ! Update the sea level timeframes so we can interpolate between two points in the sea level curve
-
-    IMPLICIT NONE
-
-    TYPE(type_global_forcing),         INTENT(INOUT)   :: forcing
-    REAL(dp),                             INTENT(IN)   :: time
-
-    ! Local variables:
-    CHARACTER(LEN=256), PARAMETER                      :: routine_name = 'update_sealevel_timeframes_from_curve'
-    INTEGER                                            :: ti0, ti1, tii, ncid, nt
-    CHARACTER(LEN=256)                                 :: str
-    REAL(dp)                                           :: dt_min 
-
-    ! Add routine to path
-    CALL init_routine( routine_name)
-
-    
-    ! Update sea level
-    ! Find timeframe closest to desired time
-    nt = size(forcing%sea_level_time)
-    if (forcing%sea_level_time( 1) > time) then
-      ! Desired time beyond lower limit
-      !call warning('desired timeframe at t = {dp_01} before start of sea level record time; reading data from t = {dp_02} instead!', &
-      !  dp_01 = time, dp_02 = forcing%sea_level_time( 1))
-      ti0 = 1
-    elseif (forcing%sea_level_time( nt) < time) then
-      ! Desired time beyond upper limit
-      !call warning('desired timeframe at t = {dp_01} after end of sea level record time; reading data from t = {dp_02} instead!', &
-      !  dp_01 = time, dp_02 = forcing%sea_level_time( nt))
-      ti0 = nt
-    else
-      ! Desired time is within the file time
-      dt_min = huge( 1._dp)
-      do tii = 1, nt
-        if (abs( forcing%sea_level_time( tii) - time) < dt_min) then
-          ti0 = tii
-          dt_min = abs( forcing%sea_level_time( tii) - time)
-        end if
-      end do
-      if (dt_min > 0._dp) then
-        !call warning('desired timeframe at t = {dp_01} not present in sea level record; reading data from closest match at t = {dp_02} instead!', &
-        !  dp_01 = time, dp_02 = forcing%sea_level_time( ti0))
-      end if
-    end if
-      
-    
-    forcing%sl_t0    = forcing%sea_level_time(ti0)
-    forcing%sl_at_t0 = forcing%sea_level_record(ti0)
-      
-    ! if the desired time is after t0, we take one record after for t1
-    if (time >= forcing%sl_t0) then
-      if (ti0 == size(forcing%sea_level_time)) then
-        IF (par%primary) WRITE(0,*) 'desired timeframe is at or beyond the last record. Using last available value for both timeframes...'
-        forcing%sl_t1    = forcing%sea_level_time(ti0)
-        forcing%sl_at_t1 = forcing%sea_level_record(ti0)
-      else
-        forcing%sl_t1    = forcing%sea_level_time(ti0+1)
-        forcing%sl_at_t1 = forcing%sea_level_record(ti0+1)
-      end if
-    else
-      ! otherwise we read one record before for t0, and that record is t1
-      if (ti0 == 1) then
-        IF (par%primary) WRITE(0,*) 'desired timeframe is at or before the first record. Using first available value for both timeframes...'
-        forcing%sl_t1    = forcing%sea_level_time(ti0)
-        forcing%sl_at_t1 = forcing%sea_level_record(ti0)
-      else
-        forcing%sl_t1    = forcing%sea_level_time(ti0)
-        forcing%sl_at_t1 = forcing%sea_level_record(ti0)
-        forcing%sl_t0    = forcing%sea_level_time(ti0-1)
-        forcing%sl_at_t0 = forcing%sea_level_record(ti0-1)
-      end if
-    end if
-
-    ! Finalise routine path
-    CALL finalise_routine( routine_name)
-
-  END SUBROUTINE update_sealevel_timeframes_from_curve
 
   SUBROUTINE update_sealevel_in_model(forcing, mesh, ice, time)
   ! Update the current sea level based on the loaded sea level curve
@@ -246,7 +167,7 @@ CONTAINS
     CALL init_routine( routine_name)
 
     ! Calculate timeframe interpolation weights (plus safety checks for when the extend beyond the record)
-    CALL interpolate_sea_level_from_forcing_record(forcing, time, computed_sea_level) ! We might be calling this twice with no need, but might be worth keeping it like that to facilitate future impoementations
+    CALL interpolate_value_from_forcing_record(forcing%sl_t0, forcing%sl_t1, forcing%sl_at_t0, forcing%sl_at_t1, time, computed_sea_level) ! We might be calling this twice with no need, but might be worth keeping it like that to facilitate future impoementations
 
     do vi = mesh%vi1, mesh%vi2
       ice%SL( vi) = computed_sea_level
@@ -256,44 +177,5 @@ CONTAINS
     CALL finalise_routine( routine_name)
 
   END SUBROUTINE update_sealevel_in_model
-
-  SUBROUTINE interpolate_sea_level_from_forcing_record(forcing, time, computed_sea_level)
-  ! Interpolate between time frames
-
-    IMPLICIT NONE
-
-    TYPE(type_global_forcing),         INTENT(IN)      :: forcing
-    REAL(dp),                          INTENT(  OUT)   :: computed_sea_level
-    REAL(dp),                          INTENT(IN   )   :: time
-
-    ! Local variables:
-    CHARACTER(LEN=256), PARAMETER                      :: routine_name = 'interpolate_sea_level_from_forcing_record'
-    REAL(dp)                                           :: wt0,wt1
-
-    ! Add routine to path
-    CALL init_routine( routine_name)
-
-
-    if (forcing%sl_t1 == forcing%sl_t0) then
-      wt0 = 0._dp
-      wt1 = 1._dp
-    else
-      if (time > forcing%sl_t1) then
-        wt0 = 0._dp
-      elseif (time < forcing%sl_t0) then
-        wt0 = 1._dp
-      else
-        wt0 = (forcing%sl_t1 - time) / (forcing%sl_t1 - forcing%sl_t0)
-      end if
-      wt1 = 1._dp - wt0
-    end if
-
-    ! Interpolate the two timeframes - constant sea level over the entire region
-    computed_sea_level = wt0 * forcing%sl_at_t0 + wt1 * forcing%sl_at_t1
-
-    ! Finalise routine path
-    CALL finalise_routine( routine_name)
-
-  END SUBROUTINE interpolate_sea_level_from_forcing_record
 
 END MODULE global_forcings_main
