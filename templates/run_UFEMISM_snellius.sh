3a69e698af57355017c6c09fd580cd58945cbac1
TijnBerends <c.j.berends@uu.nl>
2025-06-18 09:34:50 +0200

    Update template scripts
    Update modules for the Snellius run script, delete obsolete compile scripts.

--- templates/run_UFEMISM_snellius.sh
+++ templates/run_UFEMISM_snellius.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 #Set job requirements
 #SBATCH --time=05:00
-#SBATCH --partition=thin
+#SBATCH --partition=rome
 #SBATCH --nodes=1
 #SBATCH --mem=50G
 #SBATCH --ntasks=32
 #SBATCH --cpus-per-task=1
 
 # Load modules for MPI, NetCDF, PETSc, and other required libraries
-module load 2021
-module load eb/4.5.2
-module load foss/2021a
-module load netCDF-Fortran/4.5.3-gompi-2021a
-module load PETSc/3.15.1-foss-2021a
-module load imkl/2021.2.0-iompi-2021a
-module load OpenMPI/4.1.1-GCC-10.3.0
+module load 2024
+module load netCDF-Fortran/4.6.1-gompi-2024a
+module load PETSc/3.22.0-foss-2024a
+module load OpenMPI/5.0.3-GCC-13.3.0
 
 # Execute the program
 srun UFEMISM_program config-files/config_test
