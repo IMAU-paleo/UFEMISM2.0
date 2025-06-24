c825d0f54e5ce8052d4cbe48316959bb780f511e
TijnBerends <c.j.berends@uu.nl>
2025-06-24 14:24:08 +0200

    Move dH_dt to Halfar module
    Hurray

--- src/basic/analytical_solutions/Halfar_SIA_solution.f90
+++ src/basic/analytical_solutions/Halfar_SIA_solution.f90
@@ -20,8 +20,12 @@ module Halfar_SIA_solution
 
     ! Extended by Tijn to include expressions for the ice velocities and thinning rates
 
+    private
+
   contains
-    procedure :: H
+
+    procedure, public :: H
+    procedure, public :: dH_dt
 
   end type type_Halfar_solution
 
@@ -73,15 +77,44 @@ contains
     f3 = r(x,y) / R0
   end function f3
 
+  real(dp) function df1_dt( A,n,H0,R0,t)
+    real(dp), intent(in) :: A,n,H0,R0,t
+    df1_dt = p1( n) / t0( A,n,H0,R0) * ((t0( A,n,H0,R0) + t) / t0( A,n,H0,R0) )**(p1( n) - 1._dp)
+  end function df1_dt
+  real(dp) function df2_dt( A,n,H0,R0,t)
+    real(dp), intent(in) :: A,n,H0,R0,t
+    df2_dt = p2( n) / t0( A,n,H0,R0) * ((t0( A,n,H0,R0) + t) / t0( A,n,H0,R0) )**(p2( n) - 1._dp)
+  end function df2_dt
+
   real(dp) function G( A,n,H0,R0,x,y,t)
     real(dp), intent(in) :: A,n,H0,R0,x,y,t
     G = 1._dp - (min( 1._dp, f2( A,n,H0,R0,t) * f3( R0,x,y)))**p3( n)
   end function G
+  real(dp) function dG_dt( A,n,H0,R0,x,y,t)
+    real(dp), intent(in) :: A,n,H0,R0,x,y,t
+    dG_dt = -p3( n) * f2( A,n,H0,R0,t)**(p3( n) - 1._dp) * df2_dt( A,n,H0,R0,t) * f3( R0,x,y)**p3( n)
+  end function dG_dt
 
   real(dp) function H( self,A,n,H0,R0,x,y,t)
     class(type_Halfar_solution), intent(in) :: self
     real(dp),                    intent(in) :: A,n,H0,R0,x,y,t
     H = H0 * f1( A,n,H0,R0,t) * G( A,n,H0,R0,x,y,t)**p4( n)
   end function H
+  real(dp) function dH_dt( self,A,n,H0,R0,x,y,t)
+    class(type_Halfar_solution), intent(in) :: self
+    real(dp),                    intent(in) :: A,n,H0,R0,x,y,t
+
+    if (r(x,y) <= R0) then
+      dH_dt = H0 * (&
+        df1_dt( A,n,H0,R0,t) *          G( A,n,H0,R0,x,y,t)** p4( n) &
+        + f1   ( A,n,H0,R0,t) * p4( n) * G( A,n,H0,R0,x,y,t)**(p4( n) - 1._dp) * dG_dt( A,n,H0,R0,x,y,t))
+    else
+      dH_dt = 0._dp
+    end if
+
+    ! Convert to m/yr
+    dH_dt = dH_dt * sec_per_year
+
+  end function dH_dt
 
 end module Halfar_SIA_solution
