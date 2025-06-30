961291c695dc307b9ae43654ec5332c93aead868
Martim Mas e Braga <mabrag@bas.ac.uk>
2025-06-30 10:21:02 +0100

    GI and dT are now totally separate transient ocean models

---
+++ src/ocean/ocean_GlacialIndex.f90
@@ -0,0 +1,142 @@
+module ocean_GlacialIndex
+
+  ! Glacial Index ocean model (i.e., T,S = T_warm,S_warm + GI * (T_cold,S_cold - T_warm,S_warm)
+
+! ===== Preamble =====
+! ====================
+
+  use precisions                                             , only: dp
+  use mpi_basic                                              , only: par, sync
+  use control_resources_and_error_messaging                  , only: crash, init_routine, finalise_routine, colour_string
+  use model_configuration                                    , only: C
+  use parameters
+  use mesh_types                                             , only: type_mesh
+  use ice_model_types                                        , only: type_ice_model
+  use ocean_model_types                                      , only: type_ocean_model
+  use netcdf_io_main
+  use ocean_extrapolation                                    , only: extrapolate_ocean_forcing
+  use series_utilities
+
+  implicit none
+
+contains
+
+! ===== Main routines =====
+! =========================
+
+subroutine initialise_ocean_model_GlacialIndex( mesh, ice, ocean, region_name, start_time_of_run)
+    ! Initialise the ocean model
+    !
+    ! Use a realistic ocean scheme
+
+    ! In- and output variables
+    type(type_mesh),                        intent(in)    :: mesh
+    type(type_ice_model),                   intent(in)    :: ice
+    type(type_ocean_model),                 intent(inout) :: ocean
+    character(len=3),                       intent(in)    :: region_name
+    real(dp),                               intent(in)    :: start_time_of_run
+
+    ! Local variables:
+    character(len=256), parameter                         :: routine_name = 'initialise_ocean_model_GlacialIndex'
+    character(len=256)                                    :: filename_ocean_snapshot_warm, filename_ocean_snapshot_cold, filename_ocean_GI
+
+    ! Add routine to path
+    call init_routine( routine_name)
+
+    if (par%primary)  write(*,"(A)") '     Initialising transient ocean model "' // &
+            colour_string( trim( C%choice_ocean_model_transient),'light blue') // '"...'
+    ! We need the snapshot and the dT to apply to it
+    select case (region_name)
+        case ('NAM')
+        filename_ocean_snapshot_warm = C%filename_ocean_warm_snapshot_NAM
+        filename_ocean_snapshot_cold = C%filename_ocean_cold_snapshot_NAM
+        filename_ocean_GI            = C%filename_ocean_GI_NAM
+        case ('EAS')
+        filename_ocean_snapshot_warm = C%filename_ocean_warm_snapshot_EAS
+        filename_ocean_snapshot_cold = C%filename_ocean_cold_snapshot_EAS
+        filename_ocean_GI            = C%filename_ocean_GI_EAS
+        case ('GRL')
+        filename_ocean_snapshot_warm = C%filename_ocean_warm_snapshot_GRL
+        filename_ocean_snapshot_cold = C%filename_ocean_cold_snapshot_GRL
+        filename_ocean_GI            = C%filename_ocean_GI_GRL
+        case ('ANT')
+        filename_ocean_snapshot_warm = C%filename_ocean_warm_snapshot_ANT
+        filename_ocean_snapshot_cold = C%filename_ocean_cold_snapshot_ANT
+        filename_ocean_GI            = C%filename_ocean_GI_ANT
+        case default
+        call crash('unknown region_name "' // region_name // '"')
+    end select
+
+    ! Allocating timeframe variables; the series itself is allocated in the read function below
+    allocate(ocean%GI%GI_t0)
+    allocate(ocean%GI%GI_t1)
+    allocate(ocean%GI%GI_at_t0)
+    allocate(ocean%GI%GI_at_t1)
+    allocate( ocean%GI%T0_warm( mesh%vi1:mesh%vi2,C%nz_ocean))
+    allocate( ocean%GI%S0_warm( mesh%vi1:mesh%vi2,C%nz_ocean))
+    allocate( ocean%GI%T0_cold( mesh%vi1:mesh%vi2,C%nz_ocean))
+    allocate( ocean%GI%S0_cold( mesh%vi1:mesh%vi2,C%nz_ocean))
+    ocean%GI%T0_warm = 0._dp
+    ocean%GI%S0_warm = 0._dp
+    ocean%GI%T0_cold = 0._dp
+    ocean%GI%S0_cold = 0._dp
+
+    ! Fill in  main variables
+    call read_field_from_file_3D_ocean( filename_ocean_snapshot_warm, field_name_options_T_ocean,  mesh, ocean%GI%T0_warm)
+    call read_field_from_file_3D_ocean( filename_ocean_snapshot_warm, field_name_options_S_ocean,  mesh, ocean%GI%S0_warm)
+    call read_field_from_file_3D_ocean( filename_ocean_snapshot_cold, field_name_options_T_ocean,  mesh, ocean%GI%T0_cold)
+    call read_field_from_file_3D_ocean( filename_ocean_snapshot_cold, field_name_options_S_ocean,  mesh, ocean%GI%S0_cold)
+
+    call read_field_from_series_file(   filename_ocean_GI,       field_name_options_GI, ocean%GI%GI_series, ocean%GI%GI_series_time)
+    call update_timeframes_from_record(ocean%GI%GI_series_time, ocean%GI%GI_series, ocean%GI%GI_t0, ocean%GI%GI_t1, ocean%GI%GI_at_t0, ocean%GI%GI_at_t1, start_time_of_run)
+
+    ! Apply extrapolation method if required
+    select case (C%choice_ocean_extrapolation_method)
+        case('initialisation')
+        call extrapolate_ocean_forcing( mesh, ice, ocean%GI%T0_warm)
+        call extrapolate_ocean_forcing( mesh, ice, ocean%GI%S0_warm) 
+        call extrapolate_ocean_forcing( mesh, ice, ocean%GI%T0_cold)
+        call extrapolate_ocean_forcing( mesh, ice, ocean%GI%S0_cold) 
+        case default
+        call crash('unknown choice_ocean_extrapolation_method "' // trim( C%choice_ocean_extrapolation_method) // '"')
+    end select  
+
+    call finalise_routine(routine_name)
+
+end subroutine initialise_ocean_model_GlacialIndex
+
+subroutine run_ocean_model_GlacialIndex(mesh, ocean, time)
+  ! Runs the Glacial Index transient ocean model
+    ! In- and output variables
+    type(type_mesh),                        intent(in)    :: mesh
+    type(type_ocean_model),                 intent(inout) :: ocean
+    real(dp),                               intent(in)    :: time
+
+    ! Local variables
+    CHARACTER(LEN=256), PARAMETER                      :: routine_name = 'run_ocean_model_GlacialIndex'
+    REAL(dp)                                           :: GI_at_time
+    INTEGER                                            :: vi, z
+
+    call init_routine( routine_name)
+
+    IF (time < ocean%GI%GI_t0 .OR. time > ocean%GI%GI_t1) THEN
+          !IF (par%primary)  WRITE(0,*) '   Model time is out of the current GI timeframes. Updating timeframes...'
+          call update_timeframes_from_record(ocean%GI%GI_series_time, ocean%GI%GI_series, ocean%GI%GI_t0, ocean%GI%GI_t1, ocean%GI%GI_at_t0, ocean%GI%GI_at_t1, time)
+        END IF
+
+        call interpolate_value_from_forcing_record(ocean%GI%GI_t0, ocean%GI%GI_t1, ocean%GI%GI_at_t0, ocean%GI%GI_at_t1, time, GI_at_time)
+
+        do vi = mesh%vi1, mesh%vi2
+          do z = 1, C%nz_ocean
+            ocean%T(vi, z) = ocean%GI%T0_warm(vi, z) + GI_at_time * (ocean%GI%T0_cold(vi, z) - ocean%GI%T0_warm(vi, z))
+            ocean%S(vi, z) = ocean%GI%S0_warm(vi, z) + GI_at_time * (ocean%GI%S0_cold(vi, z) - ocean%GI%S0_warm(vi, z))
+          end do
+        end do
+
+
+    call finalise_routine(routine_name)
+
+end subroutine run_ocean_model_GlacialIndex
+
+
+end module ocean_GlacialIndex
--- src/types/ocean_model_types.f90
+++ src/types/ocean_model_types.f90
@@ -11,7 +11,7 @@ MODULE ocean_model_types
 
 ! ===== Types =====
 ! =================
-TYPE type_ocean_model_transient
+TYPE type_ocean_model_deltaT
     ! Main data fields to compute the ocean model T and S transiently
     REAL(dp), DIMENSION(:,:  ), ALLOCATABLE :: T0                           ! [degrees Celsius] Temperature
     REAL(dp), DIMENSION(:,:  ), ALLOCATABLE :: S0                           ! [PSU]             Salinity
@@ -21,14 +21,22 @@ TYPE type_ocean_model_transient
     REAL(dp), DIMENSION(:    ), ALLOCATABLE     :: dT_series 
     REAL(dp),                   ALLOCATABLE     :: dT_t0, dT_t1, dT_at_t0, dT_at_t1
 
+  END TYPE type_ocean_model_deltaT
+
+  TYPE type_ocean_model_GlacialIndex
+    ! Main data fields to compute the ocean model T and S transiently
+    REAL(dp), DIMENSION(:,:  ), ALLOCATABLE :: T0_warm                           ! [degrees Celsius] Temperature
+    REAL(dp), DIMENSION(:,:  ), ALLOCATABLE :: S0_warm                           ! [PSU]             Salinity
+    REAL(dp), DIMENSION(:,:  ), ALLOCATABLE :: T0_cold                           ! [degrees Celsius] Temperature
+    REAL(dp), DIMENSION(:,:  ), ALLOCATABLE :: S0_cold                           ! [PSU]             Salinity
+
     ! Glacial Index record
     REAL(dp), DIMENSION(:    ), ALLOCATABLE     :: GI_series_time
     REAL(dp), DIMENSION(:    ), ALLOCATABLE     :: GI_series 
     REAL(dp),                   ALLOCATABLE     :: GI_t0, GI_t1, GI_at_t0, GI_at_t1
-    REAL(dp), DIMENSION(:,:  ), ALLOCATABLE :: T0_cold                           ! [degrees Celsius] Temperature
-    REAL(dp), DIMENSION(:,:  ), ALLOCATABLE :: S0_cold                           ! [PSU]             Salinity
+  
 
-  END TYPE type_ocean_model_transient
+  END TYPE type_ocean_model_GlacialIndex
 
   TYPE type_ocean_model
     ! The ocean model data structure.
@@ -41,7 +49,8 @@ TYPE type_ocean_model_transient
     REAL(dp), DIMENSION(:    ), ALLOCATABLE :: T_draft                     ! [degrees Celsius] Temperature at ice base
     REAL(dp), DIMENSION(:    ), ALLOCATABLE :: T_freezing_point            ! [degrees Celsius] Pressure freezing point of water
 
-    TYPE(type_ocean_model_transient)        :: transient
+    TYPE(type_ocean_model_deltaT)           :: deltaT
+    TYPE(type_ocean_model_GlacialIndex)     :: GI
 
     ! Metadata
     CHARACTER(LEN=256)                      :: restart_filename            ! Name for generated restart file
