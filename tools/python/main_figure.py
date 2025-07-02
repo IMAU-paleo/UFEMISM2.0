f8c7707b9dce1c7ca83c4cd3c40c4884da882750
Erwin Lambert <45197956+erwinlambert@users.noreply.github.com>
2025-07-02 13:13:49 +0200

    add differenceplot to python tools

--- tools/python/main_figure.py
+++ tools/python/main_figure.py
@@ -42,12 +42,24 @@ class Figure(object):
     def add_field(self, Timeframe, varname, mask=None):
         field = Field(Timeframe, varname, mask=mask)
 
-        self.fields[field.name] = field
+        print(f'Added {varname} with hash {hash(field)}')
+
+        self.fields[hash(field)] = field
         return 
 
-    def delete_field(self,fieldname):
+    def add_diff(self, Timeframe1, varname1, Timeframe2, varname2, mask=None, name='diff'):
+        field1 = Field(Timeframe1, varname1)
+        field2 = Field(Timeframe2, varname2)
+        field = DiffField(field1, field2, name=name, mask=mask)
 
-        del self.fields[fieldname]
+        print(f'Added difference field {name} with hash {hash(field)}')
+
+        self.fields[hash(field)] = field
+        return 
+
+    def delete_field(self,fieldhash):
+
+        del self.fields[fieldhash]
         return
 
     def print_fields(self):
@@ -55,7 +67,7 @@ class Figure(object):
         print(self.fields.keys())
         return
 
-    def make(self,figname,dxmin=0,dxmax=0,dymin=0,dymax=0, add_gl=True):
+    def make(self,figname,dxmin=0,dxmax=0,dymin=0,dymax=0, add_gl=True, add_time=False):
 
         fig = plt.figure(figsize=self.figsize,constrained_layout=True)
 
@@ -95,7 +107,8 @@ class Figure(object):
             field.ax.set_xticks([])
             field.ax.set_yticks([])
 
-        fig.suptitle(f'Year {field.Timeframe.time:.0f}')
+        if add_time:
+            fig.suptitle(f'Year {field.Timeframe.time:.0f}')
 
         fullfigname = os.path.join(self.directory,f'{figname}.png')
         plt.savefig(fullfigname, bbox_inches = 'tight', pad_inches = 0,dpi=450)
@@ -111,6 +124,7 @@ class Field(object):
     def __init__(self, Timeframe, varname, mask=None):
 
         self.Timeframe = Timeframe
+        self.Mesh = self.Timeframe.Mesh
         self.t = self.Timeframe.t
         self.varname = varname
         self.name = f"{varname}_{self.t}"
@@ -185,13 +199,13 @@ class Field(object):
 
         #Check type (voronoi / triangle)
         if 'vi' in self.data.dims:
-            if not self.Timeframe.Mesh.got_voronois:
-                self.Timeframe.Mesh.get_voronois()
-            self.pcoll = PatchCollection(self.Timeframe.Mesh.voronois,cmap=self.cmap,norm=self.norm)
+            if not self.Mesh.got_voronois:
+                self.Mesh.get_voronois()
+            self.pcoll = PatchCollection(self.Mesh.voronois,cmap=self.cmap,norm=self.norm)
         elif 'ti' in self.data.dims:
-            if not self.Timeframe.Mesh.got_triangles:
-                self.Timeframe.Mesh.get_triangles()
-            self.pcoll = PatchCollection(self.Timeframe.Mesh.triangles,cmap=self.cmap,norm=self.norm)
+            if not self.Mesh.got_triangles:
+                self.Mesh.get_triangles()
+            self.pcoll = PatchCollection(self.Mesh.triangles,cmap=self.cmap,norm=self.norm)
         else:
             print(f'ERROR: variable {varname} is not on vertices or triangles')
 
@@ -206,10 +220,135 @@ class Field(object):
         mcmap = 'ocean'
         mnorm = mpl.colors.Normalize(vmin=1.6, vmax=3.1, clip=True)
 
-        if not self.Timeframe.Mesh.got_voronois:
-            self.Timeframe.Mesh.get_voronois()
+        if not self.Mesh.got_voronois:
+            self.Mesh.get_voronois()
 
-        self.mcoll = PatchCollection(self.Timeframe.Mesh.voronois,cmap=mcmap,norm=mnorm)
+        self.mcoll = PatchCollection(self.Mesh.voronois,cmap=mcmap,norm=mnorm)
+
+        # Fill array
+        self.mcoll.set_array(self.Timeframe.mask)
+
+        return
+
+class DiffField(object):
+    """ Difference field between two Fields """
+
+    def __init__(self, Field1, Field2, name, mask=None):
+
+        self.Field1 = Field1
+        self.Field2 = Field2
+
+        self.name = name
+        self.varname = 'diff'
+        self.mask = mask
+
+        self.xmin = self.Field1.xmin
+        self.xmax = self.Field1.xmax
+        self.ymin = self.Field1.ymin
+        self.ymax = self.Field1.ymax
+
+        self.check_compatibility()
+
+        self.get_cmap()
+
+        self.get_data()
+
+    def __repr__(self):
+        return f"DiffField({repr(self.Field1)}, {repr(self.Field2)}, {self.name})"
+
+    def __str__(self):
+        return f"DiffField between {self.Field1.name} at time {self.Field1.Timeframe.t} and {self.Field1.name} at time {self.Field1.Timeframe.t}"
+
+    def check_compatibility(self):
+        """ Check whether difference field can be computed """
+
+        assert self.xmin == self.Field2.xmin, 'ERROR: xmin is not equal between input Fields'
+        assert self.xmax == self.Field2.xmax, 'ERROR: xmax is not equal between input Fields'
+        assert self.ymin == self.Field2.ymin, 'ERROR: ymin is not equal between input Fields'
+        assert self.ymax == self.Field2.ymax, 'ERROR: ymax is not equal between input Fields'
+
+        if 'vi' in self.Field1.data.dims:
+            assert 'vi' in self.Field2.data.dims, 'ERROR: cannot take difference between voronois and triangles'
+            assert (self.Field1.Timeframe.ds['V'] == self.Field2.Timeframe.ds['V']).all(), 'ERROR: V is not equal between input Fields'
+        elif 'ti' in self.Field1.data.dims:
+            assert 'ti' in self.Field2.data.dims, 'ERROR: cannot take difference between voronois and triangles'
+            assert (self.Field1.Timeframe.ds['Tri'] == self.Field2.Timeframe.ds['Tri']).all(), 'ERROR: ti is not equal between input Fields'
+
+        print('Input Fields are compatible')
+        self.Mesh = self.Field1.Timeframe.Mesh
+        return
+
+    def get_cmap(self):
+        """ Define cmap and norm """
+        self.cmap = plt.get_cmap('cmo.diff')
+        self.dmax = max(abs(self.Field1.data-self.Field2.data).values)
+        if self.dmax == 0:
+            print('WARNING: both fields are equal')
+            vmax = 1
+        else:
+            vmax = self.dmax
+
+        self.norm = mpl.colors.Normalize(vmin=-vmax,vmax=vmax,clip=True)
+        return
+
+    def get_data(self):
+        """ Get data and add mask if necessary """
+
+        self.data = self.Field1.data - self.Field2.data
+
+        if self.mask is not None:
+            if 'vi' in self.data.dims:
+                if not self.Timeframe.got_mask:
+                    self.Timeframe.get_mask()
+
+                if self.mask == 'shelf':
+                    self.data = xr.where(self.Timeframe.mask == 4, self.data, np.nan)
+                elif self.mask == 'ice':
+                    self.data = xr.where([x in [3,4] for x in self.Timeframe.mask], self.data, np.nan)
+                else:
+                    raise ValueError('Invalid option for mask')
+            elif 'ti' in self.data.dims:
+                # Just mask out zero values, no clean option yet
+                self.data = xr.where(self.data==0, np.nan, self.data)
+            else:
+                print(f'ERROR: something went wrong with vertices and triangles')
+
+            message = f"Extracted {self.name} with mask {self.mask}"
+        else:
+            message = f"Extracted {self.name}"
+
+        return message
+
+    def get_pcoll(self):
+        """ Get patch collection """
+
+        #Check type (voronoi / triangle)
+        if 'vi' in self.data.dims:
+            if not self.Mesh.got_voronois:
+                self.Mesh.get_voronois()
+            self.pcoll = PatchCollection(self.Mesh.voronois,cmap=self.cmap,norm=self.norm)
+        elif 'ti' in self.data.dims:
+            if not self.Mesh.got_triangles:
+                self.Mesh.get_triangles()
+            self.pcoll = PatchCollection(self.Mesh.triangles,cmap=self.cmap,norm=self.norm)
+        else:
+            print(f'ERROR: variable {varname} is not on vertices or triangles')
+
+        # Fill array
+        self.pcoll.set_array(self.data.values)
+
+        return
+
+    def get_mcoll(self):
+        """ Get mask collection """
+
+        mcmap = 'ocean'
+        mnorm = mpl.colors.Normalize(vmin=1.6, vmax=3.1, clip=True)
+
+        if not self.Mesh.got_voronois:
+            self.Mesh.get_voronois()
+
+        self.mcoll = PatchCollection(self.Mesh.voronois,cmap=mcmap,norm=mnorm)
 
         # Fill array
         self.mcoll.set_array(self.Timeframe.mask)
